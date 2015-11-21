0934a27c3d0f567a7d478b428e41424e2c9274a2
joesilber <jhsilber@lbl.gov>
2015-11-21 23:10:38 +0000

    split the with-collision vs without-collision into two clearly marked internal methods

--- petal/PosScheduler.py
+++ petal/PosScheduler.py
@@ -9,12 +9,25 @@ class PosScheduler(object):
 		pass
 		
 	def schedule_moves(Qstart,Pstart,Qtarg,Ptarg,anticollision_on):
-		if anticollision_on:
-			# run the algorithm for anticollision
-		else
-			# convert all the Qs and Ps into local theta,phi
-			# delta = finish - start
-			# make move tables to do deltas
-			# use PosModel to get true moves and update the tables
-			
-		return move_tables
+         # generate initial move tables
+         if anticollision_on:
+             move_tables_ideal = __schedule_with_anticollision(Qstart,Pstart,Qtarg,Ptarg)
+         else:
+             move_tables_ideal = __schedule_without_anticollision(Qstart,Pstart,Qtarg,Ptarg)
+         # use PosModel to get true moves (quantized into steps, and true timing estimate) and update the tables
+         # re-run the anticollision check one more time
+         return move_tables
+         
+     # internal methods    
+     def __schedule_without_anticollision(Qstart,Pstart,Qtarg,Ptarg):
+         # convert all the Qs and Ps into local theta,phi
+         # delta = finish - start
+         # make move tables to do deltas
+         # use PosModel to get true moves and update the tables
+         move_tables = None # replace with lots of real code
+         return move_tables
+
+     def __schedule_with_anticollision(Qstart,Pstart,Qtarg,Ptarg):
+         # anticollision algorithm goes here
+         move_tables = None # replace with lots of real code
+         return move_tables
