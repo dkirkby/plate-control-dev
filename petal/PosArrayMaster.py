4eddcb4704f45206bb38ddf20ddca633f96268fd
joesilber <jhsilber@lbl.gov>
2015-11-22 02:05:02 +0000

    groupalized the (P,Q)

--- petal/PosArrayMaster.py
+++ petal/PosArrayMaster.py
@@ -7,28 +7,28 @@ class PosArrayMaster(object):
 	def __init__(self,list_of_posids):
 		self.positioners = some array of PosModel instances
 		
-	def setup_move_tables(Qtarget,Ptarget)
+	def setup_move_tables(PQtarget)
 		"""Given a list of target positions, returns the move tables (one for each
 		positioner) that get from start to target."""
 		
 		for pos in self.positioners
 			# use PosModel to get calibration values
 			# use PosModel to get current local theta,phi
-			# use PosTransforms to convert current local theta,phi to Qstart, Pstart
+			# use PosTransforms to convert current local theta,phi to Pstart, Qstart
 			
-		# now for the full array, pass all the start and target Q,P to PosScheduler,
+		# now for the full array, pass all the start and target (P,Q) to PosScheduler,
 		# (as well as calibration values). You get back move tables.
-		move_tables = PosScheduler.schedule_moves(Qstart,Pstart,Qtarget,Ptarget,other?)
+		move_tables = PosScheduler.schedule_moves(Pstart,Qstart,Ptarget,Qtarget,other?)
 
 		return move_tables
 		
-	def update_encoded_positions(Q,P)
+	def update_encoded_positions(PQ)
 		"""Generally used after performing moves, so the PosModel instances can be
-		informed that the hardware move was done. The argued Q,P get stored to each
+		informed that the hardware move was done. The argued (P,Q) get stored to each
 		positioner. These aren't measured (FVC) positions."""
 		
 		for pos in self.positioners
-			# use PosTransforms to convert the Q,P into local theta,phi
+			# use PosTransforms to convert the (P,Q) into local theta,phi
 			# update the PosState (via PosModel?) for each
 			
 	def hardware_ready_move_tables(move_tables)
--- petal/PosScheduler.py
+++ petal/PosScheduler.py
@@ -1,33 +1,33 @@
 # imports here
 
 class PosScheduler(object):
-	"""Generates move schedules in local (t,p) to get positioners from starts to finishes.
+	"""Generates move schedules in local (th,ph) to get positioners from starts to finishes.
 	   We should get a clear definition of the move table format in here early."""
 
 	def __init__(self):
 		#maybe not necessary?
 		pass
 		
-	def schedule_moves(Qstart,Pstart,Qtarg,Ptarg,anticollision_on):
+	def schedule_moves(PQstart,PQtarg,anticollision_on):
          # generate initial move tables
          if anticollision_on:
-             move_tables_ideal = _schedule_with_anticollision(Qstart,Pstart,Qtarg,Ptarg)
+             move_tables_ideal = _schedule_with_anticollision(PQstart,PQtarg)
          else:
-             move_tables_ideal = _schedule_without_anticollision(Qstart,Pstart,Qtarg,Ptarg)
+             move_tables_ideal = _schedule_without_anticollision(PQstart,PQtarg)
          # use PosModel to get true moves (quantized into steps, and true timing estimate) and update the tables
          # re-run the anticollision check one more time
          return move_tables
          
      # internal methods    
-     def _schedule_without_anticollision(Qstart,Pstart,Qtarg,Ptarg):
-         # convert all the Qs and Ps into local theta,phi
+     def _schedule_without_anticollision(PQstart,PQtarg):
+         # convert all the (P,Q) into local (th,ph)
          # delta = finish - start
          # make move tables to do deltas
          # use PosModel to get true moves and update the tables
          move_tables = None # replace with lots of real code
          return move_tables
 
-     def _schedule_with_anticollision(Qstart,Pstart,Qtarg,Ptarg):
+     def _schedule_with_anticollision(PQstart,PQtarg):
          # anticollision algorithm goes here
          move_tables = None # replace with lots of real code
          return move_tables
