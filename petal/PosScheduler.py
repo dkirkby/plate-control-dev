55730d5dcd8a16bbc75b12987e5170e6548e5a18
joesilber <jhsilber@lbl.gov>
2015-11-22 01:47:43 +0000

    removed name-mangler double-underscores

--- petal/PosScheduler.py
+++ petal/PosScheduler.py
@@ -11,15 +11,15 @@ class PosScheduler(object):
 	def schedule_moves(Qstart,Pstart,Qtarg,Ptarg,anticollision_on):
          # generate initial move tables
          if anticollision_on:
-             move_tables_ideal = __schedule_with_anticollision(Qstart,Pstart,Qtarg,Ptarg)
+             move_tables_ideal = _schedule_with_anticollision(Qstart,Pstart,Qtarg,Ptarg)
          else:
-             move_tables_ideal = __schedule_without_anticollision(Qstart,Pstart,Qtarg,Ptarg)
+             move_tables_ideal = _schedule_without_anticollision(Qstart,Pstart,Qtarg,Ptarg)
          # use PosModel to get true moves (quantized into steps, and true timing estimate) and update the tables
          # re-run the anticollision check one more time
          return move_tables
          
      # internal methods    
-     def __schedule_without_anticollision(Qstart,Pstart,Qtarg,Ptarg):
+     def _schedule_without_anticollision(Qstart,Pstart,Qtarg,Ptarg):
          # convert all the Qs and Ps into local theta,phi
          # delta = finish - start
          # make move tables to do deltas
@@ -27,7 +27,7 @@ class PosScheduler(object):
          move_tables = None # replace with lots of real code
          return move_tables
 
-     def __schedule_with_anticollision(Qstart,Pstart,Qtarg,Ptarg):
+     def _schedule_with_anticollision(Qstart,Pstart,Qtarg,Ptarg):
          # anticollision algorithm goes here
          move_tables = None # replace with lots of real code
          return move_tables
