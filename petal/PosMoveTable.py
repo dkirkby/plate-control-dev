7d10e3f86d50d9f5e3a146df61562468245cb073
joesilber <jhsilber@lbl.gov>
2015-11-23 01:05:42 +0000

    moved the flags for final anti-backlash and final creep into being top level booleans in the module

--- petal/PosMoveTable.py
+++ petal/PosMoveTable.py
@@ -11,44 +11,47 @@ class PosMoveTable(object):
     """
     
     def __init__(self, posmodel=PosModel.PosModel()):
-        self.posmodel = posmodel  # the particular positioner this table applies to
-        self.__rows = [] # internal representation of the data
+        self.posmodel = posmodel       # the particular positioner this table applies to
+        self.final_backlash_on = True  # whether to do anti-backlash submoves on the final move
+        self.final_creep_on    = True  # whether to do precision creep submoves on the final move
+        self.__rows = [] # internal representation of the move data
+
         
     # getters
     def for_scheduler(self):
         """Version of the table suitable for move scheduling.
         Distances are given at the output shafts, in degrees.
         Times are given in seconds.
-        Does not include extra submoves.
         """
         # note to self... always return the quantized obs vals
-        
-    def for_scheduler_with_submoves(self):
-        """Simliar to for_scheduler, but before returning the table, any
-        rows with submoves for anti-backlash or final precision creep are
-        appropriately modified, and the extra submove rows are added.
-        """
-        # note to self... always return the quantized obs vals
+        #             ... always include the extra submoves if the top-level flags say so
         
     def for_hardware(self):
         """Version of the table suitable for the hardware side.
         Distances are given at the motor shafts, in discrete steps.
         Times are given in milliseconds.
-        Always includes any submoves for anti-backlash or final precision creep.
         """
-        
+        # note to self... organize the pauses so that there are only "postpauses", the way we're implementing the firmware
+        #             ... always include the extra submoves if the top-level flags say so
+
     # setters
-    def set_move_in_obs_degrees(self, rowidx, axisid, distance, speed_mode):
-        """If row index does not exist yet, then it will be added.
+    def set_move_in_obs_degrees(self, rowidx, axisid, distance):
+        """Scheduler uses this method to get move distances into the table.
+        If row index does not exist yet, then it will be added.
         """
         
     def set_move_in_motor_steps(self, rowidx, axisid, nsteps, speed_mode):
+        """PosModel uses this method to update existing move distances in the
+        table with their quantized values.
+        """
         
-    def set_pause(self, rowidx, axisid, pause):
+    def set_prepause(self, rowidx, axisid, prepause):
+        """For use by scheduler to get pauses before moves in.
+        """
         
-    def set_antibacklash_mode(self, rowidx, axisid, onoff):
-        
-    def set_finalcreep_mode(self, rowidx, axisid, onoff):
+    def set_postpause(self, rowidx, axisid, postpause):
+        """For use by scheduler to get pauses after moves in.
+        """
                 
     # row manipulations            
     def insert_row(self,index,posmoverow):
@@ -100,8 +103,9 @@ class PosMoveRow(object):
                 'speed_mode_P'       :'cruise', # ['cruise' or 'creep'] phi, whether to cruise (fast, coarse resolution) or creep (slow, fine resolution)
                 'speed_continuous_T' :self.posmodel.speed_cruise_T, # [deg/sec] ideal speed of theta rotation
                 'speed_continuous_P' :self.posmodel.speed_cruise_P, # [deg/sec] ideal speed of phi rotation
+                'prepause'           :0,        # [sec] delay for this number of seconds before executing the move
                 'move_time'          :0,        # [sec] time it takes the move to execute
-                'pause'              :0,        # [sec] delay for this number of seconds, after any moves have completed
+                'postpause'          :0,        # [sec] delay for this number of seconds after the move has completed
                 'add_antibacklash_T' :False,    # [bool] whether to add/replace this move with additional anti-backlash submoves
                 'add_antibacklash_P' :False,    # [bool] whether to add/replace this move with additional anti-backlash submoves
                 'add_final_creep_T'  :False,    # [bool] whether to add/replace this move with additional final creep submoves
