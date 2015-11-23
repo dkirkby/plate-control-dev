400faca88c1e0b97bfa5d12e85363a9b94a3d0f9
joesilber <jhsilber@lbl.gov>
2015-11-23 06:06:38 +0000

    split up the Ps and Qs

--- petal/PosScheduler.py
+++ petal/PosScheduler.py
@@ -1,33 +1,31 @@
 # imports here
 
 class PosScheduler(object):
-	"""Generates move schedules in local (th,ph) to get positioners from starts to finishes.
-	   We should get a clear definition of the move table format in here early."""
+    """Generates move table schedules in local (theta,phi) to get positioners
+    from starts to finishes. The move tables are instances of the PosMoveTable
+    class.
+    """
 
 	def __init__(self):
 		#maybe not necessary?
 		pass
 		
-	def schedule_moves(PQstart,PQtarg,anticollision_on):
-         # generate initial move tables
-         if anticollision_on:
-             move_tables_ideal = _schedule_with_anticollision(PQstart,PQtarg)
+	def schedule_moves(Pstart,Qstart,Ptarg,Qtarg,anticollision_mode_on):
+         if anticollision_mode_on:
+             move_tables = _schedule_with_anticollision(Pstart,Qstart,Ptarg,Qtarg)
          else:
-             move_tables_ideal = _schedule_without_anticollision(PQstart,PQtarg)
-         # use PosModel to get true moves (quantized into steps, and true timing estimate) and update the tables
-         # re-run the anticollision check one more time
+             move_tables = _schedule_without_anticollision(Pstart,Qstart,Ptarg,Qtarg)
          return move_tables
          
      # internal methods    
-     def _schedule_without_anticollision(PQstart,PQtarg):
+     def _schedule_without_anticollision(Pstart,Qstart,Ptarg,Qtarg):
          # convert all the (P,Q) into local (th,ph)
          # delta = finish - start
-         # make move tables to do deltas
-         # use PosModel to get true moves and update the tables
+         # use PosMoveTable to make move tables that do the deltas
          move_tables = None # replace with lots of real code
          return move_tables
 
-     def _schedule_with_anticollision(PQstart,PQtarg):
+     def _schedule_with_anticollision(Pstart,Qstart,Ptarg,Qtarg):
          # anticollision algorithm goes here
          move_tables = None # replace with lots of real code
          return move_tables
