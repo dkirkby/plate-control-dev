568a34ac9a5ad2433d024c1374de385d870727ff
joesilber <jhsilber@lbl.gov>
2015-11-23 00:38:26 +0000

    initial implementation sketch of a module that can uniformly handle all these move tables

---
+++ petal/PosMoveTable.py
@@ -0,0 +1,124 @@
+class PosMoveTable(object):
+    """A move table contains the information for a single positioner's move
+    sequence, in both axes. This object defines the move table structure, and
+    presents functions to view or convert the data in the several formats
+    required through the move scheduling pipeline.
+    
+    The internal representation of the table data should not be directly accessed
+    or modified, since there is some extra logic associated with correctly
+    appending, inserting, validating, etc. Use the provided setters / getters
+    instead.
+    """
+    
+    def __init__(self, posmodel=PosModel.PosModel()):
+        self.posmodel = posmodel  # the particular positioner this table applies to
+        self.__rows = [] # internal representation of the data
+        
+    # getters
+    def for_scheduler(self):
+        """Version of the table suitable for move scheduling.
+        Distances are given at the output shafts, in degrees.
+        Times are given in seconds.
+        Does not include extra submoves.
+        """
+        # note to self... always return the quantized obs vals
+        
+    def for_scheduler_with_submoves(self):
+        """Simliar to for_scheduler, but before returning the table, any
+        rows with submoves for anti-backlash or final precision creep are
+        appropriately modified, and the extra submove rows are added.
+        """
+        # note to self... always return the quantized obs vals
+        
+    def for_hardware(self):
+        """Version of the table suitable for the hardware side.
+        Distances are given at the motor shafts, in discrete steps.
+        Times are given in milliseconds.
+        Always includes any submoves for anti-backlash or final precision creep.
+        """
+        
+    # setters
+    def set_move_in_obs_degrees(self, rowidx, axisid, distance, speed_mode):
+        """If row index does not exist yet, then it will be added.
+        """
+        
+    def set_move_in_motor_steps(self, rowidx, axisid, nsteps, speed_mode):
+        
+    def set_pause(self, rowidx, axisid, pause):
+        
+    def set_antibacklash_mode(self, rowidx, axisid, onoff):
+        
+    def set_finalcreep_mode(self, rowidx, axisid, onoff):
+                
+    # row manipulations            
+    def insert_row(self,index,posmoverow):
+        if index >= 0:
+            if index > self.__nrows:  # fill in some blanks in-between
+                for i in range(index - self.__nrows - 1):
+                    self.append_row(self.blankrow())
+            self.__table.insert(index,posmove_row)
+            
+        else:
+            print "bad row index " + repr(index) + " for insertion"
+
+    def append_row(self,posmoverow):
+        self.insert(self.__nrows,posmoverow)
+        
+    def delete_row(self,index):
+        if index >= 0 and index < self.__nrows:
+            del self.__table[index]
+        else:
+            print "error: no row at index " + repr(index) + " to delete"
+
+    # internal helper methods
+    @property
+    def __nrows(self):
+        """For internal operations, not general use.
+        """
+        return len(self.__table)
+
+        
+class PosMoveRow(object):
+    """Essentially this is a wrapper around a dictionary. The purpose is the
+    validations and updates in using the "setval" method. Values should not be
+    set in any other way. The general user should not ever directly use the
+    internal values of a PosMoveRow instance, but rather should rely on the
+    higher level table forms exportable by PosMoveTable.
+    """
+    def __init__(self, posmodel=PosModel.PosModel()):
+        self._posmodel = posmodel
+        self._data = self.defaultrow()
+
+    def defaultrow():
+        return {'obs_dT_continuous'  :0,        # [deg] ideal theta distance to move (as seen by external observer)
+                'obs_dP_continuous'  :0,        # [deg] ideal phi distance to move (as seen by external observer)
+                'obs_dT_quantized'   :0,        # [deg] real theta distance, quantized and calibrated to a particular number of motor steps
+                'obs_dP_quantized'   :0,        # [deg] real phi distance, quantized and calibrated to a particular number of motor steps
+                'motor_steps_T'      :0,        # [-] number of steps the hardware should move the theta motor shaft
+                'motor_steps_P'      :0,        # [-] number of steps the hardware should move the phi motor shaft
+                'speed_mode_T'       :'cruise', # ['cruise' or 'creep'] theta, whether to cruise (fast, coarse resolution) or creep (slow, fine resolution)
+                'speed_mode_P'       :'cruise', # ['cruise' or 'creep'] phi, whether to cruise (fast, coarse resolution) or creep (slow, fine resolution)
+                'speed_continuous_T' :self.posmodel.speed_cruise_T, # [deg/sec] ideal speed of theta rotation
+                'speed_continuous_P' :self.posmodel.speed_cruise_P, # [deg/sec] ideal speed of phi rotation
+                'move_time'          :0,        # [sec] time it takes the move to execute
+                'pause'              :0,        # [sec] delay for this number of seconds, after any moves have completed
+                'add_antibacklash_T' :False,    # [bool] whether to add/replace this move with additional anti-backlash submoves
+                'add_antibacklash_P' :False,    # [bool] whether to add/replace this move with additional anti-backlash submoves
+                'add_final_creep_T'  :False,    # [bool] whether to add/replace this move with additional final creep submoves
+                'add_final_creep_P'  :False}    # [bool] whether to add/replace this move with additional final creep submoves
+
+    def setval(self,key,val):
+        """All value setting should be done via this method, so that validations
+        and updates to related fields can be performed."""
+        updates_list = []
+        if key == 'obs_dT_continuous' or key == 'obs_dP_continuous':
+            # update motor steps
+        if key == 'motor_steps_T' or key == 'motor_steps_P':
+            # update quantized vals
+        if key == 'obs_dT_quantized' or key == 'obs_dP_quantized':
+            # update move time
+        if key == 'speed_mode_T'
+            # update theta continuous speed
+        if key == 'speep_mode_P'
+            # update phi continuous speed
+        
