bb07ec70a2b730313e14a01f7e616da2fadca3b5
joesilber <jhsilber@lbl.gov>
2015-11-25 19:59:06 +0000

    added detailed description of the format of output from hardware_ready_move_tables. fixed some indents, missing colons, and formatting

--- petal/PosArrayMaster.py
+++ petal/PosArrayMaster.py
@@ -1,4 +1,6 @@
 import PosModel
+import PosScheduler
+import PosState
 
 class PosArrayMaster(object):
     """Orchestrates the modules to generate fiber positioner move sequences.
@@ -27,15 +29,42 @@ class PosArrayMaster(object):
 
         # now for the full array, pass all the start and target (P,Q) to PosScheduler,
         # (as well as calibration values). You get back move tables.
-		self.move_tables = PosScheduler.schedule_moves(Pstart,Qstart,Ptarget,Qtarget,anticollision)
+            self.move_tables = PosScheduler.schedule_moves(Ptarg,Qtarg,anticollision)
 			
-	def hardware_ready_move_tables(self):
-		"""Strips out information that isn't necessary to send to petalbox, and
-		formats for sending. Any cases of multiple tables for one positioner are
-        merged in sequence into a single table."""
+    def hardware_ready_move_tables(self):
+        """Strips out information that isn't necessary to send to petalbox, and
+        formats for sending. Any cases of multiple tables for one positioner are
+        merged in sequence into a single table.
+        
+        Output format:
+            List of dictionaries.
+            
+            Each dictionary is the move table for one positioner.
+            
+            The dictionary has the following fields:
+                {'posid':'','nrows':0,'motor_steps_T':[],'motor_steps_P':[],'speed_mode_T':[],'speed_mode_P':[],'move_time':[],'postpause':[]}
+            
+            The fields have the following types and meanings:
+
+                posid         ... string                    ... identifies the positioner by 'SERIAL_ID'
+                
+                nrows         ... unsigned integer          ... number of elements in each of the list fields (i.e. number of rows of the move table)            
+                
+                motor_steps_T or
+                motor_steps_P ... list of signed integers   ... number of motor steps to rotate 
+                                                                    ... motor_steps_X > 0 ... ccw rotation
+                                                                    ... motor_steps_X < 0 ... cw rotation
+                speed_mode_T or
+                speed_mode_P  ... list of strings           ... 'cruise' or 'creep'
+                
+                movetime      ... list of unsigned floats   ... estimated time the row's motion will take, in seconds, not including the postpause
+                
+                postpause     ... list of unsigned integers ... pause time after the row's motion, in milliseconds, before executing the next row
+                
+        """
         tbls = self.move_tables
         i = 0
-        while i < len(s):
+        while i < len(tbls):
             j = i + 1
             extend_list = []
             while j < len(tbls):
@@ -43,16 +72,16 @@ class PosArrayMaster(object):
                     extend_list.extend(tbls.pop(j))
                 else:
                     j += 1
-            for e in extend_list
+            for e in extend_list:
                 tbls[i].extend(e)
             i += 1        
         hw_tables = []
-		for m in tbls
+        for m in tbls:
             hw_tables.append(m.for_hardware())
         return hw_tables
-        
-	def postmove_cleanup(self):
-		"""Always call this after performing a set of moves, so that PosModel instances
+
+    def postmove_cleanup(self):
+        """Always call this after performing a set of moves, so that PosModel instances
         can be informed that the hardware move was done.
         """
         for m in self.move_tables:
