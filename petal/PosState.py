0d932b77055030eb8e875cfbb72865246e0d958a
joesilber <jhsilber@lbl.gov>
2015-11-26 16:47:24 +0000

    added change list of variable names

--- petal/PosState.py
+++ petal/PosState.py
@@ -10,3 +10,51 @@ class PosState(object):
     
     def __init__(self):
         pass
+
+# Parker, as I've been working the code into shape I've modified / removed
+# some of the state variables. These are the changes I've tracked:
+
+    # keep these    
+    'SERIAL_ID'
+    'BUS_ID'
+    'LENGTH_R1', 'LENGTH_R2'
+    'POLYN_T0', 'POLYN_T1', 'POLYN_T2'
+    'POLYN_P0', 'POLYN_P1', 'POLYN_P2'
+    'POLYN_X0', 'POLYN_X1'
+    'POLYN_Y0', 'POLYN_Y1'
+    'PHYSICAL_RANGE_T', 'PHYSICAL_RANGE_P'
+    'BACKLASH'
+    'PRINCIPLE_HARDSTOP_DIR_T', 'PRINCIPLE_HARDSTOP_DIR_P'
+    'ALLOW_EXCEED_LIMITS'
+    'LIMIT_SEEK_EXCEED_RANGE_FACTOR'
+    'SHOULD_DEBOUNCE_HARDSTOPS'
+    'BACKLASH_REMOVAL_BACKUP_FRACTION'
+    'CURR_SPIN_UP_DOWN', 'CURR_CRUISE', 'CURR_CREEP', 'CURR_HOLD_AFTER_CREEP'
+    'GEAR_T', 'GEAR_P'
+    
+    # rename these
+    'MOTOR_CW_DIR_T' --> 'MOTOR_CCW_DIR_T' # ... and change comments to 'counter-clockwise'
+    'MOTOR_CW_DIR_P' --> 'MOTOR_CCW_DIR_P' # ... and change comments to 'counter-clockwise'
+    'BACKLASH_REMOVAL_DIR_T' --> 'ANTIBACKLASH_FINAL_MOVE_DIR_T' # +1 or -1 ... remove cw / ccw statements in old comment
+    'BACKLASH_REMOVAL_DIR_P' --> 'ANTIBACKLASH_FINAL_MOVE_DIR_P' # +1 or -1 ... remove cw / ccw statements in old comment
+    'ONLY_CREEP_TO_LIMITS' --> 'CREEP_TO_LIMITS'
+    
+    # new ones
+    'DEVICE_ID'           # string, id number of location on the petal or plate
+    'PETAL_ID'            # string, id number of the petal or plate it is mounted on
+    'MOTOR_ID_T'          # 0 or 1, which motor number in firmware commands corresponds to theta
+    'MOTOR_ID_P'          # 0 or 1, which motor number in firmware commands corresponds to phi
+    'ONLY_CREEP'          # boolean, if true disable cruising speed
+    'FINAL_CREEP_ON'      # boolean, if true do a finishing creep move after a cruise
+    'BACKLASH_REMOVAL_ON' # boolean, if true do an antibacklash sequence at end of a move
+    'FINAL_CREEP_DIST'    # float, distance in deg to creep after a cruise
+    'PRINCIPLE_HARDSTOP_CLEARANCE_T' # float, minimum distance in deg to stay clear of theta principle hardstop
+    'SECONDARY_HARDSTOP_CLEARANCE_T' # float, minimum distance in deg to stay clear of theta secondary hardstop
+    'PRINCIPLE_HARDSTOP_CLEARANCE_P' # float, minimum distance in deg to stay clear of phi principle hardstop
+    'SECONDARY_HARDSTOP_CLEARANCE_P' # float, minimum distance in deg to stay clear of phi secondary hardstop
+    
+    # remove these
+    'HARDSTOP_CLEARANCE'
+    'BACKLASH_REMOVAL_ON_CW_MOVES'    
+    'BACKLASH_REMOVAL_ON_CCW_MOVES'
+    'FIRST_CMD_AXIS'
