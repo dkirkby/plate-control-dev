ee1c09effae04391ecbc5fc71db0e491d7fd2b62
parfa30 <parker.fagrelius@noirlab.edu>
2015-12-10 01:07:20 +0000

    validation

--- petal/PosState.py
+++ petal/PosState.py
@@ -1,10 +1,15 @@
+#############################
+# Classes: PosState         #
+# Version: Python 3         #
+# Date: Dec. 9, 2015        #
+# Author: P. Fagrelius      #
+#############################
 
-
-from __future__ import print_function
 import os, sys
 import datetime
 #import newdict
 from configobj import ConfigObj
+from validate import Validator
 import PosConstants
 
 
@@ -18,26 +23,43 @@ class PosState(object):
     single object.
     Values are stored in a config file.
     """
-
-    def __init__(self,pos_id):
+    def __init__(self,pos_id=None):
+        if pos_id is None:
+            self.pos_id = 'DEF'
+        else:
+            self.pos_id = str(pos_id)
+            
         self.verbose = PosConstants.verbose
         configfile = os.getcwd()+'/configfile.conf' #Need to decide where to put this
+        configspecfile = os.getcwd()+'/configspec.ini' # Validation file
         #configfile = os.environ.get('CONFPATH')+'configfile.conf'
-        self.config = ConfigObj(configfile)
-        self.pos_id = str(pos_id)
+        self.config = ConfigObj(configfile,configspec = configspecfile)
+
+        #Validate
+        validator = Validator()
+        try:
+            self.config.validate(validator)
+        except:
+            print('Something is wrong with the config file')
     
-    def read(self,*args):
+    def read(self,arg):
         """
         Returns current values of state variables as an array
         """
-        self.conf = []
-        try:
-            for arg in args:
-                self.conf.append(self.config[self.pos_id][arg])
-            return self.conf
-        except:
-            print('\n Error in reading Config file \n')
-            return False
+        if arg == 'GEAR_T':
+            gear_name = self.config[self.pos_id][arg]
+            self.value = PosConstants.gear_ratio[gear_name]
+        elif arg == 'GEAR_P':
+            gear_name = self.config[self.pos_id][arg]
+            self.value = PosConstants.gear_ratio[gear_name]
+        else: 
+            try:
+                self.value = self.config[self.pos_id][arg]
+                return self.value
+
+            except:
+                print('\n Error in reading Config file \n')
+                return False
 
     def write(self,arg,val):
         """
