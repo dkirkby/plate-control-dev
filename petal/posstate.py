5faab8fb10760c8d26802a6f03a8a9df9fe0a5ef
joesilber <jhsilber@lbl.gov>
2015-12-27 23:08:13 +0000

    in-progress updates, bringing into line with the more structured approach to unit vs general settings, and sub-sections of unit settings

--- petal/posstate.py
+++ petal/posstate.py
@@ -3,49 +3,74 @@ import configobj
 import posconstants
 
 class PosState(object):
-    """State variables for the positioner are generally stored, accessed,
+    """Variables for the positioner are generally stored, accessed,
     and queried through this class. The approach has been to put any
-    parameters which may vary from positioner in this
+    parameters which may vary from positioner to positioner in this
     single object.
 
-    INPUTS: pos_id = SERIAL_ID of positioner
-            config_name = section of the config file containing the particular value set to use
+    INPUTS: unit_id = SERIAL_ID of positioner
 
-    Note: There is a default positioner config file if no pos_id is supplied
-    Values are stored in a config file.
+    Notes:
+        Default settings are used if no unit_id is supplied.
+        Values are stored in configobj files.
+        There is a distinction between unit parameters (specific to one hardware
+        unit) and general parameters (settings which apply uniformly to many units).
     """
-    def __init__(self,pos_id=None,config_name='DEFAULT'):
-        configpath = os.getcwd()+'/pos_configs/'
-        if pos_id:
-            self.pos_id = str(pos_id)
-            configfile = configpath+'pos_' + str(self.pos_id) + '.conf'
-            self.config = configobj.ConfigObj(configfile,unrepr=True)
+    def __init__(self, unit_id=None):
+        directory = os.getcwd() + '/pos_settings/'
+        if unit_id:
+            filename = directory + 'unit_' + str(unit_id) + '.conf'
+            self.unit = configobj.ConfigObj(filename,unrepr=True)
         else:
-            configfile = configpath + '_pos_template.conf'        # read in the template file
-            self.config = configobj.ConfigObj(configfile,unrepr=True)
-            self.config.filename = configpath + '_pos_xxxxx.conf'  # now change file name so won't later accidentally overwrite the template
-            self.config.initial_comment = ['test file, not associated with a particular positioner',''] # also strip out and replace header comments specific to the template file
-            self.pos_id = self.config[config_name]['SERIAL_ID']
-        self.section = config_name
+            filename = directory + '_unit_settings_DEFAULT.conf'        # read in the template file
+            self.unit = configobj.ConfigObj(filename,unrepr=True)
+            self.unit.filename = directory + '_unit_settings_TEMP.conf'  # now change file name so won't later accidentally overwrite the template
+            self.unit.initial_comment = ['temporary test file, not associated with a particular positioner',''] # also strip out and replace header comments specific to the template file
+            self.unit.write()
+        filename = directory + self.unit['STATE']['GENERAL_SETTINGS_FILE']
+        self.genl = configobj.ConfigObj(filename,unrepr=True)
+        self.all_unit_keys = posstate.allkeys(self.unit)
+        self.all_genl_keys = posstate.allkeys(self.genl)
+
+    def read(self,key):
+        """Returns current value for a given key.
+        """
+        if key in self.all_unit_keys:
+            if key == 'GEAR_T' or key == 'GEAR_P':
+                gear_name = self.unit[''][key]
+                return posconstants.gear_ratio[gear_name]
+            else:
+                return self.unit[self.section][key]
+
+    def write(self,key,val,write_to_disk=None):
+        """Set a value.
+        In the default usage, there is an important distinction made between
+        unit parameters and general parameters:
+            unit    ... the new value is stored in memory, but is ALSO written to the file on disk
+            general ... the new value is only stored in memory, it is NOT written to disk
+        This default behavior can be overridden (True or False) using the write_to_disk boolean argument.
+        Caution should be exercised, since usually one does not a particular positioner to overwrite general settings.
+        """
+        self.config[self.section][key] = val
         self.config.write()
 
-    def read(self,arg):
+    def log_unit(self):
+        """All current unit parameters are written to the hardware unit's log file.
         """
-        Returns current values of state variables as an array
-        """
-        if arg == 'GEAR_T' or arg == 'GEAR_P':
-            gear_name = self.config[self.section][arg]
-            return posconstants.gear_ratio[gear_name]
-        else:
-            return self.config[self.section][arg]
-
-    def write(self,arg,val):
-        """
-        Change the values of the configuration file
-        """
-        self.config[self.section][arg] = val
-        self.config.write()
-
-    def log_config(self):
         pass
 
+    @staticmethod
+    def allkeys(confobj):
+        """Return list of all lowest-level keys (recurseively found to any depth) in a configobj structure.
+        """
+        if confobj.sections == []:
+            return confobj.keys()
+        else:
+            keyslist = []
+            for section in confobj.sections:
+                keyslist += allkeys(confobj[section])
+            return keyslist
+
+# MAYBE INSTEAD OF ALLKEYS, HAVE A FUNCTION THAT JUST RETURNS THE VALUE FOR A KEY AT ANY DEPTH? IF NO VALUE RETURNED, THEN IT WASN'T IN THERE
+    @staticmethod
+    def get_recursive(confobj,key)
