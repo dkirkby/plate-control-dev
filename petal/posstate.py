002f729de297fe59e98ecaebeb1bd1fdbd5619ba
joesilber <jhsilber@lbl.gov>
2015-12-29 08:15:28 +0000

    filled in some detail on recursive looking through unit configobj structure (though will probably flatten this in future version -- it's complicated and doesn't add much)

--- petal/posstate.py
+++ petal/posstate.py
@@ -1,6 +1,8 @@
 import os
 import configobj
-import posconstants
+import datetime
+import csv
+import posconstants as pc
 
 class PosState(object):
     """Variables for the positioner are generally stored, accessed,
@@ -16,61 +18,111 @@ class PosState(object):
         There is a distinction between unit parameters (specific to one hardware
         unit) and general parameters (settings which apply uniformly to many units).
     """
+
     def __init__(self, unit_id=None):
-        directory = os.getcwd() + '/pos_settings/'
-        if unit_id:
-            filename = directory + 'unit_' + str(unit_id) + '.conf'
-            self.unit = configobj.ConfigObj(filename,unrepr=True)
+        settings_directory = os.getcwd() + '/pos_settings/'
+        logs_directory = os.getcwd() + '/pos_logs/'
+        self.max_log_length = 5 # increase after debugging
+        if unit_id != None:
+            self.unit_basename = 'unit_' + str(unit_id)
+            unit_filename = settings_directory + self.unit_basename + '.conf'
+            self.unit = configobj.ConfigObj(unit_filename,unrepr=True)
         else:
-            filename = directory + '_unit_settings_DEFAULT.conf'        # read in the template file
-            self.unit = configobj.ConfigObj(filename,unrepr=True)
-            self.unit.filename = directory + '_unit_settings_TEMP.conf'  # now change file name so won't later accidentally overwrite the template
+            temp_filename = settings_directory + '_unit_settings_DEFAULT.conf'        # read in the template file
+            self.unit = configobj.ConfigObj(temp_filename,unrepr=True)
+            self.unit_basename = '_unit_settings_TEMP'
+            self.unit.filename = settings_directory + self.unit_basename + '.conf'  # now change file name so won't later accidentally overwrite the template
             self.unit.initial_comment = ['temporary test file, not associated with a particular positioner',''] # also strip out and replace header comments specific to the template file
             self.unit.write()
-        filename = directory + self.unit['STATE']['GENERAL_SETTINGS_FILE']
-        self.genl = configobj.ConfigObj(filename,unrepr=True)
-        self.all_unit_keys = posstate.allkeys(self.unit)
-        self.all_genl_keys = posstate.allkeys(self.genl)
+        genl_filename = settings_directory + self.unit['STATE']['GENERAL_SETTINGS_FILE']
+        self.genl = configobj.ConfigObj(genl_filename,unrepr=True)
+        all_logs = os.listdir(logs_directory)
+        self.unit_logs = [x for x in all_logs if self.unit_basename in x]
+        if self.unit_logs:
+            self.unit_logs.sort(reverse=True)
+            self.unit_latest_log = self.unit_logs[0]
+            #check if maxed out, if so add another fresh log (possibly include this in the log_unit method?)
+        else:
+            #add another fresh log (possibly include this in the log_unit method?)
+            pass
+        self.log_unit(note='initialization')
 
     def read(self,key):
         """Returns current value for a given key.
+        All sections of all configobj structures are searched to full-depth.
         """
-        if key in self.all_unit_keys:
+        val = posstate.get_fulldepth(self.unit,key)
+        if val:
             if key == 'GEAR_T' or key == 'GEAR_P':
-                gear_name = self.unit[''][key]
-                return posconstants.gear_ratio[gear_name]
-            else:
-                return self.unit[self.section][key]
+                return pc.gear_ratio[val]
+            return val
+        val = posstate.get_fulldepth(self.genl,key)
+        if not(val):
+            print('no key "' + repr(key) + '" found')
+            return None
+        return val
 
     def write(self,key,val,write_to_disk=None):
         """Set a value.
-        In the default usage, there is an important distinction made between
-        unit parameters and general parameters:
+        All sections of all configobj structures are searched to full-depth, to find where the key/value pair is.
+        In the default usage, there is an important distinction made between unit parameters and general parameters:
             unit    ... the new value is stored in memory, but is ALSO written to the file on disk
             general ... the new value is only stored in memory, it is NOT written to disk
         This default behavior can be overridden (True or False) using the write_to_disk boolean argument.
         Caution should be exercised, since usually one does not a particular positioner to overwrite general settings.
         """
-        self.config[self.section][key] = val
-        self.config.write()
+        if set_fulldepth(self.unit,key,val):
+            if write_to_disk != False:
+                self.unit.write()
+        elif set_fulldepth(self.genl,key,val):
+            if write_to_disk == True:
+                self.genl.write()
+        else:
+            print('value not set, because the key "' + repr(key) + '" was not found')
 
-    def log_unit(self):
+    def log_unit(self,note=''):
         """All current unit parameters are written to the hardware unit's log file.
         """
-        pass
+        timestamp = datetime.datetime.now().strftime(pc.timestamp_format)
+        # more to-do
+        #self.log_filename = os.path.splitext(self.unit.filename)[0] + '_log_' + somenumber + '.csv'
+        with open(self.log_filename, 'w') as csvfile:
+            some_flat_dict
+            fieldnames = some_flat_dict.keys()
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            #writer.writeheader() # first time only?
+            writer.writerow(some_flat_dict)
 
     @staticmethod
-    def allkeys(confobj):
-        """Return list of all lowest-level keys (recurseively found to any depth) in a configobj structure.
+    def get_fulldepth(confobj,key):
+        """Recursively look through full-depth of a configobj structure, and return
+        the corresponding value for the argued key. Returns None if no such key found.
         """
-        if confobj.sections == []:
-            return confobj.keys()
-        else:
-            keyslist = []
+        if key in confobj.keys():
+            val = confobj[key]
+        elif confobj.sections:
             for section in confobj.sections:
-                keyslist += allkeys(confobj[section])
-            return keyslist
+                val = posstate.get_fulldepth(confobj[section],key)
+                if val:
+                    break
+        else:
+            val = None
+        return val
 
-# MAYBE INSTEAD OF ALLKEYS, HAVE A FUNCTION THAT JUST RETURNS THE VALUE FOR A KEY AT ANY DEPTH? IF NO VALUE RETURNED, THEN IT WASN'T IN THERE
     @staticmethod
-    def get_recursive(confobj,key)
+    def set_fulldepth(confobj,key,val):
+        """Recursively look through full-depth of a configobj structure, and set
+        the corresponding value for the argued key. Returns True if the value was
+        set, False if not (i.e., because the key could not be found).
+        """
+        if key in confobj.keys():
+            confobj[key] = val
+            val_was_set = True
+        elif confobj.sections:
+            for section in confobj.sections:
+                val_was_set = posstate.set_fulldepth(confobj,key,val)
+                if val_was_set:
+                    break
+        else:
+            val_was_set = False
+        return val_was_set
