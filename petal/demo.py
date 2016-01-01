40e2fd06088f5f609a7e5bda5dbbf3b2f69af528
joesilber <jhsilber@lbl.gov>
2016-01-01 00:03:47 +0000

    added more test cases for coordinate systems qs, xy

--- petal/demo.py
+++ petal/demo.py
@@ -1,5 +1,6 @@
 import posarraymaster
 import legacypositionercomm
+import numpy as np
 
 """Demonstrator script for initializing / moving positioner.
 """
@@ -11,45 +12,75 @@ m = posarraymaster.PosArrayMaster(posids,configs)
 
 # Initialization of communications
 # These commands are very specific to the hacked-together LegacyPositionerComm.
-comm = legacypositionercomm.LegacyPositionerComm('COM6')
+comm = legacypositionercomm.LegacyPositionerComm('COM5')
 comm.master = m
 m.comm = comm
 
 # Demo script flags
-should_home = False
-should_move_QS_grid = False
-should_move_abs_tp = True
-should_move_rel_dtdp = True
+should_home      = False
+should_move_qs   = False
+should_move_dqds = False
+should_move_xy   = False
+should_move_dxdy = True
+should_move_tp   = False
+should_move_dtdp = False
 
 # Moves
 if should_home:
     m.expert_request_schedule_execute_moves(posids, ['homing']*len(posids), 0, 0)
 
-if should_move_QS_grid:
+if should_move_qs:
+    # starting with targets in (x,y) space, just to illustrate some transformatons, too
     posX = [3,  0, -3,  0] # target x in positioner local coordinates
     posY = [0,  3,  0, -3] # target y in positioner local coordinates
     Qtargs = []
     Stargs = []
-    for i in range(len(posids)):
-        trans = m.get(posids[i]).trans # coordinate transformer for the positioner
+    for posid in posids:
+        trans = m.get(posid).trans # coordinate transformer for the positioner
         obsXY_targs = trans.posXY_to_obsXY([posX,posY]) # in observer global XY coordinate system
         QStargs = trans.obsXY_to_QS(obsXY_targs) # in QS global coordinate system
         Qtargs.append(QStargs[0])
         Stargs.append(QStargs[1])
-        m.request_schedule_execute_moves([posids[i]]*len(Qtargs), Qtargs, Stargs)
+        m.request_schedule_execute_moves([posid]*len(Qtargs), Qtargs, Stargs) # immediate execution on this positioner
+
+if should_move_dqds:
+    dq = [ 0,  90, -180, 90]
+    ds = [ 3,  -3,    6,  6]
+    for posid in posids:
+        Qstart = m.expected_current_position(posid,'Q')
+        Sstart = m.expected_current_position(posid,'S')
+        Qtargs = (Qstart + np.cumsum(dq)).tolist()
+        Stargs = (Sstart + np.cumsum(ds)).tolist()
+        m.request_moves([posid]*(len(Qtargs)), Qtargs, Stargs) # delay execution until all positioners have been loaded with targets
+    self.schedule_moves() # could have an optional anticollision on/off flag as an argument
+    self.send_tables_and_execute_moves()
+
+if should_move_xy:
+    x = [3, 0, -6,  0, 0]
+    y = [0, 3,  0, -6, 0]
+    for posid in posids:
+        m.expert_request_schedule_execute_moves([posid]*len(x), ['xy']*len(x), x, y)
+
+if should_move_dxdy:
+    dx = [ 3,  3, -12,  6, 0]
+    dy = [ 0,  0,   0, -6, 6]
+    for posid in posids:
+        m.expert_request_moves([posids]*len(T), ['tp']*len(T), T, P)
+    m.schedule_moves()
+    m.send_tables_and_execute_moves()
 
 if should_move_abs_tp:
     T = [-90,  0, 90,   0]
     P = [ 90, 90, 90, 180]
-    for i in range(len(posids)):
-        m.expert_request_moves([posids[i]]*len(T), ['tp']*len(T), T, P)
+    for posid in posids:
+        m.expert_request_moves([posid]*len(T), ['tp']*len(T), T, P)
     m.schedule_moves()
     m.send_tables_and_execute_moves()
 
 if should_move_rel_dtdp:
     dT = [-30, 30,   0,  0, -30, 30]
     dP = [  0,  0, -30, 30, -30, 30]
-    for i in range(len(posids)):
-        m.expert_request_moves([posids[i]]*len(dT), ['dtdp']*len(dT), dT, dP)
+    for posid in posids:
+        m.expert_request_moves([posid]*len(dT), ['dtdp']*len(dT), dT, dP)
     m.schedule_moves()
     m.send_tables_and_execute_moves()
