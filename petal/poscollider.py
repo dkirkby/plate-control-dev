46fa975c43400d0b398c0b02ba005e80d3acdd20
joesilber <jhsilber@lbl.gov>
2016-01-11 01:36:15 +0000

    fleshing out positioner geometry definition. approach is to keep things simple, object-oriented (and slow). will use numba and other tricks later to make anti-collision calc fast, once it *works*

--- petal/poscollider.py
+++ petal/poscollider.py
@@ -1,13 +1,5 @@
 import numpy as np
-
-# Available geometry definitions (will select one version when initializing below)
-DESI_0899_v4 = {'keepoutP' : [[3.967, 3.918, 3.269, 1.712, 1.313, 0.000, -1.324, -2.106, -2.106, -1.324,  0.000,  1.313,  1.712,  3.269,  3.918],
-                              [0.000, 1.014, 1.583, 1.391, 1.959, 2.395,  1.959,  0.848, -0.848, -1.959, -2.395, -1.959, -1.391, -1.583, -1.014]],
-                'keepoutT' : [[ 0.814,  2.083,  2.613,  4.194,  4.893, -1.902, -2.007, -1.139, -0.170],
-                              [-3.236, -2.707, -2.665, -2.761, -1.168, -0.935, -2.665, -3.137, -3.332]],
-                'keepoutP_point0_idx' : 12,
-                'keepoutT_point0_idx' : 0
-                }
+import postransforms
 
 class PosCollider(object):
     """PosCollider contains geometry definitions for mechanical components of the
@@ -17,15 +9,61 @@ class PosCollider(object):
     See DESI-0899 for geometry specifications, illustrations, and kinematics.
     """
     def __init__(self):
-        geom = DESI_0899_v4
-        self.keepoutP = PosPoly(geom['keepoutP'], geom['keepoutP_point0_idx'])
-        self.keepoutT = PosPoly(geom['keepoutT'], geom['keepoutT_point0_idx'])
+        self.timestep = 0.1 # [sec] time increment for collision checking
+        self.posgeoms = []
 
+    def add_positioner(self, posmodel):
+        """Add a positioner to the collider.
+        """
+        self.posgeoms.append(PosGeom(posmodel))
 
-class PosPoly:
-    """Represents geometry envelope definitions for mechanical components of the
-    fiber positioner.
+    def refresh_geometry_configs(self):
+        """Update positioner geometry to latest values from config files.
+        """
+        for p in posgeoms:
+            p.refresh_from_config()
+
+    def collision_between(self):
+        """Searches for collisions in time and space between two polygon geometries
+        which are rotating according to argued move tables.
+        """
+        pass
+
+class PosGeom(object):
+    """Represents the mechancial geometry for a single positioner.
     """
+    def __init__(self, posmodel):
+        self.posmodel = posmodel
+        self.keepoutP = PosPoly([])
+        self.keepoutT = PosPoly([])
+        self.R1 = 3.0
+        self.R2 = 3.0
+        self.xy0 = np.array([0,0])
+        self.tp0 = np.array([0,0])
+        self.tp_ranges = np.array([[-np.pi,np.pi],[0,np.pi]])
+        self.refresh_from_config()
+
+    def refresh_from_config(self):
+        """Reads latest versions of all configuration file offset, range, and
+        polygon definitions, and updates the positioner geometries accordingly.
+        """
+        keepoutP_points = [self.posmodel.state.read('KEEPOUT_PHI_X'),   self.posmodel.state.read('KEEPOUT_PHI_Y')]
+        keepoutT_points = [self.posmodel.state.read('KEEPOUT_THETA_X'), self.posmodel.state.read('KEEPOUT_THETA_Y')]
+        keepoutP_start_idx = self.posmodel.state.read('KEEPOUT_PHI_PT0')
+        keepoutT_start_idx = self.posmodel.state.read('KEEPOUT_THETA_PT0')
+        self.keepoutP = PosPoly(keepoutP_points, keepoutP_start_idx)
+        self.keepoutT = PosPoly(keepoutT_points, keepoutT_start_idx)
+        self.R1 = self.posmodel.state.read('LENGTH_R1')
+        self.R2 = self.posmodel.state.read('LENGTH_R2')
+        self.xy0 = np.array([self.posmodel.state.read('OFFSET_X'), self.posmodel.state.read('OFFSET_Y')])
+        self.tp0 = np.array([self.posmodel.state.read('OFFSET_T'), self.posmodel.state.read('OFFSET_P')])
+        self.tp_ranges = np.array(posmodel.trans.shaft_ranges('targetable'))
+
+class PosPoly(object):
+    """Represents a polygonal envelope definition for a mechanical component of
+    the fiber positioner.
+    """
+
     def __init__(self, points, point0_index=0):
         points = np.array(points)
         self.points = np.append(points[:,np.arange(point0_index,len(points[0]))], points[:,np.arange(0,point0_index+1)], axis=1)
@@ -41,5 +79,6 @@ class PosPoly:
 # 2D rotation matrix constructors
 def rotmat2D_rad(angle):
     return np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+
 def rotmat2D_deg(angle):
     return rotmat2D_rad(np.deg2rad(angle))
