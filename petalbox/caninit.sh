0f4dd3466a0f37bb8f003b8a35140a009b50e6e2
schubnel <schubnel@umich.edu>
2016-03-28 17:07:07 +0000

    rearranged files

--- petalbox/caninit.sh
+++ petalbox/caninit.sh
@@ -3,11 +3,12 @@
 for i in `seq 0 2`;
 do
 	echo $i
-	check=ip link show can$i | grep brd
+	check="$(ip link show can$i | grep brd)"
+	echo "check:"$check
 	if ! [[ -z "${check// }" ]];
 	then
 		echo "detected SYSTEC on can$i"
-		sudo ip link set can$i type can bitrate 500000
-  		sudo ip link set can$i up 
+		ip link set can$i type can bitrate 500000
+  		ip link set can$i up 
 	fi
 done
