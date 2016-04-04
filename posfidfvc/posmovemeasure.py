ae8d8d0554bd1c241ab438d8c6bc53abc8232cd4
joesilber <jhsilber@lbl.gov>
2016-04-04 16:07:45 +0000

    in-progress implementation -- basic high-level commands are in there, needs filling in (and migrating from old matlab implementation) of implementation details, for example in the range seeking and arc measurement methods

---
+++ posfidfvc/posmovemeasure.py
@@ -0,0 +1,355 @@
+class PosMoveMeasure(object):
+    """Coordinates moving fiber positioners with fiber view camera measurements.
+    """
+    def __init__(self, petals, fvc):
+        self.petals = petals # list of petal object
+        self.fvc = fvc # fvchandler object
+##        self.p = positioner_object  # positioner object; initialization
+##        self.nom_xy_ref = []        # Nx2, nominal position(s) of reference fiber(s), [] means no ref fiber
+##        self.nom_xy_ctr = [0,0]     # for user convenience, PosMoveMeasure can keep it's own xy offset value so user sees things in a "centered" coordinate system
+##        self.n_ref_fibers = 1       # number of fixed reference fibers in FVC field of views
+##        self.meas_unit_scale = 1    # multiply fvc units (i.e. pixels) by this value to get real lengths (e.g. mm)
+##        self.ccd_angle = -90        # [deg] angle of CCD w.r.t. horizontal
+##        self.initial_nudge_dist = 2 # [mm] when determining which is the moving fiber and which are refs
+##        self.n_cycles = 0           # keeps count of non-tiny moves
+
+    def fiducials_on(self):
+        """Turn on all fiducials on all petals."""
+        for petal in self.petals:
+            petal.fiducials_on()
+
+    def fiducials_off(self):
+        """Turn off all fiducials on all petals."""
+        for petal in self.petals:
+            petal.fiducials_off()
+
+    def measure(self):
+        pass
+
+    def move_and_converge(self, pos_ids, targets, coordinates='QS', num_corr_max=2):
+        """Move positioners to target coordinates, then make a series of correction
+        moves in coordination with the fiber view camera, to converge.
+
+        INPUTS:     pos_ids      ... list of positioner ids to move
+                    targets      ... list of target coordinates of the form [[u1,v2],[u2,v2],...]
+                    coordinates  ... 'QS' or 'XY', identifying the coordinate system the targets are in
+                    num_corr_max ... maximum number of correction moves to perform on any positioner
+        """
+        pass
+
+
+    def move_measure(self, pos_ids, commands, values):
+        """Move positioners and measure output with FVC.
+
+        INPUTS:     pos_ids      ... list of positioner ids to move
+                    commands     ... list of move command arguments, see comments in petal.py
+                    targets      ... list of target coordinates of the form [[u1,v2],[u2,v2],...]
+        """
+        pos_ids_by_ptl = self.pos_data_listed_by_ptl(pos_ids,'POS_ID')
+        for petal in pos_ids_by_ptl.keys():
+            indexes = [pos_ids.index(x) for x in pos_ids if x in pos_ids_by_ptl[petal]]
+            these_pos_ids  = [pos_ids[i]  for i in indexes]
+            these_commands = [commands[i] for i in indexes]
+            these_values   = [values[i]    for i in indexes]
+            petal.request_targets(these_pos_ids, these_commands, these_values)
+            petal.schedule_send_and_execute_moves() # in future, do this in a different thread for each petal
+        self.measure()
+
+    def rehome(self,pos_ids='all'):
+        """Find hardstops and reset current known positions.
+        INPUTS:     pos_ids ... 'all' or a list of specific pos_ids
+        """
+        pos_ids_by_ptl = self.pos_data_listed_by_ptl(pos_ids,'POS_ID')
+        for petal in pos_ids_by_ptl.keys():
+            petal.request_homing(pos_ids_by_ptl[petal])
+            petal.schedule_send_and_execute_moves() # in future, do this in a different thread for each petal
+        self.measure()
+
+    def calibrate(self,pos_ids='all'):
+        """Find hardstops, then sweep a circle of points about theta and phi to measure
+        positioner center locations, R1 and R2 arm lengths, theta and phi offsets, and
+        then set all these calibration values for each positioner.
+        """
+        self.rehome(pos_ids)
+        self.measure_calibration_arc(pos_ids,'theta')
+        self.measure_calibration_arc(pos_ids,'phi')
+
+    def measure_calibration_arc(self,pos_ids='all',axis='theta'):
+        """Expert usage. Sweep an arc of points about axis ('theta' or 'phi')
+        on positioners identified by pos_ids. Measure these points with the FVC
+        and calculate calibration values.
+        """
+        pos_ids_by_ptl = self.pos_data_listed_by_ptl(pos_ids,'POS_ID')
+        pass
+
+    def measure_range(self,pos_ids='all',axis='theta'):
+        """Expert usage. Sweep several points about axis ('theta' or 'phi') on
+        positioners identified by pos_ids, striking the hard limits on either end.
+        Calculate the total available travel range. Note that for axis='phi', the
+        positioners must enter the collisionable zone, so the range seeking will
+        occur in several successive stages.
+        """
+        pos_ids_by_ptl = self.pos_data_listed_by_ptl(pos_ids,'POS_ID')
+        pass
+
+    def pos_data_listed_by_ptl(self, pos_ids='all', key=''):
+        """Returns a dictionary with keys = petal objects and values = lists of data
+        (the particular data to retrieve is identified by key) on each petal.  If
+        pos_ids argument == 'all', then each list contains all the data on that petal.
+        If pos_ids argument is a list of pos_ids, then in the returned dictionary, each list
+        contains only the data for those positioners which are in the intersection set of
+        pos_ids with that petal's positioner ids.
+        """
+        data_by_ptl = {}
+        for petal in self.petals:
+            all_pos_on_ptl = petal.pos.get(key='POS_ID')
+            if pos_ids == 'all':
+                these_pos_ids = all_pos_on_ptl
+            elif isinstance(pos_ids,list):
+                these_pos_ids = [p for p in pos_ids if p in all_pos_on_ptl]
+            else:
+                print('invalid argument ' + str(pos_ids) + ' for pos_ids')
+                these_pos_ids = []
+            pos_ids_by_ptl[petal] = petal.pos.get(these_pos_ids,key)
+        return pos_ids_by_ptl
+
+##    def measure_xy(self):
+##        """
+##        Make a fiber view camera measurement
+##        note: output data from fvc gets multiplied by argument
+##        meas_unit_scale and ccd angle rotated out
+##        """
+##        start_time = time.time()
+##        nom_xy_ref_temp = self.nom_xy_ref
+##        if not nom_xy_ref_temp: #MATLAB: if not(isempty(nom_xy_ref_temp))
+##            ccd_rot = PosConstants.rotmat2D(self.ccd_angle)
+##            nom_xy_ref_temp = (ccd_rot * nom_xy_ref_temp) #MATLAB: temp')'
+##        xy, xy_ref = fvc.fvc_measure_with_ref(nom_xy_ref_temp/self.meas_unit_scale)
+##        elapsed_time = time.time() - start_time
+##        xy = xy * self.meas_unit_scale
+##        xy_ref = xy_ref * self.meas_unit_scale
+##        ccd_rot = PosConstants.rotmat2D(-1 * self.ccd_angle)
+##        xy = (ccd_rot * xy) #MATLAB: xy')'
+##        xy_ref = (ccd_rot * xy_ref) #MATLAB: xy')'
+##        return xy, xy_ref, elapsed_time
+##
+##    def move(self, move_cmd, move_val_1, move_val_2):
+##        """
+##        Command positioner to move, but no fvc measurement made
+##        """
+##        rowid = self.p.move(move_cmd, move_val_1, move_val_2) #p is a positioner object; there is a move function in PositionerController
+##        small_move = 0.2
+##        if str_compare_nocase(move_cmd, 'rel_dxdy') & sqrt(math.pow(move_val_1, 2) + math.pow(move_val_2, 2)) < small_move: #MATLAB: ||
+##            #don't count as a cycle
+##            pass
+##        else:
+##            self.n_cycles = self.n_cycles + 1
+##        return rowid
+##
+##    def move_meas(self, move_cmd, move_val_1, move_val_2):
+##        """
+##        Command a positioner move, make the fvc measurement, and record
+##        Returns the rowid in the log table where values were recorded
+##        Returns 1x2 xy_trns, which is the measured xy minus the ref fiber
+##            location, then transformed to positioner center using state
+##            object's calibration values
+##        """
+##        rowid = self.move(move_cmd, move_val_1, move_val_2)
+##        xy_meas, xy_ref, meas_time = self.measure_xy
+##        xy_corr = xy_meas
+##        if not xy_ref: #MATLAB: if not(isempty(xy_ref))
+##            xy_corr = xy_corr - xy_ref[0,:] # assuming use one ref fiber #syntax concern: xy_ref[0,:]
+##        xy_corr = xy_corr - self.nom_xy_ctr
+##        xy_trns = self.p.axes.trans.obsXY_to_posXY(xy_corr) #MATLAB: corr')'
+##        #MATLAB: log_entries = containers.Map
+##        log_entries = {}
+##        log_entries['x_meas'] = xy_meas[0]
+##        log_entries['y_meas'] = xy_meas[1]
+##        log_entries['x_corr'] = xy_corr[0]
+##        log_entries['y_corr'] = xy_corr[1]
+##        log_entries['x_trns'] = xy_trns[0]
+##        log_entries['y_trns'] = xy_trns[1]
+##        for i in range(len(xy_ref)):
+##            log_entries['x_ref_' + str(i)] = xy_ref[i][0] #changed from xy_ref[i,0]
+##            log_entries['y_ref_' + str(i)] = xy_ref[i][1] #changed from xy_ref[i,1]
+##        log_entries['meas_time'] = meas_time
+##        log_entries['n_cycles'] = self.n_cycles
+##        self.p.state.add_kv_in_log(rowid, log_entries)
+##        return xy_trns, rowid
+##
+##    def move_meas_sub_note(self, move_cmd, move_val_1, move_val_2, submove, note):
+##        """
+##        same as move_meas but with additional fields for log
+##        submove         ... empty or a string
+##        note            ... empty or a string
+##        """
+##        xy_trns, rowid = self.move_meas(move_cmd, move_val_1, move_val_2)
+##        log_entries = {}
+##        if not submove: #MATLAB: if not(isempty(submove))
+##            log_entries['submove'] = submove
+##        if not note: #MATLAB: if not(isempty(note))
+##            log_entries['note'] = note
+##        self.p.state.add_kv_in_log(rowid, log_entries)
+##        return xy_trns, rowid
+##
+##    def identify_ref_fibers(self):
+##        """
+##        Nudge positioner forward/back to determine which fibers are reference fibers
+##        """
+##        ref_dist_tol = 0.02
+##        print('Nudge positioner to identify reference fiber(s).\n')
+##        print('Distance tol for identifying a fixed fiber is set to %g.\n',ref_dist_tol)
+##        self.p.move('abs_xy', 0, 0)
+##        self.p.move('rel_dxdy', self.initial_nudge_dist, 0)
+##        xy_init = self.measure_xy
+##        self.p.move('rel_dxdy', -self.initial_nudge_dist, 0)
+##        xy_test = self.measure_xy
+##        xy_ref = []
+##        for i in range(len(xy_test)): # MATLAB: for i = 1:size(xy_test,1)
+##            #MATLAB: test_dist = sqrt(sum((ones(size(xy_test,1),1)*xy_test(i,:)-xy_init).^2,2));
+##            test_dist = math.pow((np.ones(len(xy_test)) * xy_test[i,:] - xy_init),2) #syntax concern: xy_test[i,:]
+##            test_dist = np.asarray(test_dist)
+##            test_dist = np.sqrt(np.sum(test_dist,axis = 0)) #axis = 0 is col sum; axis = 1 is row sum
+##            test_dist = test_dist.tolist()
+##            if test_dist < ref_dist_tol: #MATLAB: if find(test_dist < ref_dist_tol)
+##                xy_ref.append(xy_test[i,:]) #syntax concern: xy_test[i,:]
+##                print('Refernece fiber detected at (x,y) = (%.3f,%.3f)\n', xy_test[i][0], xy_test[i][1]) #changed from: xy_test[i,0], xy_test[i,1]
+##        self.nom_xy_ref = xy_ref
+##        n_ref_fibers_found = len(xy_ref)
+##        #MATLAB:
+##            #if obj.n_ref_fibers ~= n_ref_fibers_found
+##                #error('Detected %i reference fiber(s), but expected %i',n_ref_fibers_found,obj.n_ref_fibers);
+##            #end
+##        if self.n_ref_fibers == n_ref_fibers_found:
+##            try:
+##                pass
+##            except ValueError:
+##                print('Detected %i reference fiber(s), but expected %i', n_ref_fibers_found, self.n_ref_fibers)
+##        self.n_ref_fibers = n_ref_fibers_found
+##        return
+##
+##    def circle_meas(self, axis_idx, tp_initial, final, tp_between, n_points, note):
+##        """
+##        measure a circle of points over the positioning range
+##            axis_idx     ... identifies whether to use theta (1) or phi (2)
+##            tp_initial   ... abs theta phi to start at
+##            final        ... abs ending value of the axis that's moving
+##            tp_between   ... in between measurements return to this position. if empty, just go straight to next target
+##            n_points     ... number of points to take
+##            note         ... will be included in log entries for all moves
+##
+##            xy_ctr       ... best fit circle center
+##            radius       ... best fit circle radius
+##            xy_meas      ... the points that were measured
+##            tp_abs_cmd   ... absolute tp of the points that were commanded to go to
+##        """
+##        #axis_idx is either 1 or 2
+##        #MATLAB: switch/case code
+##        if axis_idx == 1:
+##            tp_cmd = [np.linspace(tp_initial[0], final, num = n_points).tolist(), (tp_initial[1] * np.ones(n_points)).tolist()]
+##            axisname = 'theta'
+##        elif axis_idx == 2:
+##            tp_cmd = [(tp_initial[0] * np.ones(n_points)).tolist(), np.linspace(tp_initial[1], final, num = n_points).tolist()]
+##            axisname = 'phi'
+##        xy_meas = []
+##        for i in range(n_points):
+##            print('Measuring %s circle point %i of %i ...\n', axisname, i, n_points)
+##            if tp_between:
+##                self.move('abs_tp', tp_between[0], tp_between[1])
+##            xy_meas.append(self.move_meas_sub_note('abs_tp', tp_cmd[i,0], tp_cmd[i,1], [], note))
+##        xy_ctr, radius = fitcircle(xy_meas) #MATLAB: meas')
+##        xy_ctr = xy_ctr #MATLAB = ctr'
+##        return xy_ctr, radius, xy_meas, tp_cmd
+##
+##    def range_seek(self, axis_idx, note):
+##        """
+##        measure physical range of an axis
+##        """
+##        #axis_idx is either 1 or 2
+##        #MATLAB: switch/case code
+##        if axis_idx == 1:
+##            range_val = self.p.axes.positioning_range_T
+##            tp_initial = [min(range_val), min(self.p.axes.positioning_range_P)]
+##            meas_axis = [1,0]
+##            axisname = 'theta'
+##        elif axis_idx == 2:
+##            range_val = self.p.axes.positioning_range_P
+##            tp_initial = [math.mean(self.p.axes.positioning_range_T), min(range_val)]
+##            meas_axis = [0,1]
+##            axisname = 'phi'
+##        self.move('abs_tp',tp_initial[0],tp_initial[1])
+##        print('Measuring %s min limit...\n', axisname)
+##        xy = self.move_meas_sub_note('seek_limit', -meas_axis[0], -meas_axis[1], [], note)
+##        old_allow_exceed_limits = self.p.state.kv('ALLOW_EXCEED_LIMITS')
+##        self.p.state.kv('ALLOW_EXCEED_LIMITS') = 1 #python does not like this line; no others errors show until this is fixed
+##        for i in range(2):
+##            print('Measuring intermediate point %i of 2 during %s range seek...\n', i, axisname)
+##            test_steps[i] = 0.4 * diff(range_val)
+##            xy.append(self.move_meas_sub_note('rel_dtdp', test_steps[i] * meas_axis[0], test_steps[i] * meas_axis[1], [], note))
+##        self.p.state.kv('ALLOW_EXCEED_LIMITS') = old_allow_exceed_limits
+##        print('Measuring %s max limit...\n', axisname)
+##        xy.append(self.move_meas_sub_note('seek_limit', meas_axis[0], meas_axis[1], [], note))
+##        xy_ctr, radius = fitcircle(xy) #MATLAB: y')
+##        #MATLAB: xy_ctr = xy_ctr' ..... \_O_/
+##        '''unwrap and calculate range'''
+##        test_steps[i+1] = diff(range_val) - sum(test_steps)
+##        xy_centered[:,0] = xy[:,0] - xy_ctr[0] #might not be valid python syntax
+##        xy_centered[:,1] = xy[:,1] - xy_ctr[1] #might not be valid python syntax
+##        test_steps_abs = [tp_initial[axis_idx], tp_initial[axis_idx] + sum(test_steps)] #MATLAB: cumsum(test_steps)]'
+##        a_meas = PosMoveMeasure.unwrapped_meas_angle(test_steps_abs, xy_centered)
+##        meas_range = a_meas[-1] - a_meas[0]
+##        return meas_range, xy_ctr, radius, xy
+##
+##    def stability_meas(self, n_meas):
+##        """
+##        repeated measurements without moving to determine stability
+##        """
+##        xy = []
+##        xyref = []
+##        for i in range(n_meas):
+##            temp.xy, temp.xyref = self.measure_xy #not sure of the temp.__ name is kosher for python
+##            xy.append(temp.xy)
+##            xyref.append(temp.xyref)
+##        return xy, xyref
+##
+##    """Static Methods"""
+##    def multi_unwrapped_meas_angle(a_cmd, xy_meas, wrap_restart_idxs):
+##        a_meas_unwrapped = []
+##        for i in range(len(wrap_restart_idxs)):
+##            if i == len(wrap_restart_idxs):
+##                unwrap_range = range(wrap_restart_idxs[i], len[a_cmd]) #MATLAB: unwrap_range = wrap_restart_idxs(i):length(a_cmd); in python 3: list(range(#,#))
+##            else:
+##                unwrap_range = range(wrap_restart_idxs[i], (wrap_restart_idxs[i+1]-1))
+##            a_meas_unwrapped.append(PosMoveMeasure.unwrapped_meas_angle(a_cmd[unwrap_range],xy_meas[unwrap_range,:]))
+##        return a_meas_unwrapped
+##
+##    def unwrapped_meas_angle(a_cmd, xy_meas):
+##        cmd_steps = np.diff(np.asarray(a_cmd), n = 1)
+##        a = math.atan2(xy_meas[:,1],xy_meas[:,0]) #MATLAB: a = atan2d(xy_meas(:,2),xy_meas(:,1));
+##        da = np.diff(np.asarray(a))
+##        wrap_at_180 = list_compare(np.sign(cmd_steps).tolist(), np.sign(da).tolist())
+##        for i in range(len(wrap_at_180)):
+##            if wrap_at_180[i]:
+##                da_options = [360 - da[i], 360 + da[i], da[i] - 360, da[i] + 360]
+##                _, da_select = min(abs(da_options - cmd_steps[i]))
+##                da[i] = da_options[da_select]
+##        first_pt_wrap_cutoff = 340
+##        if a[1] - a_cmd[1] > first_pt_wrap_cutoff: #checking for wrapping of first point
+##            a[1] = a[1] - 360
+##        elif a[1] - a_cmd[1] < -first_pt_wrap_cutoff:
+##            a[1] = a[1] + 360
+##        a_meas_unwrapped.append(a[1] + sum(da)) # MATLAB: a_meas_unwrapped = [a(1);a(1)+cumsum(da)];
+##        return a_meas_unwrapped
+##
+##    def make_splinefit(cmd, meas, n_pieces, order):
+##        if not n_pieces:
+##            n_pieces = ceil(len(cmd)/order) #add better logic if technique proves generally useful
+##        pp = splinefit(cmd, meas, n_pieces, order, 'r')
+##        return pp
+##
+##
+##
+##
+##
+##
+##
