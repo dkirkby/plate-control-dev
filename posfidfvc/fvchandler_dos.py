45f94876ae3e44cb5d919e12c6f40d02cc4c9c72
daqiii <honscheid.1@osu.edu>
2016-07-25 22:09:32 +0000

    updates

--- posfidfvc/fvchandler_dos.py
+++ posfidfvc/fvchandler_dos.py
@@ -26,18 +26,23 @@ class FVCHandler(object):
     """
     def __init__(self, fvc_type='SBIG'):
         self.fvc_type = fvc_type # 'SBIG' or 'FLI'
+        self.dos_fvc = {'pyro_uri' : None, 'proxy':None, 'uid':None}
         if self.fvc_type == 'SBIG':
             self.sbig = sbig_grab_cen.SBIG_Grab_Cen()
         elif self.fvc_type == 'FLI':
             # In case an instance is not running, do not count on a name server
             # Fine the FVC through the advertising instead
-            self.dos_fvc = {'proxy':None, 'uid':None}
-            seeker = Seeker('-dos-', 'DOStest', found_callback=self._dos_seeker_callback)
-            seeker.seek()
-            print('Seeking FVC...')
-            while self.dos_fvc['proxy'] == None:
-                time.sleep(1)
-            print('Found FVC')
+            self.seeker = Seeker('-dos-', 'DOStest', found_callback=self._dos_seeker_callback)
+            self.seeker.seek()
+            delay = 4.0
+            self.seeker_thread = threading.Thread(target=self._repeat_seeker, 
+                                                  kwargs={'delay':delay})
+            self.seeker_thread.setDaemon(True)
+            self.seeker_thread.start()
+            if self.dos_fvc['proxy'] == None:
+                print('Looking for FVC app in the background')
+            else:
+                print('Found FVC')
         elif self.fvc_type == 'simulator':
             self.sim_err_max = 0.1
             print('FVCHandler is in simulator mode with max errors of size ' + str(self.sim_err_max) + '.')
@@ -45,7 +50,63 @@ class FVCHandler(object):
         self.scale = 1.0         # scale factor from image plane to object plane
         self.translation = [0,0] # translation of origin within the image plane
 
-    def measure_and_identify(self, target_dict):
+        self.fid_dict = {1100: {'x': -71.3107,'y': 145.9311,'mag': 0.000, 'meas_err': 1.000,'flags':8},
+                         1118: {'x': -71.3222,'y': 104.0277,'mag': 0.000, 'meas_err': 1.000,'flags':8},
+                         1102: {'x': -80.5255,'y': 166.9440,'mag': 0.000, 'meas_err': 1.000,'flags':8},
+                         1103: {'x': -80.4125,'y': 83.0324,'mag': 0.000, 'meas_err': 1.000,'flags':8},
+                         1117: {'x': -89.4959,'y': 135.5507,'mag': 0.000, 'meas_err': 1.000,'flags':8},
+                         1106: {'x': -89.4696,'y': 114.5588,'mag': 0.000, 'meas_err': 1.000,'flags':8},
+                         1107: {'x': -107.8175,'y': 166.9728,'mag': 0.000, 'meas_err': 1.000,'flags':8},
+                         1108: {'x': -107.7875,'y': 124.9432,'mag': 0.000, 'meas_err': 1.000,'flags':8},
+                         1116: {'x': -107.8036,'y': 83.0034,'mag': 0.000, 'meas_err': 1.000,'flags':8},
+                         1115: {'x': -125.0158,'y': 180.9813,'mag': 0.000, 'meas_err': 1.000,'flags':8},
+                         1111: {'x': -125.0252,'y': 68.8910,'mag': 0.000, 'meas_err': 1.000,'flags':8},
+                         1112: {'x': -149.9411,'y': 181.0755,'mag': 0.000, 'meas_err': 1.000,'flags':8},
+                         1113: {'x': -160.0506,'y': 69.0207,'mag': 0.000, 'meas_err': 1.000,'flags':8},
+                         1114: {'x': -185.0396,'y': 114.9531,'mag': 0.000, 'meas_err': 1.000,'flags':8}}
+
+    # Internal callback and utility functions
+    def _repeat_seeker(self, delay = 4.0):
+        while True:
+            self.seeker.seek()
+            time.sleep(delay)
+            
+    def create_target_dict(self, expected_pos_xy):
+        """creates a target dict to send to fvc handler
+        INPUT: expected xy positions of positioners in [[x1,y1],[x2,y2],..]
+
+        Written by PAF for the FLI"""
+
+        target_dict = self.fid_dict
+        for i,n in enumerate(expected_pos_xy):
+            id = int(3000+i)
+            target_dict[id] = {'x':n[0],'y':n[1],'mag':0.000,'meas_err':1.000,'flags':4}
+
+        return target_dict
+
+    def measured_xy_from_fvc_centroid(self,center_dict):
+        """reformats the centroid dictionary from the fvc to a list of measured [x,y]
+        centroids for the positioner software
+
+        written by PAF for the FLI""" 
+
+        measured_pos_xy = []
+        measured_ref_xy = []
+        for n in center_dict:
+            if center_dict[n]['flag']==5.0:
+                measured_pos_xy.append([center_dict[n]['x'],center_dict[n]['y']])
+            elif center_dict[n]['flag']==3.0:
+                measured_pos_xy.append([center_dict[n]['x'],center_dict[n]['y']])
+                print(center_dict[n],'This was flagged as a 3')
+            elif center_dict[n]['flag']==8.0:
+                measured_ref_xy.append([center_dict[n]['x'],center_dict[n]['y']])
+            else:
+                print(center_dict[n]['flag'],type(center_dict[n]['flag']),'Doesnt seem to be a fiber or positioner') 
+
+
+        return measured_pos_xy, measured_ref_xy
+
+    def measure_and_identify(self,expected_pos_xy,expected_ref_xy):
         """Calls for an FVC measurement, and returns a list of measured centroids.
         The centroids are in order according to their closeness to the list of
         expected xy values.
@@ -61,25 +122,48 @@ class FVCHandler(object):
         Lists of xy coordinates are of the form [[x1,y1],[x2,y2],...]
         """
         if self.fvc_type == 'simulator':
-#            sim_errors = np.random.uniform(-self.sim_err_max,self.sim_err_max,np.shape(expected_pos_xy))
-#            measured_pos_xy = (expected_pos_xy + sim_errors).tolist()
-#            measured_ref_xy = expected_ref_xy
-            measured_pos_xy = []
-            measured_ref_xy = []
+            sim_errors = np.random.uniform(-self.sim_err_max,self.sim_err_max,np.shape(expected_pos_xy))
+            measured_pos_xy = (expected_pos_xy + sim_errors).tolist()
+            measured_ref_xy = expected_ref_xy
         elif self.fvc_type == 'FLI':
-            self.exptime = 1 #sec
-            # 1. pass expected_pos_xy (in mm at the focal plate) thru platemaker to get expected_pos_xy (in pixels at the FVC CCD)          
-            # For now, just need to pass the FVC a list of expected positions, then tell is to take an exposure and measure centroids, then return those.
-            fvc = self.dos_fvc['proxy']
-            print(target_dict)
-            fvc.set_target_dict(target_dict) # 2. tell FVC software where we expect the positioner centroids to be so it can identify positioners
-            fvc.set(exptime=self.exptime)
-            fvc.measure() # 3. use DOS commands to ask FVC to take a picture
-            measured_pos_xy = fvc.get_all_centers() # 4. use DOS commands to get the centroids list
-            measured_ref_xy = []
-
+            # 1. pass expected_pos_xy (in mm at the focal plate) thru platemaker to get expected_pos_xy (in pixels at the FVC CCD)
+            # 2. tell FVC software where we expect the positioner centroids to be so it can identify positioners
+            # 3. use DOS commands to ask FVC to take a picture
+            # 4. use DOS commands to get the centroids list
             # 5. send centroids (in pixels at FVC) thru platemaker to get measured xy (in mm at focal plate)
             # 6. organize those centroids so you can return them as measured_pos_xy, measured_ref_xy
+            self.exptime = 1 #sec
+            # current set of FVC commands
+            #target_dict = self.create_target_dict(expected_pos_xy)
+            #fvc_uri = 'PYRO:FVC@131.243.51.74:40539'
+            #fvc = Pyro4.Proxy(fvc_uri)
+            #fvc.set(exptime=self.exptime)
+            #fvc.set_target_dict(target_dict) 
+            #fvc.print_targets()
+            # proposed set of FVC commands
+            try:
+                fvc = self.dos_fvc['proxy']
+                fvc.set(exptime=self.exptime)
+            except Exception as e:
+                self.error('messure_and_identify: Exception communicating with the FVC: %s' % str(e))
+                self.dos_fvc['uid'] = None
+                self.seeker.seek()
+                try:
+                    fvc = self.dos_fvc['proxy']
+                    fvc.set(exptime=self.exptime)
+                except Exception as e:
+                    rstring = 'measure_and_identify: Exception  attempting to reset FVC connection: %s' % str(e))
+                    self.error(rstring)
+                    self.dos_fvc['uid'] = None
+                    return 'FAILED: ' + rstring
+            fvc.set_target_dict(expected_pos_xy)
+                
+            #fvc.calibrate_image()
+            fvc.measure() 
+            measured_dict = fvc.get("centers")
+            print(measured_dict)
+            measured_pos_xy,measured_ref_xy = self.measured_xy_from_fvc_centroid(measured_dict)	
+            return measured_pos_xy, measured_ref_xy
         else:
             expected_xy = expected_pos_xy + expected_ref_xy
             num_objects = len(expected_xy)
@@ -151,6 +235,7 @@ class FVCHandler(object):
                     return # Already have a connection
                 proxy = Pyro4.Proxy(value['pyro_uri'])
                 self.dos_fvc['proxy'] = proxy
+                self.dos_fvc['pyro_uri'] = value['pyro_uri']
                 self.dos_fvc['uid'] = value['uid']
 
     @staticmethod
