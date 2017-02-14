6f8bb968c682b7cfe5a1f70b91a51c32c59cf156
schubnel <schubnel@umich.edu>
2017-02-14 01:58:08 +0000

    reduced pfp_utulity to reading FW version and SI ID only

--- petalbox/pfp_utility.py
+++ petalbox/pfp_utility.py
@@ -18,7 +18,7 @@ import os
 
 can_frame_fmt = "=IB3x8s"
 os.system('cansend ' + str(sys.argv[1]) + ' 004e2080#00')
-sleept = 0.5
+sleept = 0.2
 
 class _read_key:
     def __init__(self):
@@ -98,83 +98,30 @@ class PositionerControl(object):
 	
 if __name__ == '__main__':
 	canchan=sys.argv[1]
-	_sel=_read_key()
-	print("")
-	print(" ID programming requires a single positioner or fiducial on the CAN bus")
-	print("")	
-	print(" (Using CANbus "+str(canchan)+")")
-	print("")	
+	can_id=int(sys.argv[2])
+ 
+	pmc=PositionerControl(canchan)
+	pmc.set_mode(can_id)
+	time.sleep(sleept)
+	try:
+		posid, data=pmc.get_can_address(can_id)
+	except:
+		posid = 'READ ERROR'	
+	print ("   CAN ID: ", posid)
+	try:
+		posid,sid=pmc.get_sid(can_id)
+		sid_str= ":".join("{:02x}".format(c) for c in sid)
+	except:
+		sid_str='READ ERROR'
+	print ("    Si ID: ",sid_str)
+	try:
+		posid,fw=pmc.get_firmware_version(can_id)
+		if len(fw) == 1:
+			fw=str(int(ord(fw))/10)
+		else:			
+			fw=str(int(str(fw[1]),16))+"."+str(int(str(fw[0]),16))
+	except:
+		fw='unknown'
+	print ("  FW rev.: ", fw)
 
-	loop=True
-	while loop:
-#		print("Select:")
-		print("[b]link LED (using broadcast address)")
-		print("[r]ead CAN address, silicon ID and software revision")
-		print("[e]xit")
-		print("[p]rogram new CAN address")
-		print("Select: ")
 		
-		sel=_sel.__call__()		
-		#print("sel:",sel)
-
-		sel=sel.lower()
-		if sel=='e':
-			print ("Bye...")
-			sys.exit()
-
-		canchan=sys.argv[1]
-#		print('CAN channel selected: '+canchan)
-		can_id=int(sys.argv[2])
-	 
-		pmc=PositionerControl(canchan)
-
-		if sel=='b':
-			pmc.set_mode(can_id)
-			time.sleep(sleept)	
-			for i in range(8):
-				pmc.set_reset_led(can_id,'on')
-				time.sleep(0.1)
-				pmc.set_reset_led(can_id,'off')
-
-		if sel=='r':
-		#print (" Sending command 21 - read CAN address")
-			pmc.set_mode(can_id)
-			time.sleep(sleept)
-			posid, data=pmc.get_can_address(can_id)
-			print ("   CAN ID: ", posid)
-		#print(" Sending command 19 - read sid short")
-			posid,sid=pmc.get_sid(can_id
-)
-			sid_str= ":".join("{:02x}".format(c) for c in sid)
-			print ("    Si ID: ",sid_str)
-			try:
-				posid,fw=pmc.get_firmware_version(can_id)
-				if len(fw) == 1:
-					fw=str(int(ord(fw))/10)
-				else:
-					
-					fw=str(int(str(fw[1]),16))+"."+str(int(str(fw[0]),16))
-		
-			except:
-				fw='unknown'
-			print ("  FW rev.: ", fw)
-
-		if sel=='p':
-			pmc.set_mode(can_id)
-			time.sleep(sleept)
-			new_id=input("Enter new CAN address (in decimal): ")
-			new_id=int(new_id)
-			new_id=(hex(new_id)).replace('0x','')			
-			new_id=new_id.zfill(4)
-			posid,sid=pmc.get_sid(can_id)
-			sid=flip_byteorder(sid)
-			pmc.check_sid_short(can_id,sid)
-			#print (" check sid short okay ")
-			#print (" Sending command 20 - write CAN address")
-			can_address=new_id # '000E' 
-			print ("Writing new CAN address ...")
-			try:
-				pmc.write_can_address(brdcast_id,can_address)
-				print ("Writing new CAN address okay ")
-			except:
-				print ("Writing new CAN address failed ...")
