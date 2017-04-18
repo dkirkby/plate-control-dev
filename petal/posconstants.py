ba29d51954a2cb10a5619017b40d369adce0cc57
joesilber <jhsilber@lbl.gov>
2017-04-18 00:52:37 +0000

    check whether temp files directory exists and if not, make one

--- petal/posconstants.py
+++ petal/posconstants.py
@@ -36,6 +36,8 @@ test_settings_directory      = all_settings_directory + os.path.sep + 'test_sett
 collision_settings_directory = all_settings_directory + os.path.sep + 'collision_settings' + os.path.sep
 hwsetups_directory           = all_settings_directory + os.path.sep + 'hwsetups' + os.path.sep
 temp_files_directory         = os.environ.get('HOME') + os.path.sep + 'fp_temp_files' + os.path.sep
+if not(os.path.exists(temp_files_directory)):
+    os.mkdir(temp_files_directory)
 
 # Mapping of radial coordinate R to pseudo-radial coordinate S (distance along focal surface from optical axis)
 R2Spoly = [5.00010E-01,9.99997E-01,1.91532E-07,1.72104E-09,7.31761E-11,-5.78982E-13,3.30271E-15,-1.11245E-17,1.90376E-20,-1.26341E-23]
