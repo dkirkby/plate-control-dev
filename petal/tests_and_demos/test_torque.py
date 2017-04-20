21ad7f4a77da1d0de73e39539f16d3de72067168
carldobson-lbl <cadobson@lbl.gov>
2017-04-20 21:17:20 +0000

    Modified to create a csv file in the format Jeremy wants, and not to wait for cool down after the last move. -cad

--- petal/tests_and_demos/test_torque.py
+++ petal/tests_and_demos/test_torque.py
@@ -26,19 +26,28 @@ print(sys.path)
 import petalcomm
 
 # Edit the values below before each test
-Petal_Controller_Number=20
+
+# Michigan - Irena test
+# Petal_Controller_Number=20
+# CanBusID='can0'
+# CanID=208
+
+# LBL - CAD test
+Petal_Controller_Number=10
 CanBusID='can0'
-CanID=208
+CanID=27
+
 Currents=[100,100,100,0]    # percentages for spin-up,cruise,creep,hold
 Torque_Moves=[
     #'motor','speed','direction','angle','cool_down_seconds'
-    ['phi'  ,'creep' ,'cw'  ,90,  120],
-    ['phi'  ,'creep' ,'ccw' ,90,  120]
+    ['phi'  ,'creep' ,'ccw' ,90,  120],
+    ['phi'  ,'creep' ,'cw'  ,90,  120]
 ]
 
 # End of normally edited section
 
 
+n_mm_to_oz_in = 0.1416119
 TTLogFile="torque_test.csv"
 
 # indices into MoveTable list
@@ -86,7 +95,9 @@ class Operator_Torque_Test:
         if(4!=len(CurrentList)):
             print("CurrentList should be a list of 4 values!")
             return 1
-        print("CanID=",int(CanID))
+        operator_id=input("Enter Your Name: ")
+        motor_sn=input("Enter Motor SN: ")
+        test_voltage=input("Enter Test Voltage: ")
         can_ids=[CanID]
         can_bus_ids=[CanBusID]
         print("Checking ready_for_tables, can_bus_ids="+str(can_bus_ids)+", can_ids="+str(can_ids)) 
@@ -95,6 +106,9 @@ class Operator_Torque_Test:
         if(bool_val):
             print("Setting current: CanBusID="+str(CanBusID)+", CanID="+str(CanID))
             self.pcomm.set_currents(CanBusID,CanID,Currents,Currents)
+            response=input("Press Enter when ready for next move: ")
+            max_torque=[]
+            res_torque=[]
             for i in range(len(MoveTable)):
                 print("Moving: CanID="+str(CanID))
                 error = self.pcomm.move(CanBusID, CanID,MoveTable[i][iDirection],MoveTable[i][iMode],MoveTable[i][iMotor],MoveTable[i][iAngle])
@@ -102,15 +116,45 @@ class Operator_Torque_Test:
                 while(not ready):
                     time.sleep(1)
                     ready=self.pcomm.ready_for_tables(can_bus_ids,can_ids)
-                max_torque=input("Enter Max Torque: ")
-                res_torque=input("Enter Residual Torque: ")
-                self.logprint(str(MoveTable[i][iMotor])+","+str(MoveTable[i][iMode])+","+str(MoveTable[i][iDirection])+","+str(MoveTable[i][iAngle])+","+str(MoveTable[i][iCoolSecs])+","+str(max_torque)+","+str(res_torque))
+                max_torque.append(input("Enter Max Torque: "))
+                res_torque.append(input("Enter Settled Torque: "))
                 print("Waiting "+str(MoveTable[i][iCoolSecs])+" seconds for cool down")
-                time.sleep(int(MoveTable[i][iCoolSecs]))
                 if(i+1<len(MoveTable)):
+                    for j in range(int(MoveTable[i][iCoolSecs])):
+                        left=int(MoveTable[i][iCoolSecs])-j
+                        if((0!=j) and 0==(left%10)):
+                            print("Waiting "+str(left)+" seconds for cool down")
+                        time.sleep(1)
                     response=input("Press Enter when ready for next move: ")
                 else:
                     print("Finished")
+                    operator_notes=input("Enter any notes: ")
+                    spreadsheet_str =str(motor_sn)+"," 		   # motor serial number
+                    spreadsheet_str+=str(operator_id)+","          # operator id
+                    spreadsheet_str+=str(test_voltage)+","         # test_voltage
+                    spreadsheet_str+=str(MoveTable[0][iMotor])+"," # phi or theta
+                    spreadsheet_str+=str(MoveTable[0][iMode])+","  # creep or cruise
+                    spreadsheet_str+=str(MoveTable[i][iCoolSecs])+"," # cooldown seconds
+                    if(str(MoveTable[0][iDirection]).lower()=='ccw'):
+                        spreadsheet_str+=str(max_torque[0])+","			# max ccw torque in oz-in
+                        spreadsheet_str+=str(format(float(max_torque[0])/n_mm_to_oz_in,'.3f'))+","	# max ccw torque in n-mm
+                        spreadsheet_str+=str(res_torque[0])+","			# settled ccw torque in oz-in
+                        spreadsheet_str+=str(format(float(res_torque[0])/n_mm_to_oz_in,'.3f'))+","	# settled ccw torque in n-mm
+                        spreadsheet_str+=str(max_torque[1])+","			# max cw torque in oz-in
+                        spreadsheet_str+=str(format(float(max_torque[1])/n_mm_to_oz_in,'.3f'))+","	# max cw torque in n-mm
+                        spreadsheet_str+=str(res_torque[1])+","			# settled cw torque in oz-in
+                        spreadsheet_str+=str(format(float(res_torque[1])/n_mm_to_oz_in,'.3f'))+","	# settled cw torque in n-mm
+                    else:
+                        spreadsheet_str+=str(max_torque[1])+","			# max ccw torque in oz-in
+                        spreadsheet_str+=str(format(float(max_torque[1])/n_mm_to_oz_in,'.3f'))+","	# max ccw torque in n-mm
+                        spreadsheet_str+=str(res_torque[1])+","			# settled ccw torque in oz-in
+                        spreadsheet_str+=str(format(float(res_torque[1])/n_mm_to_oz_in,'.3f'))+","	# settled ccw torque in n-mm
+                        spreadsheet_str+=str(max_torque[0])+","			# max cw torque in oz-in
+                        spreadsheet_str+=str(format(float(max_torque[0])/n_mm_to_oz_in,'.3f'))+","	# max cw torque in n-mm
+                        spreadsheet_str+=str(res_torque[0])+","			# settled cw torque in oz-in
+                        spreadsheet_str+=str(format(float(res_torque[0])/n_mm_to_oz_in,'.3f'))+","	# settled cw torque in n-mm
+                    spreadsheet_str+=str(operator_notes)
+                    self.logprint(spreadsheet_str)
         else:
             print("PC"+int(self.pcnum)+" not ready, try again later")
         return 0
@@ -134,11 +178,14 @@ if __name__ == '__main__':
                 verbose=False
     
     
-    root =Tk()
+#   root =Tk()
+    root = 0
     ott = Operator_Torque_Test(root,Petal_Controller_Number)
     if(ott.pcomm.is_connected()):
         ott.torque_moves(CanID,Currents,Torque_Moves)
     else:
         print("Not connected to petal controller")
+#   root.withdraw()
+
 
 
