295be259d086c884c87907d2a8a5b1a27fbd8859
brandonfreud <brandonfreud@localhost>
2017-09-07 19:31:19 +0000

    Unfinished script for more efficient CAN ID programming

---
+++ xytest/posblast.py
@@ -0,0 +1,110 @@
+import sys
+import os 
+sys.path.append(os.path.abspath('../petal/'))
+sys.path.append(os.path.abspath('../../../positioner_logs/data_processing_scripts/'))
+import petalcomm
+import googlesheets
+import posdance
+
+canbus = 'can0'
+        
+if __name__ == '__main__':
+    
+    _sel = posdance._read_key()
+    loop = True
+    ptlID = input("Input Petal ID:")
+    pcomm = petalcomm.PetalComm(ptlID)
+    while loop:
+        print("[a]uto CAN ID programming")
+	print("[m]anual CAN ID programming")
+	print("[h]ardware setup update")
+        print("[e]xit")
+	print("Select: ")
+
+        choice = _sel.__call__()
+        choice = choice.lower()
+
+        if choice == 'a':
+            boardIDs = googlesheets.connect_to_sheet('SiliconIDs')
+            eshop = googlesheets.connect_to_sheet('FIPOS Board Production Log')
+            infodict = pc.get_posfid_info(canbus)
+
+            for info in infodict.values():
+                sid = info[3]
+                sid = sid.split(':')
+                sid = ''.join(sid)
+                sid_found = True
+                if googlesheets.check_for_value(boardIDs, "\"" + sid + "\""):
+                    boardID = googlesheets.read(boardIDs, "\"" + sid + "\"", "Board ID")
+                    desiredCAN = googlesheets.read(eshop, str(boardID), "Serial #")
+                    pc.set_canid(canbus, sid, desiredCAN)
+                    print("Programmed CAN ID " + str(desiredCAN))
+                else:
+                    print("Could not find " + sid + " in silicon ID log")
+
+       if choice == 'm':
+           poslist = []
+           print('Input all positioner IDs that you wish to go into the summary, and type \'done\' when finished')
+           
+           while posinput != 'done':
+               posinput = input('Positioner ID: ')
+               if len(posinput) < 6 and not posinput == 'done':
+                   newinput = 'M'
+                   for x in range(5 - len(posinput)):
+                       newinput += '0'
+                   posinput = newinput + posinput
+               elif not posinput == 'done':
+                   poslist.append(posinput)
+
+           sidlist = []
+
+           for posid in poslist:
+               
+               input("Please plug in positioner " + posid + " and press Enter.")
+           
+               for x in range(len(posid)):
+                   if not (posid[x] == 'M' or posid[x] == '0'):
+                       posid = str(posid[x+1::])
+                       break
+               
+               newsids = []
+               infodict = pc.get_posid_info(canbus)
+
+               for info in infodict.values():
+                   sid = info[3]
+                   sid = sid.split(':')
+                   sid = ''.join(sid)
+                   newsids.append(sid)
+                   for sid in sidlist:
+                       newsids.pop(sid)
+                   if len(newsids) == 1:
+                       sidlist.append(newsids[0])
+                   else:
+                       print('Error - expected . . .')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                       
+
+
+
+
+
+
