868a7731cc34b2a86acac76bf7d0c9b00096d543
igershko <igershko@localhost>
2018-01-02 05:20:10 +0000

    Further revision to loadpetal.py

--- xytest/loadpetal.py
+++ xytest/loadpetal.py
@@ -16,7 +16,7 @@ if "TEST_LOCATION" in os.environ and os.environ['TEST_LOCATION']=='Michigan':
 else:
 	sys.path.append(os.path.abspath('../petal/'))
 	sys.path.append(os.path.abspath('../posfidfvc/SBIG'))
-	sys.path.append(os.path.abspath('../../../positioner_logs/data_processing_scripts/'))
+	sys.path.append(os.path.abspath('../../positioner_logs/data_processing_scripts/'))
 import time
 
 import fvchandler
@@ -44,20 +44,20 @@ import googlesheets
 import time
 
 class LoadPetal(object):
-	def __init__(self,hwsetup_conf='',xytest_conf=''):
-		global gui_root
-		gui_root = tkinter.Tk()
-		
-		self.simulate = False
-		self.logfile='MoveGUI.log'
-		fvc_type='simulator'
+    def __init__(self,hwsetup_conf='',xytest_conf=''):  
+#        global gui_root
+#        gui_root = tkinter.Tk()
+#        gui_root.title='Move Controll for Petal '+str(self.ptl_id)
+	
+        self.simulate = False
+        self.logfile='LoadPetal.log'
+        fvc_type='simulator'
 
-		self.ptl_id=13
-		fidids=['F021']   
-		gui_root.title='Move Controll for Petal '+str(self.ptl_id)
-		self.pcomm=petalcomm.PetalComm(self.ptl_id)
-		self.mode = 0
-		#petalcomm.
+        self.ptl_id=13
+        fidids=['F021']   
+        self.pcomm=petalcomm.PetalComm(self.ptl_id)
+        self.mode = 0
+	#petalcomm.
    #     info=self.petalcomm.get_device_status()
         canbus='can0'
         self.bus_id=canbus
@@ -76,34 +76,37 @@ class LoadPetal(object):
         self.ptl = petal.Petal(self.ptl_id, self.posids, fidids, simulator_on=self.simulate, printfunc=self.logwrite)
         self.fvc = fvchandler.FVCHandler(fvc_type,printfunc=self.logwrite,save_sbig_fits=False)               
         self.m = posmovemeasure.PosMoveMeasure([self.ptl],self.fvc,printfunc=self.logwrite)
-        self.posschedule=posschedule.PosSchedule(self.ptl)
-        # Add target for each positioner
+
+#        self.posschedule=posschedule.PosSchedule(self.ptl)
+       # Add target for each positioner
         self.posmodels=self.ptl.posmodels
         for posid in self.posids:
-            self.posschedule.request_target(posid, 'posTP', 0., 180., log_note='') # extend every positioner
+            print('\n',posid)
+            self.ptl.schedule.request_target(posid, 'posTP', 0., 180., log_note='') # extend every positioner
+            print(len(self.ptl.schedule.requests))
         # rotate one positioner, check if it can avoid the obstacle?
-        for i in range(36):
-            self.posschedule.request_target(posid, 'posTP', (i+1)*10., 180., log_note='')
-        self.posschedule._schedule_with_anticollision() # Make move_tables
+        for i in range(36): # cannot request more than one target per positioner in a given schedule
+            self.ptl.schedule.request_target('M00100', 'posTP', (i+1)*10., 180., log_note='')
+        print(self.ptl.schedule.requests)
+        self.ptl.schedule._schedule_with_anticollision() # Make move_tables
+       
+        move_tables=self.ptl.schedule.move_tables
         
-        move_tables=self.posschedule.move_tables()
+#        self.ptl.send_move_tables()
         
-        print(move_tables)
-        self.ptl.send_move_tables()
-        
-        # Make an animation
-        self.ptl.collider.add_positioners(self.posmodels)
-        self.ptl.collider.posmodels=self.posmodels
-        possweep=poscollider.PosSweep()
-        possweep.fill_exact(init_obsTP,move_tables)
+#        # Make an animation
+#        self.ptl.collider.add_positioners(self.posmodels)
+#        self.ptl.collider.posmodels=self.posmodels
+#        possweep=poscollider.PosSweep()
+#        possweep.fill_exact(init_obsTP,move_tables)
         
 
-	def logwrite(self,text,stdout=True):
-		"""Standard logging function for writing to the test traveler log file.
-		"""
-		line = '# ' + pc.timestamp_str_now() + ': ' + text
-		with open(self.logfile,'a') as fh:
-			fh.write(line + '\n')
-		if stdout:
-			print(line)
+    def logwrite(self,text,stdout=True):
+        """Standard logging function for writing to the test traveler log file.
+        """
+        line = '# ' + pc.timestamp_str_now() + ': ' + text
+        with open(self.logfile,'a') as fh:
+            fh.write(line + '\n')
+        if stdout:
+            print(line)
 			
