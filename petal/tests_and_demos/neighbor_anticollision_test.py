36b0a993468eff79a27d43ff5662a70e360fefa6
zhangkai <zhangkai@localhost>
2018-08-08 18:47:14 +0000

    Some syntax fixes

--- petal/tests_and_demos/neighbor_anticollision_test.py
+++ petal/tests_and_demos/neighbor_anticollision_test.py
@@ -6,16 +6,16 @@ import sys
 sys.path.append('../')
 import petal
 
-posids = {'left': 'M00137', 'right': 'M00138'} #Change this
-posid_list = [posids['left'],posids['right']
+posids = {'left': 'M00131', 'right': 'M00137'} #Change this
+posid_list = [posids['left'],posids['right']]
 petalid = 48 #change this
-fifids = None
+fifids = []
 
 ptl = petal.Petal(petalid, posid_list, fifids)
 ptl.anticollision_default = 'adjust'
 
-case_1 = {'command':'obsTP','type': 'Type II', 'left': [0,0], 'right', [190,0]}
-case_2 = {'command':'obsTP','type': 'Interference', 'left': [0,0], 'right', [180,0]}
+case_1 = {'command':'obsTP','type': 'Type II', 'left': [0,0], 'right': [190,0]}
+case_2 = {'command':'obsTP','type': 'No', 'left': [0,145], 'right': [180,45]}
 
 def assemble_requests(case):
     log_note = case['type'] + ' collision test'
@@ -30,15 +30,18 @@ def move(requests):
     
 if __name__ == '__main__':
     sleep_time = 5
-    for i in range(3):
-        ptl.request_rehoming(posids)
+    for i in range(2):
+        print('rehoming')
+        ptl.request_homing(posid_list)
         time.sleep(sleep_time)
-        request = assemble_requests(case1)
-        print(request['log_note'])
+        request = assemble_requests(case_1)
+        print(request[posid_list[0]]['log_note'])
+        move(request)
         time.sleep(sleep_time)
-        ptl.request_rehoming(posids)
+        print('rehoming')
+        ptl.request_homing(posid_list)
         time.sleep(sleep_time)
-        request = assemble_requests(case2)
-        print(request['log_note'])
+        request = assemble_requests(case_2)
+        print(request[posid_list[0]]['log_note'])
+        move(request)
         time.sleep(sleep_time)
-        
