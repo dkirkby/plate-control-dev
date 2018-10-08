b2844bf3ee9388f4c117663945b9455226a19f88
zhangkai <zhangkai@localhost>
2018-10-08 23:46:09 +0000

    A working instrmaker.py

--- posfidfvc/instrmaker.py
+++ posfidfvc/instrmaker.py
@@ -12,17 +12,17 @@ class InstrMaker(object):
     metrology data -- they are the nominal center positions of each device at
     the aspheric focal surface.
     """
-    def __init__(self,plate_type,ptl,m,fvc,hwsetup):
-        if plate_type == 'petal':
-            self.file_metro=os.getenv('FP_SETTINGS_DIR')+'/hwsetups/EMPetal_XY.csv'
+    def __init__(self,ptl,m,fvc,hwsetup):
+        if ptl.shape == 'petal':
+            self.file_metro=os.getenv('FP_SETTINGS_PATH')+'/hwsetups/Petal_Metrology.csv'
             # import the positioner locations file
-        elif plate_type == 'small_array':
-            self.file_metro=os.getenv('FP_SETTINGS_DIR')+'/hwsetups/SWIntegration_XY.csv'
+        elif ptl.shape == 'small_array':
+            self.file_metro=os.getenv('FP_SETTINGS_PATH')+'/hwsetups/SWIntegration_XY.csv'
             # import the small array block locations file
         else:
-            self.file_metro=os.getenv('FP_SETTINGS_DIR')+'/hwsetups/SWIntegration_XY.csv'
+            self.file_metro=os.getenv('FP_SETTINGS_PATH')+'/hwsetups/SWIntegration_XY.csv'
 
-        self.PFS_file=os.getenv('FP_SETTINGS_DIR')+'/hwsetups/PFS_ID_Map.csv' 
+        self.PFS_file=os.getenv('FP_SETTINGS_PATH')+'/hwsetups/PFS_ID_Map.csv' 
         self.m=m 
         self.ptl=ptl
         self.hwsetup=hwsetup
@@ -34,7 +34,10 @@ class InstrMaker(object):
         #data,imgfile=self.m.measure()
         # Read dots identification result from ptl and store a dictionary
         pix_size=0.006
-        flip=1  # x flip right now this is hard coded since we don't change the camera often. 
+        if self.fvc.fvc_type == 'FLI':
+            flip=1  # x flip right now this is hard coded since we don't change the camera often. 
+        else:
+            flip=0 
         posids=list(self.ptl.posids)
         fidids=list(self.ptl.fidids)
         n_pos=len(posids)
@@ -57,13 +60,18 @@ class InstrMaker(object):
             posids_PFS.append(row['DEVICE_ID'])
             device_loc_PFS.append(row['DEVICE_LOC'])
         
-        for i in range(n_pos):
-            posid=posids[i]
+        enabled_posids=self.m.enabled_posids
+
+        for i in range(len(enabled_posids)):
+            posid=enabled_posids[i]
             obsX_arr.append(float(self.ptl.get_posfid_val(posid,'LAST_MEAS_OBS_X')))
             obsY_arr.append(float(self.ptl.get_posfid_val(posid,'LAST_MEAS_OBS_Y')))
             obsXY_arr.append([obsX_arr[i],obsY_arr[i]])
             fvcXY_this=self.fvc.obsXY_to_fvcXY([obsXY_arr[i]])
-            fvcX_arr.append(-fvcXY_this[0][0])
+            if flip ==1:
+                fvcX_arr.append(-fvcXY_this[0][0])
+            else:
+                fvcX_arr.append(fvcXY_this[0][0])
             fvcY_arr.append(fvcXY_this[0][1])
             index=posids_PFS.index(posid.strip('M'))
             device_loc_this=device_loc_PFS[index]
@@ -84,18 +92,40 @@ class InstrMaker(object):
         pars.add('offx', value=0.)
         pars.add('offy', value=0.)
         pars.add('angle', value=0.)
-        out = minimize(self._residual, pars, args=(fvcX_arr,fvcY_arr,metroX_arr,metroY_arr)) # Find the minimum chi2
+        print('fvcX:',fvcX_arr,'\n metroX:',metroX_arr,'\n fvcY:',fvcY_arr,'\n metroY:',metroY_arr,'\n')
+        if self.fvc.fvc_type == 'FLI':
+            out = minimize(self._residual, pars, args=(fvcX_arr,fvcY_arr,metroX_arr,metroY_arr)) # Find the minimum chi2
+        else:
+            out = minimize(self._residual_sbig, pars, args=(fvcX_arr,fvcY_arr,metroX_arr,metroY_arr)) # Find the minimum chi2^M
+
+
+        if flip==1:
+            offy=-out.params['offy'].value
+            rot=out.params['angle'].value % 360 - 180.
+        else:
+            offy=out.params['offy'].value
+            rot=out.params['angle'].value
      #  Plot
         pars_out=out.params
-        model=pars_out['scale']*(self._rot(fvcX_arr+pars_out['offx'],fvcY_arr+pars_out['offy'],pars_out['angle']))
-        model_x=np.array(model[0,:]).ravel()
-        model_y=np.array(model[1,:]).ravel()
+        if self.fvc.fvc_type=='FLI':
+            model=pars_out['scale']*(self._rot(fvcX_arr+pars_out['offx'],fvcY_arr+pars_out['offy'],pars_out['angle']))
+            model_x=np.array(model[0,:]).ravel()
+            model_y=np.array(model[1,:]).ravel()
+            label='fvcmag  '+str(out.params['scale'].value/pix_size)+'\n'+'fvcrot  '+str(rot)+'\n' +'fvcxoff  '+str(out.params['offx'].value)+'\n'+'fvcyoff  '+str(offy)+'\n'
+        else:
+            rot_xy=self._rot(fvcX_arr,fvcY_arr,pars['angle'])
+            model=pars_out['scale']*rot_xy
+            model_x=np.array(model[0,:])+pars_out['offx']
+            model_y=np.array(model[1,:])+pars_out['offy']
+            model_x=model_x.ravel()
+            model_y=model_y.ravel()
+            label='fvcmag  '+str(out.params['scale'].value)+'\n'+'fvcrot  '+str(rot)+'\n' +'fvcxoff  '+str(out.params['offx'].value)+'\n'+'fvcyoff  '+str(offy)+'\n'
+
         pp = PdfPages('instrmaker_fit_check.pdf')
         plt.figure(1,figsize=(15,15))
-        plt.subplot(221)
-        plt.plot(metroX_arr,metroY_arr,'ko',label='fvcmag  '+str(out.params['scale'].value/pix_size)+'\n'+'fvcrot  '+str(out.params['angle'].value % 360 - 180.)+'\n' \
-                                         +'fvcxoff  '+str(out.params['offx'].value)+'\n'+'-fvcyoff  '+str(out.params['offy'].value)+'\n')
-        plt.plot(model_x,model_y,'b')
+        plt.subplot(111)
+        plt.plot(metroX_arr,metroY_arr,'ko',label=label)
+        plt.plot(model_x,model_y,'b+')
         plt.xlabel('metroX')
         plt.ylabel('metroY')
         plt.legend(loc=2)
@@ -105,15 +135,25 @@ class InstrMaker(object):
         pp.close()
  
         # Write the output
-    
-        filename = 'instrmaker.par'
-        f = open(filename,'w')
-        output_lines='fvcmag  '+str(out.params['scale'].value/pix_size)+'\n'+'fvcrot  '+str(out.params['angle'].value % 360-180.)+'\n' \
-                    +'fvcxoff  '+str(out.params['offx'].value)+'\n'+'fvcyoff  '+str(-out.params['offy'].value)+'\n' \
-                    +'fvcflip  '+str(flip)+'\n'+'fvcnrow  6000 \n'+'fvcncol  6000 \n'+'fvcpixmm  '+str(pix_size) 
-        f.write(output_lines)
-        f.close()
-        pdb.set_trace()
+        if self.fvc.fvc_type == 'FLI': 
+            filename = 'instrmaker.par'
+            f = open(filename,'w')
+            output_lines='fvcmag  '+str(out.params['scale'].value/pix_size)+'\n'+'fvcrot  '+str(rot)+'\n' \
+                        +'fvcxoff  '+str(out.params['offx'].value)+'\n'+'fvcyoff  '+str(offy)+'\n' \
+                        +'fvcflip  '+str(flip)+'\n'+'fvcnrow  6000 \n'+'fvcncol  6000 \n'+'fvcpixmm  '+str(pix_size) 
+            f.write(output_lines)
+            f.close()
+            print('PM instrument file saved to instrmaker.par')
+        else:
+            print('Instrument Pars:',out.params['angle'].value ,out.params['scale'].value,[out.params['offx'].value,out.params['offy'].value])
+            print('The original Instrument Pars are:',self.fvc.rotation,self.fvc.scale,self.fvc.translation)
+            a=input('Do you want to update the Pars?')
+            if a =='Y' or a=='y' or a=='Yes' or a=='YES' or a=='yes':
+                self.fvc.rotation=out.params['angle'].value 
+                self.fvc.translation=[out.params['offx'].value,out.params['offy'].value]
+                self.fvc.scale=out.params['scale'].value
+
+
         return out
     
     
@@ -138,6 +178,14 @@ class InstrMaker(object):
         res=np.array((x_model-x_data))**2+np.array((y_model-y_data))**2
         return res
     
+    def _residual_sbig(self,pars,x,y,x_data,y_data):
+        rot_xy=self._rot(x,y,pars['angle'])
+        xy_model=pars['scale']*rot_xy
+        x_model=np.array(xy_model[0,:])+pars['offx']
+        y_model=np.array(xy_model[1,:])+pars['offy']
+        res=np.array((x_model-x_data))**2+np.array((y_model-y_data))**2
+        return res
+
         # 1. extract positioner locations that we just measured with FVC
         # 2. read file with nominal locations from metrology data (see DESI-2850 for format)
         # 3. compare FVC measurements with metrology, to calculate:
