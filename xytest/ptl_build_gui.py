87e1438597002cd98b377a5a477a7e418e8f40b3
parfa30 <parker.fagrelius@noirlab.edu>
2019-06-05 21:18:37 +0000

    working for making file

--- xytest/ptl_build_gui.py
+++ xytest/ptl_build_gui.py
@@ -17,6 +17,7 @@ sys.path.append(os.path.abspath(os.getenv('HOME')+'/focalplane/pos_utility/'))
 
 import summarizer
 import numpy as np
+import pandas as pd
 import time
 import pos_xytest_plot
 import um_test_report as test_report
@@ -33,8 +34,10 @@ import time
 import show_detected
 import pdb
 from configobj import ConfigObj
-import write_conf_files as wc
-import subprocess as sub
+from datetime import datetime
+
+pos_settings_path = ('/home/msdos/focalplane/fp_settings/pos_settings/')
+hwsetup_path = ('/home/msdos/focalplane/fp_settings/hwsetups/')
 
 class PtlTestGUI(object):
     def __init__(self,hwsetup_conf='',xytest_conf=''):
@@ -45,13 +48,13 @@ class PtlTestGUI(object):
         self.mode = 0
 
         # GUI geometry       
-        w=800
-        h=700
-        ws=gui_root.winfo_screenwidth()
-        hs=gui_root.winfo_screenheight()
-        x=(ws/2)-(w/2)
-        y=(hs/2)-(h/2)
-        gui_root.geometry('%dx%d+%d+%d' % (w,h,x,y))
+        #w=800
+        #h=700
+        #ws=gui_root.winfo_screenwidth()
+        #hs=gui_root.winfo_screenheight()
+        #x=(ws/2)-(w/2)
+        #y=(hs/2)-(h/2)
+        #gui_root.geometry('%dx%d+%d+%d' % (w,h,x,y))
         
         #Set Petal and PetalBox
         self.get_petal = Entry(gui_root, width = 8, justify = 'right')
@@ -60,11 +63,11 @@ class PtlTestGUI(object):
         self.get_pc = Entry(gui_root, width = 8, justify = 'right')
         self.get_pc.grid(row=1, column=0)
         self.get_pc.insert(0, 'PC')
-        Button(gui_root, width = 10, text = 'PETAL + PC', command=lambda: self.set_peta_no()).grid(row=0, column=1)
+        Button(gui_root, width = 10, text = 'PETAL + PC', command=lambda: self.set_petal_no()).grid(row=0, column=1)
 
-        #Set Scaleself.get_petal = Entry(gui_root, width = 8, justify = 'right')
+        #Set Scale
         self.scale_conf = ConfigObj('b141_scale.conf', unrepr=True,encoding='utf-8')
-        self.scale = float(scale_conf['SCALE'])
+        self.scale = float(self.scale_conf['SCALE'])
         self.get_scale = Entry(gui_root, width = 8, justify = 'right')
         self.get_scale.grid(row=0, column=2)
         self.get_scale.insert(0, str(self.scale))
@@ -95,54 +98,45 @@ class PtlTestGUI(object):
         self.get_devloc.grid(row=4, column = 2)
         self.get_devloc.insert(0, str('DEV LOC'))   
 
-        Button(gui_root, width = 10, text = 'ADD POS', command=lambda: self.get_pos()).grid(row=4, column=4)   
+        Button(gui_root, width = 10, text = 'ADD POS', command=lambda: self.get_pos()).grid(row=4, column=3)   
 
+        #Write Pos .conf file
+        Label(gui_root, text = 'Write CONF files').grid(row=1, column = 4)
+        Button(gui_root, width = 10, text = 'POS Conf', command=lambda: self.pos_conf()).grid(row=2, column=4)
+        Button(gui_root, width = 10, text = 'HWSETUP', command=lambda: self.hwsetup_conf()).grid(row=3, column=4)
 
-        yscroll_text1 = Scrollbar(gui_root, orient=tkinter.VERTICAL)
-        yscroll_text1.grid(row=6, column=4, rowspan=20,sticky=tkinter.E+tkinter.N+tkinter.S,pady=5)
-        self.text1=Text(gui_root,height=30,width=90,wrap=WORD)
-        self.text1.grid(row=6,column=1,columnspan=4,rowspan=20,sticky=W,pady=4,padx=15)
-        self.text1.configure(yscrollcommand=yscroll_text1.set)
-        self.text1.tag_configure('bold_italics', font=('Arial', 12, 'bold', 'italic'))
-        self.text1.tag_configure('big', font=('Verdana', 12, 'bold','bold'))
-        self.text1.tag_configure('green', foreground='#476042', font=('Tempus Sans ITC', 12, 'bold'))
-        self.text1.tag_configure('red', foreground='#ff0000', font=('Tempus Sans ITC', 12, 'bold'))
-        self.text1.tag_configure('yellow', background='#ffff00', font=('Tempus Sans ITC', 12, 'bold'))
-        yscroll_text1.config(command=self.text1.yview)
+        #Run Tests
+        Label(gui_root, text = 'Tests').grid(row=1, column=5)
+        Button(gui_root, width = 10, text = 'Init HW', command=lambda: self.run_init_hwsetup()).grid(row=2, column=5)
+        Button(gui_root, width = 10, text = 'XYTEST', command=lambda: self.run_xytest()).grid(row=3, column=5)
+        Button(gui_root, width = 10, text = 'Get RESULTS', command=lambda: self.test_results()).grid(row=4, column=5)
+     
+        #Quit Button
+        Button(gui_root, width = 10, text = 'QUIT', command=lambda: self.quit()).grid(row=0, column=6)
 
-        self.listbox1 = Listbox(gui_root, width=20, height=20,selectmode='multiple',exportselection=0)
+        #Main print screen
+        yscroll_main = Scrollbar(gui_root, orient=tkinter.VERTICAL)
+        yscroll_main.grid(row=6, column=4, rowspan=20,sticky=tkinter.E+tkinter.N+tkinter.S,pady=5)
+        self.main=Text(gui_root,height=30,width=90,wrap=WORD)
+        self.main.grid(row=6,column=1,columnspan=4,rowspan=20,sticky=W,pady=4,padx=15)
+        self.main.configure(yscrollcommand=yscroll_main.set)
+        self.main.tag_configure('bold_italics', font=('Arial', 12, 'bold', 'italic'))
+        self.main.tag_configure('big', font=('Verdana', 12, 'bold','bold'))
+        self.main.tag_configure('green', foreground='#476042', font=('Tempus Sans ITC', 12, 'bold'))
+        self.main.tag_configure('red', foreground='#ff0000', font=('Tempus Sans ITC', 12, 'bold'))
+        self.main.tag_configure('yellow', background='#ffff00', font=('Tempus Sans ITC', 12, 'bold'))
+        yscroll_main.config(command=self.main.yview)
+
+        #Positioner List Box
+        self.listbox1 = Listbox(gui_root, width=15, height=20,selectmode='multiple',exportselection=0)
         self.listbox1.grid(row=6, column=0,rowspan=10,pady=4,padx=15)
-        # create a vertical scrollbar to the right of the listbox
         yscroll_listbox1 = Scrollbar(command=self.listbox1.yview, orient=tkinter.VERTICAL)
         yscroll_listbox1.grid(row=6, column=0, rowspan=10,sticky=tkinter.E+tkinter.N+tkinter.S,pady=5)
         self.listbox1.configure(yscrollcommand=yscroll_listbox1.set)
-        self.listbox1.insert(tkinter.END,'ALL')
-        
-
+        #self.listbox1.insert(tkinter.END,'ALL')
         self.listbox1.bind('<ButtonRelease-1>', self.get_list)
 
-
-# Right part of the GUI to write to Acceptance Traveller
-
-#       Load the information 
-        Label(gui_root,text="You selected").grid(row=3,column=5)
-        self.e_selected=Entry(gui_root)
-        self.e_selected.grid(row=3,column=6)
-        
-        yscroll_text2 = Scrollbar(gui_root, orient=tkinter.VERTICAL)
-        yscroll_text2.grid(row=6, column=6, rowspan=20,sticky=tkinter.E+tkinter.N+tkinter.S,pady=5)     
-        self.text2=Text(gui_root,height=30,width=38)
-        self.text2.grid(row=6,column=5,columnspan=2,rowspan=20,sticky=W+E+N+S,pady=4,padx=15)
-        self.text2.tag_configure('bold_italics', font=('Arial', 12, 'bold', 'italic'))
-        self.text2.tag_configure('big', font=('Verdana', 12, 'bold','bold'))
-        self.text2.tag_configure('green', foreground='#476042', font=('Tempus Sans ITC', 12, 'bold'))
-        self.text2.tag_configure('red', foreground='#ff0000', font=('Tempus Sans ITC', 12, 'bold'))
-        self.text2.tag_configure('yellow', background='#ffff00', font=('Tempus Sans ITC', 12, 'bold'))
-        self.text2.configure(yscrollcommand=yscroll_text2.set)   
-        yscroll_text2.config(command=self.text2.yview)
-        
-        
-        
+        #Logo Image 
         photo = PhotoImage(file='desi_logo.gif')
         photo=photo.subsample(7)
         label = Label(image=photo,width=20,height=100)
@@ -152,31 +146,28 @@ class PtlTestGUI(object):
 
         mainloop()
         
-    def set_ptl_id(self):
-        self.ptl_id=self.e1.get()
-        self.canbus='can'+self.e_can.get().strip()        
-        print('Loading Petal'+self.ptl_id+', canbus:'+self.canbus)
-        gui_root.destroy()
- 
     def set_petal_no(self):
         try:
             self.petal = int(self.get_petal.get())
         except:
-            print("Input an integer for the Petal number")
+            self.main.insert(END,"Input an integer for the Petal number" +'\n')
          
         try:
             self.pc = int(self.get_pc.get())
         except:
-            print("Input an integer for the Petal Controller number")
+            self.main.insert(END,"Input an integer for the Petal Controller number" +'\n')
+
+        if (isinstance(self.petal,int)) and (isinstance(self.pc,int)):
+            self.main.insert(END, "Testing to proceed with Petal #%d, PetalController PC%s \n" % (self.petal, str(self.pc).zfill(2))) 
 
     def update_scale(self):
         try:
             self.scale = float(self.get_scale.get())
             self.scale_conf['SCALE'] = self.scale
             self.scale_conf.write()
-            print("Scale resaved as %f" % self.scale)
+            self.main.insert(END,"Scale resaved as %f \n" % self.scale)
         except:
-            print("Input a float for the scale")
+            self.main.insert(END,"Input a float for the scale" +'\n')
 
     def get_pos(self):
         self.canid = self.get_canid.get()
@@ -186,15 +177,15 @@ class PtlTestGUI(object):
         try:
             self.canid = int(self.canid)
         except:
-            print('Need to input CAN ID as an integer')
+            self.main.insert(END,'Need to input CAN ID as an integer'+'\n')
         try:
             self.busid = int(self.busid)
         except:
-            print('Need to input BUS ID as an integer')
+            self.main.insert(END,'Need to input BUS ID as an integer'+'\n')
         try:
             self.devloc = int(self.devloc)
         except:
-            print('Need to input DEV LOC as an integer')
+            self.main.insert(END,'Need to input DEV LOC as an integer'+'\n')
 
         if (isinstance(self.canid, int)) and (isinstance(self.busid, int)) and (isinstance(self.devloc, int)):
             self.pos_info['CAN'].append(self.canid)
@@ -206,13 +197,64 @@ class PtlTestGUI(object):
             return
 
     def print_pos_info(self):
-        self.listbox1.clear(0,tkinter.END)
-        this_pos_info = pd.DataFrame.from_dict(self.pos_info)
-        self.listbox1.insert(tkinter.END,this_pos_info)
+        self.listbox1.delete(0,tkinter.END)
+        this_pos_info = np.asarray([self.pos_info['CAN'], self.pos_info['BUS'], self.pos_info['DEV']]).T
+        #self.listbox1.insert(tkinter.END,'ALL')
+        self.listbox1.insert(tkinter.END,'CAN BUS DEV')
+        for pos in this_pos_info:
+            self.listbox1.insert(tkinter.END,pos)
+
+    def pos_conf(self):
+        df = pd.DataFrame.from_dict(self.pos_info)
+        df.to_csv('pos_list.csv',index=False)
+        
+        self.write_pos_conf()
+        
+
+    def hwsetup_conf(self):
+        df = pd.DataFrame.from_dict(self.pos_info)
+        df.to_csv('pos_list.csv',index=False)
+
+        self.write_hwsetup_conf()
+
+
+    def write_pos_conf(self):
+        pos_list = pd.read_csv('pos_list.csv', header=0)
+        pos_list = pos_list.to_records(index=False)
+        for line in pos_list:
+            pos = str('M' + str(line['CAN']).zfill(5))
+            try:
+                file_name = pos_settings_path+'unit_%s.conf'%pos
+                config = ConfigObj(file_name,unrepr=True,encoding='utf-8')
+                bus = line['BUS']
+                busid = 'can'+str(bus)
+                config['BUS_ID'] = str(busid)
+                config['DEVICE_ID'] = int(line['DEV'])
+                config['PETAL_ID'] = int(self.petal)
+                config.write()
+                self.main.insert(END,"Wrote config file for %s \n" % pos)
+            except:
+                self.main.insert(END,"Something went wrong with device %s \n "%pos)
+        self.main.insert(END, "Done writing POS Conf files \n")
+
+    def write_hwsetup_conf(self):
+        pos_list = pd.read_csv('pos_list.csv', header=0)
+        pos_list = pos_list.to_records(index=False)
+        hwconfig = ConfigObj(hwsetup_path+'hwsetup_test_xytest.conf', unrepr=True, encoding='utf-8')
+        pos_ids = [str('M'+str(line['CAN']).zfill(5)) for line in pos_list]
+        time_now = str(datetime.now())
+        hwconfig['scale'] = float(self.scale)
+        hwconfig['pos_ids'] = pos_ids
+        hwconfig['last_updated'] = time_now
+        hwconfig['ptl_id'] = int(str(self.petal).zfill(2))
+        hwconfig.write()
+        self.main.insert(END,"hwsetup file updated with these positioners: \n")
+        for pos in pos_ids:
+            self.main.insert(END,pos+'\n')
 
     def check_svn(self):
         if (self.svnuser is None) | (self.svnpass is None):
-            print("You must enter your SVN credentials before proceeding")
+            self.main.insert(END,"You must enter your SVN credentials before proceeding"+'\n')
 
 
     def svn_info(self):
@@ -223,13 +265,23 @@ class PtlTestGUI(object):
             if err == 0:
                 pass
             else:
-                print("Something wrong with the SVN credentials")
+                self.main.insert(END,"Something wrong with the SVN credentials"+'\n')
         except:
-            print("Please put in your SVN credentials")
+            self.main.insert(END,"Please put in your SVN credentials"+'\n')
 
 
+    def run_init_hwsetup(self):
+        self.main.insert(END, "You selected to run the Initialize HWSetup. This will identify positioners and run calibration \n")
 
+    def run_xytest(self):
+        self.main.insert(END, "You selected to run the XYTest \n")
+
+    def test_results(self):
+        self.main.insert(END, "Here is where the results will print \n")
  
+    def quit(self):
+        sys.exit()
+
     def get_list(self,event):
         # get selected line index
         index = self.listbox1.curselection()
@@ -250,9 +302,9 @@ class PtlTestGUI(object):
 
             
     def show_info(self):
-        self.text1.insert(END,str(len(self.info.keys())).strip()+' Pos+Fid are found \n')
+        self.main.insert(END,str(len(self.info.keys())).strip()+' Pos+Fid are found \n')
         for key in sorted(self.info.keys()):
-            self.text1.insert(END,str(key)+' '+str(self.info[key])+'\n')
+            self.main.insert(END,str(key)+' '+str(self.info[key])+'\n')
         
 
         
@@ -263,7 +315,7 @@ class PtlTestGUI(object):
         with open(self.logfile,'a') as fh:
             fh.write(line + '\n')
         if stdout:
-            print(line)
+            self.main.insert(END,line)
 
 
 
