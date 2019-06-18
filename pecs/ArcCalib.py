3bf6b0ca61963ccad7a643bf721630044ac54835
kfanning <fanning@SLAC.Stanford.EDU>
2019-06-18 21:16:27 +0000

    ArcCalib to test

--- pecs/ArcCalib.py
+++ pecs/ArcCalib.py
@@ -2,6 +2,7 @@
 Runs an Arc Calibration using fvc and petal proxies. Requires running DOS instance. See pecs.py
 '''
 from pecs import PECS
+import posconstants as pc
 
 class Arc(PECS)
 	
@@ -11,7 +12,7 @@ class Arc(PECS)
 		self.clear_angle_margin = 3.0
 		self.ptlid = list(self.ptls.keys())[0]
 
-	def arc_calibration(selection=[],n_points_P=6, n_points_T=6,auto_update=True):
+	def arc_calibration(selection=None,n_points_P=6, n_points_T=6,auto_update=True):
 		if not selection:
 			posid_list = list(self.ptls[self.ptlid].get_positioners(enabled_only=enabled_only).loc[:'DEVICE_ID'])
 		elif posids[0][0] == 'c': #User passed busids
@@ -22,31 +23,37 @@ class Arc(PECS)
 		T_data = []
 		for request in requests_list_T:
 			self.ptls[self.ptlid].prepare_move(request)
-			expected_positions = self.ptls[self.ptlid].execute_move()
-			measured_positions = pandas.DataFrame.from_dict(measured_positions)
-			measured_positions.rename(columns={'q':'MEASURED_Q','s':'MEASURED_S','flags':'FLAGS', 'id':'DEVICE_ID'},inplace=True)
+	        measured_positions = self.fvc.measure(expected_positions)
+	        measured_positions = pandas.DataFrame(measured_positions)
+	        measured_positions.rename(columns={'q':'Q','s':'S','flags':'FLAGS', 'id':'DEVICE_ID'},inplace=True)
+	        used_positions = measured_positions['DEVICE_ID'].isin(posid_list)
 			request.rename(columns={'X1':'TARGET_T','X2':'TARGET_P'})
-			measured_positions.merge(request, how='outer',on='DEVICE_ID')
-			T_data.append(measured_positions)
+			used_positions.merge(request, how='outer',on='DEVICE_ID')
+			T_data.append(used_positions)
 		P_data = []
 		for request in requests_list_P:
 			self.ptls[self.ptlid].prepare_move(request)
-			expected_positions = self.ptls[self.ptlid].execute_move()
-			measured_positions = pandas.DataFrame.from_dict(measured_positions)
-			measured_positions.rename(columns={'q':'MEASURED_Q','s':'MEASURED_S','flags':'FLAGS', 'id':'DEVICE_ID'},inplace=True)
+	        measured_positions = self.fvc.measure(expected_positions)
+	        measured_positions = pandas.DataFrame(measured_positions)
+	        measured_positions.rename(columns={'q':'Q','s':'S','flags':'FLAGS', 'id':'DEVICE_ID'},inplace=True)
+	        used_positions = measured_positions['DEVICE_ID'].isin(posid_list)
 			request.rename(columns={'X1':'TARGET_T','X2':'TARGET_P'})
-			measured_positions.merge(request, how='outer',on='DEVICE_ID')
-			P_data.append(measured_positions)
+			used_positions.merge(request, how='outer',on='DEVICE_ID')
+			P_data.append(used_positions)
 		data = self.ptls[self.ptlid].calibrate_from_arc_data(T_data,P_data,auto_update=auto_update)
 		return data
 
 if __name__ == '__main__':
 	arc = Arc()
 	user_text = input('Please list BUSIDs or POSIDs (not both) seperated by spaces, leave it blank to use all on petal' + self.petal_id +' : ')
-	user_text = user_text.split()
-	selection = []
-	for item in user_text:
-		selction.append(item)
+	if user_text == '':
+		user_text = user_text.split()
+		selection = []
+		for item in user_text:
+			selection.append(item)
+	else:
+		selection = None
 	data = arc.arc_calibration(selection=selection)
 	print(data)
+	data.to_csv('arc_calibration_'+pc.filename_timestamp_str_now()+'.csv')
 	
