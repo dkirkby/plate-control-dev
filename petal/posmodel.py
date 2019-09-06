5075fe808fd52c8609ceec872135e929963abb35
duanyutong <duanyutong@localhost>
2019-09-06 17:10:16 +0000

    fixed method name incosistency

--- petal/posmodel.py
+++ petal/posmodel.py
@@ -226,7 +226,7 @@ class PosModel(object):
         future shaft position changes. This is necessary for correct checking of
         software travel limits, when a sequence of multiple moves is being planned out.
         """
-        pos = self.expected_current_posTP
+        pos = self.expected_current_posintTP
         start = self.trans.addto_posTP([pos[0],pos[1]], expected_prior_dTdP, range_wrap_limits='none') # since expected_prior_dTdP is just tracking already-existing commands, do not perform range wrapping on it
         if not(allow_exceed_limits):
             distance = self.axis[axisid].truncate_to_limits(distance,start[axisid])
