420154bb254d3f44c4c42303c09f581da5885102
duanyutong <duanyutong@localhost>
2019-09-25 16:58:35 +0000

    restored 1p offsetstp and postp calls as previous changes added no functionality and doubled code size

--- pecs/OnePoint_offsetsTP.py
+++ pecs/OnePoint_offsetsTP.py
@@ -2,5 +2,4 @@ from OnePoint import OnePoint
 
 if __name__ == '__main__':
     print('This will update TP offsets using one point calibration.')
-    optp = OnePoint()
-    optp.run_interactively(mode='offsetsTP', tp_target='default', match_radius=80)
+    OnePoint(mode='offsetsTP', interactive=False)
--- pecs/OnePoint_posTP.py
+++ pecs/OnePoint_posTP.py
@@ -2,5 +2,4 @@ from OnePoint import OnePoint
 
 if __name__ == '__main__':
     print('This will update TP offsets using one point calibration.')
-    op = OnePoint()
-    op.run_interactively(mode='posTP', match_radius=80)
+    OnePoint(mode='posTP', interactive=False)
