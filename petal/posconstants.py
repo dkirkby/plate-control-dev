3e0f53fd159f733b182abde8e4376815f8faf9ea
joesilber <jhsilber@lbl.gov>
2019-09-27 16:33:22 +0000

    added an R2N and N2R lookups for nutation angle. Also removed the polys, which we never use, and which I think had some historical typos anyway.

--- petal/posconstants.py
+++ petal/posconstants.py
@@ -55,10 +55,6 @@ R_lookup_data = np.genfromtxt(R_lookup_path, comments="#", delimiter=",")
 
 # Mapping of radial coordinate R to pseudo-radial coordinate S
 # (distance along focal surface from optical axis)
-R2S_poly = [-1.26341e-23, 1.90376e-20, -1.11245e-17, 3.30271e-15, -5.78982e-13,
-            7.31761e-11, 1.72104e-09, 1.91532e-07, 0.999997, 0.50001]
-
-
 def R2S_lookup(R):
     return np.interp(R, R_lookup_data[:, 0], R_lookup_data[:, 2],
                      left=float('nan'))
@@ -70,18 +66,25 @@ def S2R_lookup(S):
 
 
 # Mapping of radial coordinate R to Z5 coordinate on the nominal asphere
-R2Z_poly = [6.74215e-23, -7.75243e-20, 2.9168e-17, -5.23944e-15, 1.61621e-12,
-            -4.82781e-10, 1.24578e-08, -0.000100884, 6.63924e-06, -2.33702e-05]
-
-
 def R2Z_lookup(R):
     return np.interp(R, R_lookup_data[:, 0], R_lookup_data[:, 1],
                      left=float('nan'))
 
 
-def Z2R_lookup(S):
-    return np.interp(S, R_lookup_data[:, 1], R_lookup_data[:, 0],
+def Z2R_lookup(Z):
+    return np.interp(Z, R_lookup_data[:, 1], R_lookup_data[:, 0],
                      left=float('nan'))
+
+					 
+# Mapping of radial coordinate R to Nutation angle (deg) on the nominal asphere
+def R2N_lookup(R):
+    return np.interp(R, R_lookup_data[:, 0], R_lookup_data[:, 3],
+                     left=float('nan'))
+
+def N2R_lookup(N):
+    return np.interp(N, R_lookup_data[:, 3], R_lookup_data[:, 0],
+                     left=float('nan'))
+
 
 # Mapping of positioner power supplies to can channels
 power_supply_can_map = {'V1':{'can10','can11','can13','can22', 'can23'},
