cc639881423be708cd8de7648d2f9a2e0bb8180d
kfanning <fanning@SLAC.Stanford.EDU>
2019-10-21 19:37:14 +0000

    Added loop script to move phi repeatedly.

---
+++ pecs/loop_phi_moves.py
@@ -0,0 +1,23 @@
+from OnePoint import OnePointCalib
+from pecs import PECS
+
+ctrl = PECS()
+ctrl.interactive_ptl_setup()
+fvc = ctrl.fvc
+ptls = ctrl.ptls
+posids = ctrl.posids
+petal_id = ctrl.petal_id
+
+go = True
+iterations = 0
+while go:
+    if iterations%2 == 0:
+        tp_target = (0, 145)
+    else:
+        tp_target = (0, 175)
+    user_text = input(f'Please enter "go" to move (num iterations {iterations}: '))
+    if 'g' in user_text.lower():
+        go = True
+        OnePointCalib(fvc=fvc,ptls=ptls,petal_id=petal_id,posids=posids,interactive=False,auto_update=False)
+    else:
+        go=False
