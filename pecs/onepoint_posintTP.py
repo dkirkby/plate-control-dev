5b71cd0510e2843aaf1cd5c04a5c8f19a4f5750d
duanyutong <duanyutong@localhost>
2019-12-18 21:03:11 +0000

    addec fvc collect call to one point scripts

--- pecs/onepoint_posintTP.py
+++ pecs/onepoint_posintTP.py
@@ -10,6 +10,7 @@ Requires running DOS instance. See pecs.py
 from poscalibrations import PosCalibrations
 test = PosCalibrations('1p_posintTP', interactive=True)
 test.run_1p_calibration(auto_update=True)
+test.fvc_collect(destination=test.data.dir)
 test.data.generate_data_products()
 print(  # preview calibration updates
     test.data.calibdf[['POS_T', 'POS_P', 'OFFSET_T', 'OFFSET_P']])
