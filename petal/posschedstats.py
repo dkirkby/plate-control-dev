c85076be49ee02c1b6f8d49ab4911ee98fe1b466
joesilber <jhsilber@lbl.gov>
2019-12-31 12:22:07 +0000

    now also tracking specific collision pairs from redundant checking

--- petal/posschedstats.py
+++ petal/posschedstats.py
@@ -17,6 +17,7 @@ class PosSchedStats(object):
         self.unresolved = {}
         self.unresolved_tables = {}
         self.unresolved_sweeps = {}
+        self.redundantly_checked_collision_pairs = {}
         self.strings = {'method':[]}
         self.blank_str = '-'
         self.numbers = {'n pos':[],
@@ -49,6 +50,7 @@ class PosSchedStats(object):
         self.unresolved[self.latest] = {}
         self.unresolved_tables[self.latest] = {}
         self.unresolved_sweeps[self.latest] = {}
+        self.redundantly_checked_collision_pairs[self.latest] = {}
         for key in self.strings:
             self.strings[key].append(self.blank_str)
         for key in self.numbers:
@@ -127,13 +129,14 @@ class PosSchedStats(object):
         """Add data recording number of iterations of path adjustment were made."""
         self.numbers['num path adjustment iters'][-1] += iterations
         
-    def add_redundant_collision_check(self, stage_name, num_colliding):
+    def add_redundant_collision_check(self, collision_pairs):
         """Add data recording if there were still any bots colliding after a
         redundant check."""
         # currently ignoring stage_name, seems overly elaborate to check separately
         if self.numbers[redundant_checks_str][-1] == None:
             self.numbers[redundant_checks_str][-1] = 0
-        self.numbers[redundant_checks_str][-1] += num_colliding
+        self.numbers[redundant_checks_str][-1] += len(collision_pairs)
+        self.redundantly_checked_collision_pairs[self.latest] = collision_pairs
     
     def add_unresolved_colliding_at_stage(self, stage_name, colliding_set, colliding_tables, colliding_sweeps):
         """Add data recording ids of any bots colliding in a given stage. This
@@ -174,11 +177,12 @@ class PosSchedStats(object):
     
     def summarize_unresolved_colliding(self):
         """Returns a summary dictionary of the unresolved colliding items."""
-        summary = {'unresolved colliding':[], 'unresolved colliding tables':[], 'unresolved colliding sweeps':[]}
+        summary = {'unresolved colliding':[], 'unresolved colliding tables':[], 'unresolved colliding sweeps':[], 'redundant check collision pairs found':[]}
         for sched in self.schedule_ids:
             summary['unresolved colliding'].append(self.unresolved[sched])
             summary['unresolved colliding tables'].append(self.unresolved_tables[sched])
             summary['unresolved colliding sweeps'].append(self.unresolved_sweeps[sched])
+            summary['redundant check collision pairs found'].append(self.redundantly_checked_collision_pairs[sched])
         return summary
         
     def summarize_all(self):
