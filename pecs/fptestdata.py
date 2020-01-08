e68f502769e6f51a2686e6578a5d1c3f0593199a
duanyutong <duanyutong@localhost>
2020-01-08 15:09:08 +0000

    moved PosTransforms.alt to outer layer as a class variable, which can be acquired without instantiating dummy instance

--- pecs/fptestdata.py
+++ pecs/fptestdata.py
@@ -310,7 +310,8 @@ class FPTestData:
     def export_data_logs(self):
         '''must have writte self.posids_pc, a dict keyed by pcid'''
         self.test_cfg.write()  # write test config
-        for attr in ['movedf', 'gradedf', 'calibdf', 'abnormaldf']:
+        for attr in ['movedf', 'gradedf', 'calibdf', 'abnormaldf', 
+                     'exppos', 'meapos']:
             if not hasattr(self, attr):
                 continue  # skip a df if it doesn't exist
             getattr(self, attr).to_pickle(
