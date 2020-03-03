df625628ad7a63805e7fb190c758661d449deb9d
joesilber <jhsilber@lbl.gov>
2020-03-03 01:14:30 +0000

    updated SQL example queries so now they more intelligently just grab the most recent values

--- petal/anticollision_test_harness/generate_posparam_set_from_dbdata.py
+++ petal/anticollision_test_harness/generate_posparam_set_from_dbdata.py
@@ -6,15 +6,15 @@ Created on Tue Feb 11 16:01:48 2020
 Customized script for merging data into posparam format. Data is presumed
 pulled from online databases:
     
-    "calib" --> http://desi-www.kpno.noao.edu:8080/DESIPositioners/Positioner_calibration.html
-    "moves" --> http://desi-www.kpno.noao.edu:8080/DESIPositioners/Positioner_moves.html
+    "calib" --> http://web.replicator.dev-cattle.stable.spin.nersc.org:60040/DESIPositioners/Positioner_calibration.html
+    "moves" --> http://web.replicator.dev-cattle.stable.spin.nersc.org:60040/DESIPositioners/Positioner_moves.html
     
 Data is assumed saved as csv using the browser tool.
 
 Sample SQL queries:
     
-    "calib" --> select petal_id,pos_id,time_recorded,device_loc,length_r1,length_r2,offset_x,offset_y,offset_t,offset_p,physical_range_t,physical_range_p,gear_calib_t,gear_calib_p from posmovedb.positioner_calibration_p3 order by time_recorded desc
-    "moves" --> select petal_id,pos_id,time_recorded,ctrl_enabled from posmovedb.positioner_moves_p3 order by time_recorded desc
+    "calib" --> select petal_id,pos_id,time_recorded,device_loc,length_r1,length_r2,offset_x,offset_y,offset_t,offset_p,physical_range_t,physical_range_p,gear_calib_t,gear_calib_p from posmovedb.positioner_calibration_p3 PM where EXISTS (select * from posmovedb.positioner_calibration_index_p3 where id = PM.pos_id and last_index=PM.pos_calib_index)
+    "moves" --> select petal_id,pos_id,time_recorded,ctrl_enabled from posmovedb.positioner_moves_p3 PM where exists (select * from posmovedb.positioner_moves_index_p3 where id = PM.pos_id and last_index=PM.pos_move_index)
 
 """
 
@@ -25,8 +25,8 @@ posids = 'all' # 'all', for all posids on that petal, or else a limited set
 output_file = 'posparams_30001.csv'
 
 data_folder = 'C:/Users/jhsilber/Desktop/anticollision targets'
-calibdb_name = 'PTL03 online calib db, retrieved 2020-02-11.csv'
-movesdb_name = 'PTL03 online moves db, retrieved 2020-02-11.csv'
+calibdb_name = 'PTL03 online calib db, retrieved 2020-03-02.csv'
+movesdb_name = 'PTL03 online moves db, retrieved 2020-03-02.csv'
 calibpath = os.path.join(data_folder, calibdb_name)
 movespath = os.path.join(data_folder, movesdb_name)
 calib = pandas.read_csv(calibpath)
