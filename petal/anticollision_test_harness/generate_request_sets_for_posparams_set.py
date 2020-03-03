9f7cbab2b8e57976a02ba7a5e742565b99b6810e
joesilber <jhsilber@lbl.gov>
2020-03-03 01:59:11 +0000

    fixed bug in attempts_remaining handling of while loop

--- petal/anticollision_test_harness/generate_request_sets_for_posparams_set.py
+++ petal/anticollision_test_harness/generate_request_sets_for_posparams_set.py
@@ -40,36 +40,35 @@ for i in range(num_sets_to_make):
     targets_obsTP = {}
     targets_posXY = {}
     for posid,model in posmodels.items():
-        attempts_remaining = 100 # just to prevent infinite loop if there's a bug somewhere
-        while posid not in targets_obsTP:
+        attempts_remaining = 10000 # just to prevent infinite loop if there's a bug somewhere
+        while posid not in targets_obsTP and attempts_remaining:
             rangeT = model.targetable_range_T
             rangeP = model.targetable_range_P
             max_patrol = posparams[posid]['LENGTH_R1'] + posparams[posid]['LENGTH_R2']
             min_patrol = abs(posparams[posid]['LENGTH_R1'] - posparams[posid]['LENGTH_R2'])
-            while attempts_remaining:
-                x = random.uniform(-max_patrol,max_patrol)
-                y = random.uniform(-max_patrol,max_patrol)
-                this_radius = (x**2 + y**2)**0.5
-                if min_patrol <= this_radius and this_radius <= max_patrol:
-                    break
-                attempts_remaining -= 1
-            this_posXY = [x,y]
-            this_posTP = model.trans.poslocXY_to_posintTP(this_posXY)[0]
-            this_obsTP = model.trans.posintTP_to_poslocTP(this_posTP)
-            target_interference = False
-            for neighbor in collider.pos_neighbors[posid]:
-                if neighbor in targets_obsTP:
-                    if collider.spatial_collision_between_positioners(posid, neighbor, this_obsTP, targets_obsTP[neighbor]):
-                        target_interference = True
-                        break
-            out_of_bounds = collider.spatial_collision_with_fixed(posid, this_obsTP)
-            if not target_interference and not out_of_bounds:
-                targets_obsTP[posid] = this_obsTP
-                targets_posXY[posid] = this_posXY
+            x = random.uniform(-max_patrol,max_patrol)
+            y = random.uniform(-max_patrol,max_patrol)
+            this_radius = (x**2 + y**2)**0.5
+            if min_patrol > this_radius or this_radius > max_patrol:
+                attempts_remaining = max(attempts_remaining - 1, 0)
             else:
-                attempts_remaining -= 1
+                this_posXY = [x,y]
+                this_posTP = model.trans.poslocXY_to_posintTP(this_posXY)[0]
+                this_obsTP = model.trans.posintTP_to_poslocTP(this_posTP)
+                target_interference = False
+                for neighbor in collider.pos_neighbors[posid]:
+                    if neighbor in targets_obsTP:
+                        if collider.spatial_collision_between_positioners(posid, neighbor, this_obsTP, targets_obsTP[neighbor]):
+                            target_interference = True
+                            break
+                out_of_bounds = collider.spatial_collision_with_fixed(posid, this_obsTP)
+                if not target_interference and not out_of_bounds:
+                    targets_obsTP[posid] = this_obsTP
+                    targets_posXY[posid] = this_posXY
+                else:
+                    attempts_remaining = max(attempts_remaining - 1, 0)
         if not attempts_remaining:
-            v = state._val
+            v = model.state._val
             print('Warning: no valid target found for posid: ' + posid + ' at location ' + str(v['DEVICE_LOC']) + ' (x,y) = (' + format(v['OFFSET_X'],'.3f') + ',' + format(v['OFFSET_Y'],'.3f') + ')')
     all_targets.append(targets_posXY)
     
