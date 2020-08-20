8739aa8a7b098f0a909d1bc2996ef0b2e07ee273
joesilber <jhsilber@lbl.gov>
2020-08-20 18:42:45 +0000

    fix typo target0 and 1 in posintTP seq

--- pecs/sequence_generator.py
+++ pecs/sequence_generator.py
@@ -146,7 +146,7 @@ t = 0.0
 p = 150.0
 new = sequence.Sequence(short_name='debug posintTP with corr',
                         long_name=f'single move to posintTP = ({t}, {p}) and a followup correction move')
-new.add_move(command='posintTP', target0=x, target1=y, log_note=debug_note(new), n_corr=1)
+new.add_move(command='posintTP', target0=t, target1=p, log_note=debug_note(new), n_corr=1)
 print(new,'\n')
 tests.append(new)
 
