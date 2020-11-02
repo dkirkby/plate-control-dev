b98e456178b6cc17e3948b4e09f2b34de99989d8
joesilber <jhsilber@lbl.gov>
2020-11-02 20:03:40 +0000

    in progress rework for use with simulated petal instance, rather than direct db access

--- pecs/get_calibrations.py
+++ pecs/get_calibrations.py
@@ -66,46 +66,29 @@ import psycopg2, getpass
 import posstate
 import posmodel
 
-# data columns to retrieve from db tables
-db_moves = 'moves'
-db_calib = 'calib'
-db_keys = {db_moves: {'TIME_RECORDED': 'posmovedb timestamp',
-                      'POS_MOVE_INDEX': f'unique row index within the {db_moves} table for a given positioner',
-                      'CTRL_ENABLED': 'if True, move tables are allowed to be sent to the positioner',
-                      'POS_T': 'internally-tracked theta angle, a.k.a. "posintT" or t_int',
-                      'POS_P': 'internally-tracked phi angle, a.k.a. "posintP" or p_int'
-                      },
-           db_calib: {'TIME_RECORDED': 'posmovedb timestamp',
-                      'POS_CALIB_INDEX': f'unique row index within the {db_moves} table for a given positioner',
-                      'PETAL_ID': 'unique serial id number of petal',
-                      'POS_ID': 'unique serial id number of fiber positioner',
-                      'DEVICE_LOC': 'location number on petal, c.f. DESI-0530',
-                      'LENGTH_R1': 'kinematic length between central axis and phi axis',
-                      'LENGTH_R2': 'kinematic length between phi axis and fiber',
-                      'OFFSET_T': 'mounting angle of positioner\'s x-axis (roughly halfway between theta hardstops) w.r.t. that of petal',
-                      'OFFSET_P': 'angle from hardstop at far clockwise position to phi=0',
-                      'OFFSET_X': 'x position of robot\'s central axis, in "flat" coordinate system',
-                      'OFFSET_Y': 'y position of robot\'s central axis, in "flat" coordinate system',
-                      'PHYSICAL_RANGE_T': 'angular distance between theta hardstops',
-                      'PHYSICAL_RANGE_P': 'angular distance between phi hardstops',
-                      'GEAR_CALIB_T': 'scale factor for theta shaft rotations, actual/nominal',
-                      'GEAR_CALIB_P': 'scale factor for phi shaft rotations, actual/nominal',
-                      'KEEPOUT_EXPANSION_PHI_RADIAL': 'radially expands phi keepout polygon by this amount',
-                      'KEEPOUT_EXPANSION_THETA_RADIAL': 'radially expands theta keepout polygon by this amount',
-                      'KEEPOUT_EXPANSION_PHI_ANGULAR': 'angularly expands phi keepout polygon by + and - this amount',
-                      'KEEPOUT_EXPANSION_THETA_ANGULAR': 'angularly expands theta keepout polygon by + and - this amount',
-                      'CLASSIFIED_AS_RETRACTED': 'identifies positioner for travel only within a restricted radius',
-                      },
-           }
-output_keys_map = {'TIME_RECORDED': {db_moves: 'TIME_RECORDED_MOVES',
-                                     db_calib: 'TIME_RECORDED_CALIB'},
-                   }
-descriptions = {}
-for db_table_name, defs in db_keys.items():
-    for key, desc in defs.items():
-        if key in output_keys_map:
-            key = output_keys_map[key][db_table_name]
-        descriptions[key] = desc
+# keys and descriptions to retrieve
+keys = {
+        'POS_ID': 'unique serial id number of fiber positioner',
+        'PETAL_ID': 'unique serial id number of petal',
+        'DEVICE_LOC': 'location number on petal, c.f. DESI-0530',
+        'CTRL_ENABLED': 'if True, move tables are allowed to be sent to the positioner',
+        'LENGTH_R1': 'kinematic length between central axis and phi axis',
+        'LENGTH_R2': 'kinematic length between phi axis and fiber',
+        'OFFSET_T': 'mounting angle of positioner\'s x-axis (roughly halfway between theta hardstops) w.r.t. that of petal',
+        'OFFSET_P': 'angle from hardstop at far clockwise position to phi=0',
+        'OFFSET_X': 'x position of robot\'s central axis, in "flat" coordinate system',
+        'OFFSET_Y': 'y position of robot\'s central axis, in "flat" coordinate system',
+        'PHYSICAL_RANGE_T': 'angular distance between theta hardstops',
+        'PHYSICAL_RANGE_P': 'angular distance between phi hardstops',
+        'GEAR_CALIB_T': 'scale factor for theta shaft rotations, actual/nominal',
+        'GEAR_CALIB_P': 'scale factor for phi shaft rotations, actual/nominal',
+        'KEEPOUT_EXPANSION_PHI_RADIAL': 'radially expands phi keepout polygon by this amount',
+        'KEEPOUT_EXPANSION_THETA_RADIAL': 'radially expands theta keepout polygon by this amount',
+        'KEEPOUT_EXPANSION_PHI_ANGULAR': 'angularly expands phi keepout polygon by + and - this amount',
+        'KEEPOUT_EXPANSION_THETA_ANGULAR': 'angularly expands theta keepout polygon by + and - this amount',
+        'CLASSIFIED_AS_RETRACTED': 'identifies positioner for travel only within a restricted radius',
+        }
+descriptions = {desc: key for key, desc in keys.items()}
 
 # units (where applicable)
 angular_keys = {'POS_T', 'POS_P', 'OFFSET_T', 'OFFSET_P', 'PHYSICAL_RANGE_T', 'PHYSICAL_RANGE_P', 'KEEPOUT_EXPANSION_PHI_ANGULAR', 'KEEPOUT_EXPANSION_THETA_ANGULAR'}
@@ -113,55 +96,22 @@ mm_keys = {'LENGTH_R1', 'LENGTH_R2', 'OFFSET_X', 'OFFSET_Y', 'KEEPOUT_EXPANSION_
 units = {key: 'deg' for key in angular_keys}
 units.update({key: 'mm' for key in mm_keys})
 
-# database retrieval
-def dbquery(comm, operation, parameters=None):
-    '''Cribbed from desimeter/dbutil.py. Returns a dict with keys = column names
-    and values = python lists.'''
-    cx=comm.cursor()
-    cx.execute(operation,parameters)
-    names=[d.name for d in cx.description]
-    res=cx.fetchall()
-    cx.close()
-    results=dict()
-    for i,name in enumerate(names) :
-        results[name]=[r[i] for r in res]
-    return results
+# initialize petals in simulation mode
 
-max_rows = 10000
-comm = None
-params_dict = {}
-for site, config in db_configs.items():
-    these_petal_ids = {i for i in petal_ids if i in config['petal_ids']}
-    if these_petal_ids:
-        pw = ''
-        db_text = f'{config["host"]}:{config["port"]}'
-        if config['password_required']:
-            pw = getpass.getpass(f'Enter read-access password for database at {db_text} >> ')
-        try:
-            comm = psycopg2.connect(host=config['host'], port=config['port'], database='desi_dev', user='desi_reader', password=pw)
-            print(f'success connecting to database at {db_text}')
-        except:
-            print(f'failed to connect to database at {db_text}')
-        assert comm != None, f'failed to connect to {site} database! (this connection is needed for petal_ids {these_petal_ids})'
-    for petal_id in these_petal_ids:   
-        from_moves = f'from posmovedb.positioner_moves_p{petal_id}'
-        from_calib = f'from posmovedb.positioner_calibration_p{petal_id}'
-        cmd = f'select distinct pos_id {from_calib}'
-        data = dbquery(comm, cmd)
-        all_posids = sorted(set(data['pos_id']))
-        query_config = {db_moves: {'from': from_moves, 'keys': db_keys[db_moves]},
-                        db_calib: {'from': from_calib, 'keys': db_keys[db_calib]}}
-        for posid in all_posids:
-            for db_table_name, config in query_config.items():
-                keys_str = str({key.lower() for key in config['keys']}).strip('{').strip('}').replace("'",'')
-                cmd = f"select {keys_str} {config['from']} where pos_id in ('{posid}') order by time_recorded desc limit 1"
-                data = dbquery(comm, cmd)
-                for key, value in data.items():
-                    output_key = key.upper()
-                    if output_key in output_keys_map:
-                        output_key = output_keys_map[output_key][db_table_name]
-                    params_dict[key.upper()] += value  # note how value comes back as a single element list here
+
+# validate availability of data
+available_keys_dict = ptl.quick_query()
+available_keys = set()
+for group_name, keys in available_keys_dict.items():
+    if 'keys' in group_name:
+        available_keys |= set(keys)
+missing = set(keys) - available_keys
+assert not any(missing), f'some keys are not available: {missing}'
+
+# gather data and make table
+
 t = Table(params_dict)
+
 
 # add some metadata
 meta = {}
