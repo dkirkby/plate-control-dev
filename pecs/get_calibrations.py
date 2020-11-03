ee5ce99be98e67f6b7dcd280c33ddc0a5b78a7c6
joesilber <jhsilber@lbl.gov>
2020-11-03 02:03:31 +0000

    basic implementation complete, not yet tested

--- pecs/get_calibrations.py
+++ pecs/get_calibrations.py
@@ -4,10 +4,18 @@
 an astropy table (when run as an imported module) or save to disk as an ecsv.
 """
 
-def assert2_no_log(test, message):
-    assert test, message
+import argparse
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('-ptl', '--petal_ids', type=str, default='kpno', help='Comma-separated integers, specifying one or more PETAL_ID number(s) for which to retrieve data. Defaults to all known petals at kpno database. Alternately argue "lbnl" for all known petals in lbnl (beyonce) database.')
+parser.add_argument('-o', '--outdir', type=str, default='.', help='Path to directory where to save output file. Defaults to current dir.')
+parser.add_argument('-m', '--comment', type=str, default='', help='Comment string which will be included in output file metadata.')
+parser.add_argument('-dl', '--disable_logger', action='store_true', help='Disable logging to disk.')
+uargs = parser.parse_args()
 
-import os, sys
+# general imports
+import os
+import sys
+from astropy.table import Table
 try:
     import posconstants as pc
 except:
@@ -16,77 +24,121 @@ except:
     print('Couldn\'t find posconstants the usual way, resorting to sys.path.append')
     import posconstants as pc
 
+# set up logger
+import simple_logger
+import traceback
+if uargs.disable_logger:
+    log_path = None
+else:
+    log_dir = pc.dirs['sequence_logs']
+    log_timestamp = pc.filename_timestamp_str()
+    log_name = log_timestamp + '_get_calibrations.log'
+    log_path = os.path.join(log_dir, log_name)
+logger = simple_logger.start_logger(log_path)
+assert2 = simple_logger.assert2
+
+# validate petal ids
 default_petal_ids = {'kpno': {2, 3, 4, 5, 6, 7, 8, 9, 10, 11},
                      'lbnl': {0, 1}}
 possible_petal_ids = set()
 for ids in default_petal_ids.values():
     possible_petal_ids |= ids
+use_all_ptls = uargs.petal_ids in default_petal_ids.keys()
+if use_all_ptls:
+    petal_ids = default_petal_ids[uargs.petal_ids]
+else:
+    petal_ids = uargs.petal_ids.split(',')
+    petal_ids = {int(i) for i in petal_ids}
+assert2(len(petal_ids) > 0, 'no petal ids detected in {petal_ids}')
+for petal_id in petal_ids:
+    assert2(petal_id in possible_petal_ids, f'petal id {petal_id} is unrecognized')
 
-def validate_petal_ids(proposed_ids, assert2=None):
-    '''Validates collection of petal ids. Also see comments in help docstr of
-    "--petal-ids" command line arg for more detail. Returns a set.'''
-    assert2 = assert2 if assert2 else assert2_no_log
-    if pc.is_string(proposed_ids):
-        use_all_ptls = proposed_ids in default_petal_ids.keys()
-        if use_all_ptls:
-            petal_ids = default_petal_ids[proposed_ids]
-        else:
-            petal_ids = proposed_ids.split(',')
-            petal_ids = {int(i) for i in petal_ids}
-    else:
-        assert2(pc.is_collection(proposed_ids), 'proposed petal ids {proposed_ids} is not a collection')
-        petal_ids = set(proposed_ids)
-    assert2(len(petal_ids) > 0, 'no petal ids detected in {petal_ids}')
-    for petal_id in petal_ids:
-        assert2(petal_id in possible_petal_ids, f'petal id {petal_id} is unrecognized')
-    return petal_ids
+# data keys retreivable from quick_query(), along with human-readable descriptions
+query_keys = {'POS_ID': 'unique serial id number of fiber positioner',
+              'PETAL_ID': 'unique serial id number of petal',
+              'DEVICE_LOC': 'location number on petal, c.f. DESI-0530',
+              'LENGTH_R1': 'kinematic length between central axis and phi axis',
+              'LENGTH_R2': 'kinematic length between phi axis and fiber',
+              'OFFSET_T': 'mounting angle of positioner\'s x-axis (roughly halfway between theta hardstops) w.r.t. that of petal',
+              'OFFSET_P': 'angle from hardstop at far clockwise position to phi=0',
+              'OFFSET_X': 'x position of robot\'s central axis, in "flat" coordinate system',
+              'OFFSET_Y': 'y position of robot\'s central axis, in "flat" coordinate system',
+              'PHYSICAL_RANGE_T': 'angular distance between theta hardstops',
+              'PHYSICAL_RANGE_P': 'angular distance between phi hardstops',
+              'GEAR_CALIB_T': 'scale factor for theta shaft rotations, actual/nominal',
+              'GEAR_CALIB_P': 'scale factor for phi shaft rotations, actual/nominal',
+              'KEEPOUT_EXPANSION_PHI_RADIAL': 'radially expands phi keepout polygon by this amount',
+              'KEEPOUT_EXPANSION_THETA_RADIAL': 'radially expands theta keepout polygon by this amount',
+              'KEEPOUT_EXPANSION_PHI_ANGULAR': 'angularly expands phi keepout polygon by + and - this amount',
+              'KEEPOUT_EXPANSION_THETA_ANGULAR': 'angularly expands theta keepout polygon by + and - this amount',
+              'CLASSIFIED_AS_RETRACTED': 'identifies positioner for travel only within a restricted radius',
+              'CTRL_ENABLED': 'if True, move tables are allowed to be sent to the positioner',
+              'DEVICE_CLASSIFIED_NONFUNCTIONAL': 'if True, the focal plane team has determined this positioner cannot be operated',
+              'FIBER_INTACT': 'if False, the focal plane team has determined the positioner\'s fiber cannot be measured',
+              }
 
-# keys and descriptions to retrieve
-keys = {
-        'POS_ID': 'unique serial id number of fiber positioner',
-        'PETAL_ID': 'unique serial id number of petal',
-        'DEVICE_LOC': 'location number on petal, c.f. DESI-0530',
-        'LENGTH_R1': 'kinematic length between central axis and phi axis',
-        'LENGTH_R2': 'kinematic length between phi axis and fiber',
-        'OFFSET_T': 'mounting angle of positioner\'s x-axis (roughly halfway between theta hardstops) w.r.t. that of petal',
-        'OFFSET_P': 'angle from hardstop at far clockwise position to phi=0',
-        'OFFSET_X': 'x position of robot\'s central axis, in "flat" coordinate system',
-        'OFFSET_Y': 'y position of robot\'s central axis, in "flat" coordinate system',
-        'PHYSICAL_RANGE_T': 'angular distance between theta hardstops',
-        'PHYSICAL_RANGE_P': 'angular distance between phi hardstops',
-        'GEAR_CALIB_T': 'scale factor for theta shaft rotations, actual/nominal',
-        'GEAR_CALIB_P': 'scale factor for phi shaft rotations, actual/nominal',
-        'KEEPOUT_EXPANSION_PHI_RADIAL': 'radially expands phi keepout polygon by this amount',
-        'KEEPOUT_EXPANSION_THETA_RADIAL': 'radially expands theta keepout polygon by this amount',
-        'KEEPOUT_EXPANSION_PHI_ANGULAR': 'angularly expands phi keepout polygon by + and - this amount',
-        'KEEPOUT_EXPANSION_THETA_ANGULAR': 'angularly expands theta keepout polygon by + and - this amount',
-        'CLASSIFIED_AS_RETRACTED': 'identifies positioner for travel only within a restricted radius',
-        'CTRL_ENABLED': 'if True, move tables are allowed to be sent to the positioner',
-        'DEVICE_CLASSIFIED_NONFUNCTIONAL': 'if True, the focal plane team has determined this positioner cannot be operated',
-        'FIBER_INTACT': 'if False, the focal plane team has determined the positioner\'s fiber cannot be measured',
-        }
-descriptions = {desc: key for key, desc in keys.items()}
+# general and positioner-specific data keys from the collider
+flat_note = '["flat" coordinates]'
+keepout_T_note = f'central body (theta) polygon {flat_note}'
+keepout_P_note = f'eccentric body (phi) keepout polygon {flat_note}'
+general_collider_keys = {'timestep': '[sec] quantization when sweeping polygons through rotation schedules, to check for collisions',
+                         'Eo_radius_with_margin': '[mm] radius within which a polygon is considered "retracted"',
+                         'general_keepout_T': f'basic {keepout_T_note}',
+                         'general_keepout_P': f'basic {keepout_P_note}',
+                         'keepout_PTL': f'petal boundary keepout polygon {flat_note}',
+                         'keepout_GFA': f'GFA boundary keepout polygon {flat_note}', 
+                         }
+pos_collider_keys = {'KEEPOUT_T': keepout_T_note,
+                     'KEEPOUT_P': keepout_P_note,
+                     'POS_NEIGHBORS': 'neighboring positioners',
+                     'FIXED_NEIGHBORS': 'neighboring fixed boundaries'}
+pos_collider_attr_map = {'KEEPOUT_T': 'keepouts_T',
+                         'KEEPOUT_P': 'keepouts_P',
+                         'POS_NEIGHBORS': 'pos_neighbors',
+                         'FIXED_NEIGHBORS': 'fixed_neighbor_cases'}
 
-# units (where applicable)
+# dependently-calculated fields
+offset_variants = {'PTL': 'ptlXY', 'CS5': 'obsXY'}
+offset_variant_keys = {f'{coord}_{var}': f'{coord} transformed into {system}' for coord in ['OFFSET_X', 'OFFSET_Y'] for var, system in offset_variants.items()}
+range_desc = lambda func, c: f'{func} targetable internally-tracked {"theta" if c == "T" else "phi"} angle (i.e. "POS_{c}" or "posint{c}" or "{c.lower()}_int"'
+range_keys = {f'{func.upper()}_{c}': range_desc(func, c) for func in ['max', 'min'] for c in ['T', 'P']}
+
+# summarize all keys, units, and descriptions (where applicable)
+all_keys = {}
+all_keys.update(query_keys)
+all_keys.update(pos_collider_keys)
+all_keys.update(offset_variant_keys)
+all_keys.update(range_keys)
 angular_keys = {'POS_T', 'POS_P', 'OFFSET_T', 'OFFSET_P', 'PHYSICAL_RANGE_T', 'PHYSICAL_RANGE_P', 'KEEPOUT_EXPANSION_PHI_ANGULAR', 'KEEPOUT_EXPANSION_THETA_ANGULAR'}
+angular_keys |= set(range_keys)
 mm_keys = {'LENGTH_R1', 'LENGTH_R2', 'OFFSET_X', 'OFFSET_Y', 'KEEPOUT_EXPANSION_PHI_RADIAL', 'KEEPOUT_EXPANSION_THETA_RADIAL'}
+mm_keys |= set(offset_variant_keys)
 units = {key: 'deg' for key in angular_keys}
 units.update({key: 'mm' for key in mm_keys})
+descriptions = {desc: key for key, desc in all_keys.items()}
 
-def initialize_petals(petal_ids):
-    '''Returns a dict with keys = petal_ids'''
+# initialize petals
 roles = {}
 apps = {}
 ptls = {}
-logger.info('Will attempt to get c')
+logger.info(f'Attempting to retrieve data for {len(petal_ids)} petals: {petal_ids}')
 try:
     from DOSlib.proxies import Petal
+    logger.info('Online system is available, will use PetalApp')
+    online = True
+except:
+    import petal
+    logger.warning('Online system is not available. Falling back to dummy data + petal.py instances (may be useful for other debugging).')
+    online = False
+if online:
     import threading
     def run_petal(petal_id, role):
         os.system(f'python PetalApp.py --device_mode True --sim True --petal_id {petal_id} --role {role}')
     for petal_id in petal_ids:
+        name = f'PetalApp{petal_id}'
         role = f'PETALSIM{petal_id}'
-        ptl_app = threading.Thread(target=run_petal, name=f'PetalApp{petal_id}', args=(petal_id, role))
+        logger.info(f'Initializing threaded PetalApp with name {name} and role {role}...')
+        ptl_app = threading.Thread(target=run_petal, name=name, args=(petal_id, role))
         ptl_app.daemon = False
         ptl_app.start()
         roles[petal_id] = role
@@ -94,97 +146,106 @@ try:
     for petal_id, role in roles.items():
         ptl = Petal(petal_id, role=role)
         ptls[petal_id] = ptl
-except:
-    import sys
-    path_to_petal = '../petal'
-    sys.path.append(os.path.abspath(path_to_petal))
-    import petal
+else:
     for petal_id in petal_ids:
-        ptl = petal.Petal(petal_id        = petal_id,
-                      petal_loc       = 3,
-                      posids          = pos_params.keys(),
-                      fidids          = fidids,
-                      simulator_on    = True,
-                      db_commit_on    = False,
-                      local_commit_on = False,
-                      local_log_on    = False,
-                      collider_file   = None,
-                      sched_stats_on  = True, # minor speed-up if turn off
-                      anticollision   = 'adjust',
-                      verbose         = False,
-                      phi_limit_on    = False)
+        logger.info(f'Initializing petal instance for petal id {petal_id}...')
+        ptl = petal.Petal(petal_id=petal_id,
+                          simulator_on=True,
+                          db_commit_on=False,
+                          local_commit_on=False,
+                          local_log_on=False,
+                          collider_file=None,
+                          sched_stats_on=False,
+                          )
+        ptls[petal_id] = ptl
+logger.info(f'{len(ptls)} petals initialized')
 
-
-# proceed with remainder of imports
-from astropy.table import Table
-
-# gather data
-data = {key:[] for key in keys}
-for ptl in ptls:
-    this_data = {}
-    available_keys_dict = ptl.quick_query()
-    available_keys = set()
-    for group_name, keys in available_keys_dict.items():
-        if 'keys' in group_name:
-            available_keys |= set(keys)
-    missing = set(keys) - available_keys
-    assert not any(missing), f'some keys are not available for petal {ptl.petal_id}: {missing}'
-    posids_ordered = sorted(ptl.posids)
-    for key in keys:
-        this_dict = ptl.quick_query(key=key, mode='iterable')
-        this_list = [this_dict[p] for p in posids_ordered]
-        data[key].append(this_list)
+# Most of the remainder is enclosed in a try so that we can shut down PetalApps
+# at the end, even if a crash occurs before that.
+try:
+    # gather data
+    data = {key:[] for key in all_keys}
+    meta = {}
+    meta['DATE_RETRIEVED'] = pc.timestamp_str()
+    meta['COMMENT'] = uargs.comment
+    for key in ['DATE_RETRIEVED', 'COMMENT']:        
+        logger.info(f'metadata {key} = {meta[key]}')
+    meta['PETAL_ALIGNMENTS'] = {}
+    for petal_id, ptl in ptls.items():
+        this_data = {}
+        valid_keys = ptl.quick_query()['valid_keys']
+        missing = set(query_keys) - set(valid_keys)
+        assert2(not(any(missing)), f'some keys are not available for petal {ptl.petal_id}: {missing}')
+        posids_ordered = sorted(ptl.posids)
+        logger.info('Now gathering data for {len(posids_ordered)} positioners on petal id {petal_id}...')
         
-t = Table(data)
-
-# add some metadata
-meta = {}
-meta['DATE_RETRIEVED'] = pc.timestamp_str()
-meta['EO_RADIUS_WITH_MARGIN'] = None
-t.meta = meta
-
-# add units and descriptions
-for key in t.columns:
-    if key in descriptions:
-        t[key].description = descriptions[key]
-    if key in units:
-        t[key].unit = units[key]
-
-# dependently-calculated information
-states = {}
-models = {}
-for posid in t['POS_ID']:
-    state = posstate.PosState()
-    model = posmodel.PosModel()
-    posmodels[posid] = model
+        # queryable values
+        for key in set(query_keys) | set(pos_collider_keys):
+            if key in query_keys:
+                this_dict = ptl.quick_query(key=key, mode='iterable')
+            else:
+                attr_key = pos_collider_attr_map[key]
+                this_dict = getattr(ptl.collider, attr_key)
+                if 'fixed' in attr_key.lower():
+                    this_dict = {posid: pc.case.names[enum] for posid, enum in this_dict.items()}
+            this_list = [this_dict[p] for p in posids_ordered]
+            data[key].extend(this_list)
     
-# shut down the threads
-
-# columns for desimodel
-'PETAL', 'DEVICE', 'PETAL_ID', 'DEVICE_ID', 'DEVICE_TYPE',
-'OFFSET_X_CS5', 'OFFSET_Y_CS5',
-'OFFSET_X_PTL', 'OFFSET_Y_PTL',
-'OFFSET_X_FLAT', 'OFFSET_Y_FLAT',
-'MAX_T', 'MIN_T', 'MAX_P', 'MIN_P'
-'KEEPOUT_T', 'KEEPOUT_P' # --> and have these each be in local coords (i.e. placed at x0,y0 = 0,0)
-
-
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-ptl', '--petal_ids', type=str, default='kpno', help='Comma-separated integers, specifying one or more PETAL_ID number(s) for which to retrieve data. Defaults to all known petals at kpno database. Alternately argue "lbnl" for all known petals in lbnl (beyonce) database.')
-    parser.add_argument('-o', '--outdir', type=str, default='.', help='Path to directory where to save output file. Defaults to current dir.')
-    parser.add_argument('-m', '--comment', type=str, default='', help='Comment string which will be included in output file metadata.')
-    uargs = parser.parse_args()
-
-    # set up logger
-    import simple_logger
-    log_dir = pc.dirs['sequence_logs']
-    log_timestamp = pc.filename_timestamp_str()
-    log_name = log_timestamp + '_get_calibrations.log'
-    log_path = os.path.join(log_dir, log_name)
-    logger = simple_logger.start_logger(log_path)
-    assert2 = simple_logger.assert2
+        # dependent values
+        for posid in posids_ordered:
+            model = ptl.posmodels[posid]
+            flat_offset_xy = (ptl.collider.x0[posid], ptl.collider.y0[posid])
+            for suffix, coord in offset_variants.keys():
+                transform = model.trans.construct('flatXY', coord)
+                xy_new = transform(flat_offset_xy)
+                data[f'OFFSET_X_{suffix}'].append(xy_new[0])
+                data[f'OFFSET_Y_{suffix}'].append(xy_new[1])
+            for key in range_keys:
+                func = max if 'MAX' in key else min
+                rng = model.targetable_range_posintT if 'T' in key else model.targetable_range_posintP
+                data[key].append(func(rng))
+        
+        # petal-wide values
+        meta['PETAL_ALIGNMENTS'][petal_id] = ptl.trans.petal_alignment
+            
+    # [JHS] As of 2020-11-02, these general collider parameters should be equivalent for
+    # any petal. Here, I simply use the last ptl instance from the for loop above.
+    meta['COLLIDER_ATTRIBUTES'] = general_collider_keys
+    for key in general_collider_keys:
+        meta[key] = getattr(ptl.collider, key)
+        
+    logger.info('All data gathered, generating table format...')
+    t = Table(data)
+    t.meta = meta
     
+    # add units and descriptions
+    for key in t.columns:
+        if key in descriptions:
+            t[key].description = descriptions[key]
+        if key in units:
+            t[key].unit = units[key]
+        
+    # save data to disk
     save_dir = os.path.realpath(uargs.outdir)
-    petal_ids = validate_petal_ids(uargs.petal_ids, assert2)
+    save_name = pc.filename_timestamp_str() + '_fp_calibs.ecsv'
+    save_path = os.path.join(save_dir, save_name)
+    t.write(save_path)
+    logger.info(f'Data saved to: {save_path}')
+    
+    # save a reference to this file in a standard place
+    ref_dir = pc.dirs['temp_files']
+    ref_path = os.path.join(ref_dir, 'latest_fp_calibs.txt')
+    with open(ref_path, mode='w') as file:
+        file.write(save_path)
+    logger.info(f'File path cached at standard location: {ref_path}')
+    
+except Exception:
+    logger.error('get_calibrations crashed! See traceback below:')
+    logger.critical(traceback.format_exc())
+    logger.info('Attempting to clean up lingering PetalApp instances prior to exiting...')
+
+# shut down the threads
+for petal_id in apps.keys():
+    logger.info(f'Sending shutdown command for petal {petal_id}...')
+    ptl = ptls[petal_id]
+    ptl.shutdown_event.set()
