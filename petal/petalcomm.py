af67953975f0e7734f1ee7f2da3010d3942e1877
joesilber <jhsilber@lbl.gov>
2020-12-22 23:25:24 +0000

    moved definition of send_and_execute_tables interface to a separate class in progress definition of a function to return simulated error cases of all types

--- petal/petalcomm.py
+++ petal/petalcomm.py
@@ -5,43 +5,6 @@ import Pyro4
 import sys
 from DOSlib.advertise import Seeker
 
-# Interface / error cases for send_and_execute_tables() with petalcontroller
-# --------------------------------------------------------------------------
-# All defined cases are encoded below. To ensure logical completeness of the
-# interface, there is an order in which the failure checks are supposed to be
-# applied by petalcontroller:
-#   1. Required power supplies are on?
-#   2. Required canbuses are enabled?
-#   3. We are below the moves per hour limit?
-#   4. We are above the power supply reset rate limit?
-#   5. We are below the temperature acceptance limit?
-#   6. Tables wre sent and confirmed...
-#       a) ...for all required tables?
-#       b) ...for all tables?
-# So for example a return value 'FAILED: power off' (e.g. #1 above) does not
-# necessarily mean that one of the later error cases in the list would not also
-# apply, had petalcontroller gotten to that check.
-send_fail_return_format = {'cleared': dict,  # any pos for which move tables were originally defined, but whose memories are now known to be clear
-                           'no_response': dict,  # failed pos, which didn't respond on CAN bus. collection may overlap with cleared or unknown
-                           'unknown': dict,  # failed pos, which might or might not currently have tables loaded
-                           }  # subdicts have keys = busid strings like 'can11' etc and values = canid ints
-rate_fail_return_format = {'current_rate': int,  # foo per hour
-                           'sec_until_ready': int,  # min time (seconds) we must wait until ready again to execute
-                           }
-send_and_exec_cases = {
-    'SUCCESS': None,
-    'PARTIAL SUCCESS: all required moves': send_fail_return_format,  # any returned pos are in the failed-but-not-required category
-    'FAILED: sending tables': send_fail_return_format,  # return collections will contain some *required* pos for which tables failed to send
-    'FAILED: power off': [],  # list includes only the power supplies that are disabled
-    'FAILED: canbus off': [],  # list includes only the can buses that are disabled
-    'FAILED: move rate limit': rate_fail_return_format,
-    'FAILED: power supply reset rate limit': rate_fail_return_format,
-    'FAILED: temperature limit': {},  # keys = canids, values = temperatures. only includes the cases that exceed an acceptance threshold value
-    }
-valid_power_supplies = {'PS1', 'PS2'}
-valid_canbus_ids = {f'can{i:02}' for i in range(100)} | {f'can{i}' for i in range(10)}
-
-
 class PetalComm(object):
     """
     Handles communication between petal software running on floor control computers
@@ -193,7 +156,7 @@ class PetalComm(object):
             print(bus_ids, can_ids)
             return 'FAILED: Can not execute ready_for_tables. Exception: %s' % str(e)           
 
-    def send_and_execute_tables(self, move_tables):
+    def send_and_execute_tables(self, move_tables, simulate=False):
         """
         Sends move tables for positioners over ethernet to the petal controller,
         where they are then sent over CAN to the positioners.
@@ -203,6 +166,9 @@ class PetalComm(object):
 
         INPUTS:
             move_tables ... see method "_hardware_ready_move_tables()" in petal.py
+            simulate ... False --> send tables to hardware
+                     ... True --> return SUCCESS case
+                     ... string in send_and_exec_cases keys --> simulate that error 
             
         OUTPUT:
             tuple[0] ... string
@@ -213,41 +179,20 @@ class PetalComm(object):
         validated here, so higher level code does not need to double-check whether
         they conform to the interface given in posconstants.
         """
+        if simulate:
+            errstr = sendex.SUCCESS if simulate == True else simulate
+            output = errstr, sendex.simdata(errstr, move_tables)
+            sendex.validate(output)
+            return output
         try:
             output = self._call_device('send_and_execute_tables', move_tables_ex=move_tables)
-            self.validate_send_and_exec(output)
+            sendex.validate(output)
             return output
         except Exception as e:
             msg = 'FAILED: Could not send_and_execute move tables, in an undefined way.'
             msg += ' I.e. petalcomm did not receive an error value from petalcontroller'
             msg += f' in a format it could understand. Exception: {e}'
             return msg
-            
-    @staticmethod
-    def validate_send_and_exec(output):
-        '''Validates output from send_and_execute_tables() provided by petalcontroller.
-        Throws an assertion if invalid, otherwise silent.
-        '''
-        def assert2(test):
-            f'petalcontroller return data not understood:\n{output}'
-        assert2(len(output) == 2)
-        assert2(isinstance(output, (tuple, list)))
-        errstr = output[0]
-        data = output[1]
-        assert2(errstr in send_and_exec_cases)
-        expected_fmt = send_and_exec_cases[errstr]
-        expected_type = type(expected_fmt)
-        assert2(type(data) == expected_type)
-        if errstr == 'SUCCESS':
-            assert2(data == send_and_exec_cases[errstr])
-        elif errstr == 'FAILED: power off':
-            assert2(all(x in valid_power_supplies for x in data))
-        elif errstr == 'FAILED: canbus off':
-            assert2(all(x in valid_canbus_ids for x in data))
-        elif expected_type == dict and len(expected_fmt) > 0:
-            for k, v in expected_fmt.items():
-                assert2(k in data)
-                assert2(type(v) == type(data[k]))
 
     def move(self, bus_id, can_id, direction, mode, motor, angle):
         """
@@ -397,3 +342,99 @@ class PetalComm(object):
             return self._call_device('check_can_ready', can_bus_list)
         except Exception as e:
             return 'FAILED: could not check can ready: %s' % str(e)
+
+class SendExecCases(object):
+    """Enumeration of return cases for sending and executing move tables. All
+    defined cases are encoded / documented here.
+    
+    To ensure logical completeness of the interface, there is an order in which
+    the failure checks are supposed to be applied by petalcontroller:
+        
+      1. Required power supplies are on?
+      2. Required canbuses are enabled?
+      3. We are below the moves per hour limit?
+      4. We are above the power supply reset rate limit?
+      5. We are below the temperature acceptance limit?
+      6. Tables wre sent and confirmed...
+          a) ...for all required tables?
+          b) ...for all tables?
+    
+    So for example a return value 'FAILED: power off' (e.g. #1 above) does not
+    necessarily mean that one of the later error cases in the list would not also
+    apply, had petalcontroller gotten to that check.
+    """
+    def __init__(self):
+        self.send_fail_format = {
+            'cleared': dict,  # any pos for which move tables were originally defined, but whose memories are now known to be clear
+            'no_response': dict,  # failed pos, which didn't respond on CAN bus. collection may overlap with cleared or unknown
+            'unknown': dict,  # failed pos, which might or might not currently have tables loaded
+            }  # subdicts have keys = busid strings like 'can11' etc and values = canid ints
+        
+        self.rate_fail_format = {
+            'current_rate': int,  # foo per hour
+            'sec_until_ready': int,  # min time (seconds) we must wait until ready again to execute
+            }
+        
+        self.SUCCESS = 'SUCCESS'
+        self.PARTIAL_SEND = 'PARTIAL SUCCESS: all required moves'
+        self.FAIL_SEND = 'FAILED: sending tables'
+        self.FAIL_PWROFF = 'FAILED: power off'
+        self.FAIL_BUSOFF = 'FAILED: canbus off'
+        self.FAIL_MOVERATE = 'FAILED: move rate limit'
+        self.FAIL_RESETRATE = 'FAILED: power supply reset rate limit'
+        self.FAIL_TEMPLIMIT = 'FAILED: temperature limit'
+        
+        self.defs = {
+            self.SUCCESS:        None,  # all requested move tables sent and executed ok
+            self.PARTIAL_SEND:   self.send_fail_return_format,  # any returned pos are in the failed-but-not-required category
+            self.FAIL_SEND:      self.send_fail_return_format,  # return collections will contain some *required* pos for which tables failed to send
+            self.FAIL_PWROFF:    [],  # list includes only the power supplies that are disabled
+            self.FAIL_BUSOFF:    [],  # list includes only the can buses that are disabled
+            self.FAIL_MOVERATE:  self.rate_fail_return_format,
+            self.FAIL_RESETRATE: self.rate_fail_return_format,
+            self.FAIL_TEMPLIMIT: {},  # keys = canids, values = temperatures. only includes the cases that exceed an acceptance threshold value
+            }
+        
+        self.valid_power_supplies = {'PS1', 'PS2'}
+        self.valid_canbus_ids = {f'can{i:02}' for i in range(100)} | {f'can{i}' for i in range(10)}
+            
+    def validate(self, pc_response):
+        '''Validates output from send_and_execute_tables() provided by petalcontroller.
+        Throws an assertion if invalid, otherwise silent.
+        '''
+        def assert2(test):
+            f'petalcontroller return data not understood:\n{pc_response}'
+        assert2(len(pc_response) == 2)
+        assert2(isinstance(pc_response, (tuple, list)))
+        errstr = pc_response[0]
+        data = pc_response[1]
+        assert2(errstr in self.defs)
+        expected_fmt = self.cases[errstr]
+        expected_type = type(expected_fmt)
+        assert2(type(data) == expected_type)
+        if errstr == self.SUCCESS:
+            assert2(data == self.defs[errstr])
+        elif errstr == self.FAIL_PWROFF:
+            assert2(all(x in self.valid_power_supplies for x in data))
+        elif errstr == self.FAIL_BUSOFF:
+            assert2(all(x in self.valid_canbus_ids for x in data))
+        elif expected_type == dict and len(expected_fmt) > 0:
+            for k, v in expected_fmt.items():
+                assert2(k in data)
+                assert2(type(v) == type(data[k]))
+    
+    def simdata(self, case, tables):
+        '''Generates simulated data matching a given case.
+        
+        INPUT:  case ... string defined in defs
+                tables ... list of hardware-ready move tables
+                
+        OUTPUT: data formatted per defs
+        '''
+        assert case in self.defs
+        if case == self.SUCCESS:
+            return self.defs[case]
+        if case == self.PARTIAL_SEND
+
+# so you only have to initialize this once
+sendex = SendExecCases()
