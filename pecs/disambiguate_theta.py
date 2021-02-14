59334b4b4b0ada06c7ca435447fa3c21c5ddd217
kfanning <fanning@SLAC.Stanford.EDU>
2021-02-14 00:16:52 +0000

    Re-factored disambiguate theta to a class so that I could write fp_setup.py. Added fp_setup.py

--- pecs/disambiguate_theta.py
+++ pecs/disambiguate_theta.py
@@ -24,12 +24,6 @@ uargs = parser.parse_args()
 assert 1 <= uargs.num_tries <= max_tries, f'out of range argument {uargs.num_tries} for num_tries parameter'
 assert 1 <= uargs.num_meas <= max_fvc_iter, f'out of range argument {uargs.num_meas} for num_meas parameter'
 
-# other imports
-import random
-import pandas
-
-# set up logger
-import simple_logger
 try:
     import posconstants as pc
 except:
@@ -38,151 +32,180 @@ except:
     sys.path.append(os.path.abspath(path_to_petal))
     print('Couldn\'t find posconstants the usual way, resorting to sys.path.append')
     import posconstants as pc
-log_dir = pc.dirs['calib_logs']
-log_timestamp = pc.filename_timestamp_str()
-log_name = log_timestamp + '_disambig_theta.log'
-log_path = os.path.join(log_dir, log_name)
-logger, _, _ = simple_logger.start_logger(log_path)
-logger.info(f'Running {script_name} to ascertain true theta near hardstops.')
-assert2 = simple_logger.assert2
+# other imports
+import random
+import pandas
 
-# set up pecs
-from pecs import PECS
-pecs = PECS(interactive=True, logger=logger, inputfunc=simple_logger.input2)
-logger.info(f'PECS initialized, discovered PC ids {pecs.pcids}')
-pecs.tp_tol = 0.0   # correct POS_T, POS_P for FVC measurements above this err value
-pecs.tp_frac = 1.0  # when correcting POS_T, POS_P, do so by this fraction of err distance
-
-# some boilerplate
-common_move_meas_kwargs = {'match_radius': uargs.match_radius,
-                           'check_unmatched': uargs.check_unmatched,
-                           'test_tp': True,
-                           'num_meas': uargs.num_meas,
-                           }
-
-# global collections
-enabled_posids = set(pecs.get_enabled_posids('all', include_posinfo=False))
-allowed_to_fix = set(pecs.get_enabled_posids('sub', include_posinfo=False))
-unambig = set()  # stores all posids that have been resolved
-neighbor_data = pecs.ptlm.app_get("collider.pos_neighbors")
-neighbors = {}
-for these in neighbor_data.values():
-    neighbors.update(these)
-
-# algorithm
-def disambig(n_try):
-    '''See DESI-5911 for diagram and plain-language explanation of the basic algorithm.
-    
-    INPUTS:  
-        n_try ... Integer, indicates number of times to repeat. Repetition is done by
-                  recursing this function. If n_try == 0 --> break. The direction
-                  of trial moves is flipped with each repeat, so one would almost always
-                  want the initial call to have n_try >= 2. Even values do the first move
-                  away from the presumed hardstop, odd values go toward it. The idea of
-                  allowing more than 2 repeats is to deal with cases where a robot needs
-                  its neighbors to be disambiguated before it can be safely moved itself.
-    
-    OUTPUTS:
-        Set of posids that are still in ambiguous zone. Disabled positioners are
-        *excluded* from this set, regardless of what zone they are in.
+class disambig_class():
     '''
-    logger.info(f'Starting disambiguation iteration {uargs.num_tries - n_try + 1} of {uargs.num_tries}')
+    KF - 20210212
+        Not the best but putting this in a class so it can accept an outside PECS or logger instance.
+    '''
+    def __init__(self, pecs=None, logger=None, num_meas=1, match_radius=None, check_unmatched=False, num_tries=4):
+        if logger is None:
+            # set up logger
+            import simple_logger
+            log_dir = pc.dirs['calib_logs']
+            log_timestamp = pc.filename_timestamp_str()
+            log_name = log_timestamp + '_disambig_theta.log'
+            log_path = os.path.join(log_dir, log_name)
+            self.logger, _, _ = simple_logger.start_logger(log_path)
+        else:
+            self.logger = logger
+        self.logger.info(f'Running {script_name} to ascertain true theta near hardstops.')
 
-    # get ambiguous and unambiguous posids
-    global unambig
-    ambig_dict = pecs.quick_query('in_theta_hardstop_ambiguous_zone', posids=enabled_posids)
-    in_ambig_zone = {posid for posid, val in ambig_dict.items() if val == True}
-    all_ambig = in_ambig_zone - unambig  # because previous parking moves may have put already-resolved pos into ambig theta territory
-    unambig |= enabled_posids - all_ambig
-    logger.info(f'{len(all_ambig)} enabled positioner(s) are in theta hardstop ambiguous zone: {all_ambig}')
-    logger.info(f'{len(unambig)} enabled positioner(s) are unambiguous')
-    do_not_fix = all_ambig - allowed_to_fix
-    ambig = all_ambig & allowed_to_fix
-    if do_not_fix:
-        logger.info(f'{len(do_not_fix)} positioner(s) are not within the allowed-to-fix selection group. Excluded posids: {do_not_fix}')
-    if not ambig or n_try == 0:
-        return ambig
-    logger.info(f'Disambiguation attempt {uargs.num_tries - n_try + 1} of {uargs.num_tries}')
-    logger.info(f'Will attempt to resolve {len(ambig)} posid(s): {ambig}')
-    
-    # targets for unambiguous pos
-    active_posids = ambig | unambig
-    locT_current = pecs.quick_query(key='poslocT', posids=active_posids)
-    locT_targets = {posid: locT_current[posid] for posid in unambig}
-    locP_target = 150.0
-    
-    # where possible, target unambiguous posids "opposite" ambiguous neighbors,
-    # to maximize clearance
-    for posid in unambig:
-        these_neighbors = neighbors[posid]
-        these_ambig = {n for n in these_neighbors if n in ambig}
-        if these_ambig:
-            selected = random.choice(list(these_ambig))
-            locT_targets[posid] = locT_current[selected]  # this is a good config to minimize collision opportunity
+        if pecs is None:
+            # set up pecs
+            from pecs import PECS
+            self.pecs = PECS(interactive=True, logger=self.logger)
+            self.logger.info(f'PECS initialized, discovered PC ids {self.pecs.pcids}')
+        else:
+            self.pecs = pecs
+        self._tp_tol_old = self.pecs.tp_tol
+        self._tp_frac_old = self.pecs.tp_frac
+
+        self.num_tries = num_tries
+        self.match_radius = match_radius
+        self.check_unmatched = check_unmatched
+        self.num_meas = num_meas
+        # some boilerplate
+        self.common_move_meas_kwargs = {'match_radius': self.match_radius,
+                                        'check_unmatched': self.check_unmatched,
+                                        'test_tp': True,
+                                        'num_meas': self.num_meas,
+                                        }
+
+        # global collections
+        self.enabled_posids = set(pecs.get_enabled_posids('all', include_posinfo=False))
+        self.allowed_to_fix = set(pecs.get_enabled_posids('sub', include_posinfo=False))
+        self.unambig = set()  # stores all posids that have been resolved
+        self.neighbor_data = pecs.ptlm.app_get("collider.pos_neighbors")
+        self.neighbors = {}
+        for these in self.neighbor_data.values():
+            self.neighbors.update(these)
+
+    # algorithm
+    def disambig(self, n_try=None):
+        '''See DESI-5911 for diagram and plain-language explanation of the basic algorithm.
         
-    # move unambiguous positioners to targets
-    sorted_unambig = sorted(unambig)
-    anticollision = 'adjust_requested_only'
-    request_data = {'DEVICE_ID': sorted_unambig,
-                    'COMMAND': 'poslocTP',
-                    'X1': [locT_targets[posid] for posid in sorted_unambig],
-                    'X2': locP_target,
-                    'LOG_NOTE': pc.join_notes(script_name, 'parking move for unambiguous positioner'),
-                    }
-    request = pandas.DataFrame(request_data)
-    if request.empty:
-        logger.info('No unambiguous + enabled positioners detected.')
-    else:
-        logger.info(f'Doing parking move for {len(request)} unambiguous positioners. Anticollision mode: {anticollision}')
-        pecs.move_measure(request=request, anticollision=anticollision, **common_move_meas_kwargs)
+        INPUTS:  
+            n_try ... Integer, indicates number of times to repeat. Repetition is done by
+                      recursing this function. If n_try == 0 --> break. The direction
+                      of trial moves is flipped with each repeat, so one would almost always
+                      want the initial call to have n_try >= 2. Even values do the first move
+                      away from the presumed hardstop, odd values go toward it. The idea of
+                      allowing more than 2 repeats is to deal with cases where a robot needs
+                      its neighbors to be disambiguated before it can be safely moved itself.
+        
+        OUTPUTS:
+            Set of posids that are still in ambiguous zone. Disabled positioners are
+            *excluded* from this set, regardless of what zone they are in.
+        '''
+        if n_try is None:
+            n_try = self.num_tries
+        self.logger.info(f'Starting disambiguation iteration {self.num_tries - n_try + 1} of {self.num_tries}')
 
-    # retract ambiguous positioners' phi axes, again to maximize clearance
-    # (this is safely done by *not* allowing extra theta anticollision moves)
-    anticollision = 'freeze'
-    intP_current = pecs.quick_query(key='posintP', posids=ambig)
-    intP_target = 150.0  # this also helps *extend* fully-retracted arms a little --> improves the theta measurement
-    dP = {posid: intP_target - intP_current[posid] for posid in ambig}
-    dtdp_requests = {posid: {'target': [0.0, dP[posid]],
-                             'log_note': pc.join_notes(script_name, 'retraction move for ambiguous positioner'),
-                             } for posid in ambig
-                     }
-    logger.info(f'Doing retraction move for {len(dtdp_requests)} ambiguous positioners. Anticollision mode: {anticollision}')
-    pecs.ptlm.request_direct_dtdp(dtdp_requests, return_posids_only=True)
-    pecs.ptlm.schedule_moves(anticollision=anticollision)
-    pecs.move_measure(request=None, anticollision=anticollision, **common_move_meas_kwargs)
-    
-    # theta test moves on ambiguous positioners
-    anticollision = 'freeze'
-    intT_current = pecs.quick_query(key='posintT', posids=ambig)
-    ambig_max = pecs.quick_query(key='max_theta_hardstop_ambiguous_zone', posids=ambig)
-    ambig_min = pecs.quick_query(key='min_theta_hardstop_ambiguous_zone', posids=ambig)
-    dT_abs = {posid: ambig_max[posid] - ambig_min[posid] + pc.theta_hardstop_ambig_exit_margin for posid in ambig}
-    presumed_no_hardstop_dir = {posid: 1 if intT_current[posid] < 0 else -1 for posid in ambig}
-    move_dir = {posid: presumed_no_hardstop_dir[posid] * (-1 if n_try % 2 else 1) for posid in ambig}
-    dT = {posid: dT_abs[posid] * move_dir[posid] for posid in ambig}
-    dir_note = {posid: f'{"away from" if presumed_no_hardstop_dir[posid] == move_dir[posid] else "toward"} currently-presumed closest hardstop' for posid in ambig}
-    dtdp_requests = {posid: {'target': [dT[posid], 0.0],
-                             'log_note': pc.join_notes(script_name, 'theta test move on ambiguous positioner', dir_note[posid]),
-                             } for posid in ambig
-                     }
-    logger.info(f'Doing theta test move for {len(dtdp_requests)} ambiguous positioners. Anticollision mode: {anticollision}')
-    pecs.ptlm.request_direct_dtdp(dtdp_requests, return_posids_only=True)
-    pecs.ptlm.schedule_moves(anticollision=anticollision)
-    pecs.move_measure(request=None, anticollision=anticollision, **common_move_meas_kwargs)
-    return disambig(n_try=n_try - 1)
+        self.pecs.tp_tol = 0.0   # correct POS_T, POS_P for FVC measurements above this err value
+        self.pecs.tp_frac = 1.0  # when correcting POS_T, POS_P, do so by this fraction of err distance
+        # get ambiguous and unambiguous posids
+        #global unambig #replaced by class variable
+        ambig_dict = self.pecs.quick_query('in_theta_hardstop_ambiguous_zone', posids=self.enabled_posids)
+        in_ambig_zone = {posid for posid, val in ambig_dict.items() if val == True}
+        all_ambig = in_ambig_zone - self.unambig  # because previous parking moves may have put already-resolved pos into ambig theta territory
+        self.unambig |= self.enabled_posids - all_ambig
+        self.logger.info(f'{len(all_ambig)} enabled positioner(s) are in theta hardstop ambiguous zone: {all_ambig}')
+        self.logger.info(f'{len(self.unambig)} enabled positioner(s) are unambiguous')
+        do_not_fix = all_ambig - self.allowed_to_fix
+        ambig = all_ambig & self.allowed_to_fix
+        if do_not_fix:
+            self.logger.info(f'{len(do_not_fix)} positioner(s) are not within the allowed-to-fix selection group. Excluded posids: {do_not_fix}')
+        if not ambig or n_try == 0:
+            self.pecs.tp_tol = self._tp_tol_old
+            self.pecs.tp_frac = self._tp_frac_old
+            return ambig
+        self.logger.info(f'Disambiguation attempt {self.num_tries - n_try + 1} of {self.num_tries}')
+        self.logger.info(f'Will attempt to resolve {len(ambig)} posid(s): {ambig}')
+        
+        # targets for unambiguous pos
+        active_posids = ambig | self.unambig
+        locT_current = self.pecs.quick_query(key='poslocT', posids=active_posids)
+        locT_targets = {posid: locT_current[posid] for posid in self.unambig}
+        locP_target = 150.0
+        
+        # where possible, target unambiguous posids "opposite" ambiguous neighbors,
+        # to maximize clearance
+        for posid in self.unambig:
+            these_neighbors = self.neighbors[posid]
+            these_ambig = {n for n in these_neighbors if n in ambig}
+            if these_ambig:
+                selected = random.choice(list(these_ambig))
+                locT_targets[posid] = locT_current[selected]  # this is a good config to minimize collision opportunity
+            
+        # move unambiguous positioners to targets
+        sorted_unambig = sorted(self.unambig)
+        anticollision = 'adjust_requested_only'
+        request_data = {'DEVICE_ID': sorted_unambig,
+                        'COMMAND': 'poslocTP',
+                        'X1': [locT_targets[posid] for posid in sorted_unambig],
+                        'X2': locP_target,
+                        'LOG_NOTE': pc.join_notes(script_name, 'parking move for unambiguous positioner'),
+                        }
+        request = pandas.DataFrame(request_data)
+        if request.empty:
+            self.logger.info('No unambiguous + enabled positioners detected.')
+        else:
+            self.logger.info(f'Doing parking move for {len(request)} unambiguous positioners. Anticollision mode: {anticollision}')
+            self.pecs.move_measure(request=request, anticollision=anticollision, **self.common_move_meas_kwargs)
+
+        # retract ambiguous positioners' phi axes, again to maximize clearance
+        # (this is safely done by *not* allowing extra theta anticollision moves)
+        anticollision = 'freeze'
+        intP_current = self.pecs.quick_query(key='posintP', posids=ambig)
+        intP_target = 150.0  # this also helps *extend* fully-retracted arms a little --> improves the theta measurement
+        dP = {posid: intP_target - intP_current[posid] for posid in ambig}
+        dtdp_requests = {posid: {'target': [0.0, dP[posid]],
+                                 'log_note': pc.join_notes(script_name, 'retraction move for ambiguous positioner'),
+                                 } for posid in ambig
+                         }
+        self.logger.info(f'Doing retraction move for {len(dtdp_requests)} ambiguous positioners. Anticollision mode: {anticollision}')
+        self.pecs.ptlm.request_direct_dtdp(dtdp_requests, return_posids_only=True)
+        self.pecs.ptlm.schedule_moves(anticollision=anticollision)
+        self.pecs.move_measure(request=None, anticollision=anticollision, **self.common_move_meas_kwargs)
+        
+        # theta test moves on ambiguous positioners
+        anticollision = 'freeze'
+        intT_current = self.pecs.quick_query(key='posintT', posids=ambig)
+        ambig_max = self.pecs.quick_query(key='max_theta_hardstop_ambiguous_zone', posids=ambig)
+        ambig_min = self.pecs.quick_query(key='min_theta_hardstop_ambiguous_zone', posids=ambig)
+        dT_abs = {posid: ambig_max[posid] - ambig_min[posid] + pc.theta_hardstop_ambig_exit_margin for posid in ambig}
+        presumed_no_hardstop_dir = {posid: 1 if intT_current[posid] < 0 else -1 for posid in ambig}
+        move_dir = {posid: presumed_no_hardstop_dir[posid] * (-1 if n_try % 2 else 1) for posid in ambig}
+        dT = {posid: dT_abs[posid] * move_dir[posid] for posid in ambig}
+        dir_note = {posid: f'{"away from" if presumed_no_hardstop_dir[posid] == move_dir[posid] else "toward"} currently-presumed closest hardstop' for posid in ambig}
+        dtdp_requests = {posid: {'target': [dT[posid], 0.0],
+                                 'log_note': pc.join_notes(script_name, 'theta test move on ambiguous positioner', dir_note[posid]),
+                                 } for posid in ambig
+                         }
+        self.logger.info(f'Doing theta test move for {len(dtdp_requests)} ambiguous positioners. Anticollision mode: {anticollision}')
+        self.pecs.ptlm.request_direct_dtdp(dtdp_requests, return_posids_only=True)
+        self.pecs.ptlm.schedule_moves(anticollision=anticollision)
+        self.pecs.move_measure(request=None, anticollision=anticollision, **self.common_move_meas_kwargs)
+        return self.disambig(n_try=n_try - 1)
 
 if __name__ == '__main__':
-    ambig = disambig(n_try=uargs.num_tries)
-    logger.info('Disambiguation loops complete.')
+    disambig_obj = disambig_class(pecs=None, logger=None, num_meas=uargs.num_meas, num_tries=uargs.num_tries,
+                                  match_radius=uargs.match_radius, check_unmatched=uargs.check_unmatched)
+    ambig = disambig_obj.disambig()
+    disambig_obj.logger.info('Disambiguation loops complete.')
     if ambig:
-        details = pecs.ptlm.quick_table(posids=ambig, coords=['posintTP', 'poslocTP'], as_table=False, sort='POSID')
+        details = disambig_obj.pecs.ptlm.quick_table(posids=ambig, coords=['posintTP', 'poslocTP'], as_table=False, sort='POSID')
         if pc.is_string(details):
             details_str = details
         else:
             details_str = ''
             for petal_id, table_str in details.items():
                 details_str += f'\n{petal_id}\n{table_str}\n'
-        logger.warning(f'{len(ambig)} positioners remain *unresolved*. Details:\n{details_str}')
+        disambig_obj.logger.warning(f'{len(ambig)} positioners remain *unresolved*. Details:\n{details_str}')
     else:
-        logger.info('All selected ambiguous cases were resolved!')
+        disambig_obj.logger.info('All selected ambiguous cases were resolved!')
 
---
+++ pecs/fp_setup.py
@@ -0,0 +1,129 @@
+# set up a log file
+import logging
+import simple_logger
+import traceback
+import os
+import posconstants as pc
+from pecs import PECS
+
+update_error_report_thresh = 1.0 #warn user that an update of more than value mm was found
+
+
+log_dir = pc.dirs['sequence_logs']
+log_timestamp = pc.filename_timestamp_str()
+log_name = 'FP_setup_' + log_timestamp + '.log'
+log_path = os.path.join(log_dir, log_name)
+logger, logger_fh, logger_sh = simple_logger.start_logger(log_path)
+logger_fh.setLevel(logging.DEBUG)
+logger_sh.setLevel(logging.INFO)
+simple_logger.input2(f'Alert: you are about to run the focalplane setup script to recover positioners or prepare the focalplane. This takes up to half an hour to execute. Hit enter to continue. ')
+
+cs = PECS(interactive=False, test_name=f'FP_setup', logger=logger, inputfunc=simple_logger.input2)
+
+#from 1p_calib import onepoint # doesn't work
+import importlib
+onept = importlib.import_module('1p_calib')
+onepoint = onept.onepoint
+from disambiguate_theta import disambig_class
+
+def get_pos_set(which='enabled'):
+    pos_set = set()
+    if which == 'enabled':
+        pos_dict = cs.ptlm.all_enabled_posids()
+    elif which == 'disabled':
+        pos_dict = cs.ptlm.all_disabled_posids()
+    for poslist in pos_dict.values():
+        pos_set |= set(poslist)
+    return pos_set
+
+initial_disabled = get_pos_set('disabled')
+initial_enabled = get_pos_set('enabled')
+
+logger.info('FP_SETUP: enabling positioners...')
+cs.ptlm.enable_positioners(ids='all', comment='FP setup script initial enable')
+
+e = None #no exception
+
+try:
+    logger.info('FP_SETUP: running 1p calibration...')
+    enabled_before_1p = get_pos_set('enabled')
+    updates = onepoint(cs, mode='posintTP', move=False, commit=True, tp_tol=0.065,
+                       tp_frac=1.0, num_meas=1, use_disabled=True, check_unmatched=True)
+    enabled_after_1p = get_pos_set('enabled')
+    updates = updates[['DEVICE_ID', 'DEVICE_LOC', 'PETAL_LOC', 'ERR_XY', 'POS_T', 'POS_P', 'OLD_POS_T', 'OLD_POS_P']]
+    updates.sort_values('ERR_XY', inplace=True, ascending=False)
+    logger.debug('Updates sorted by descending ERR_XY:')
+    logger.debug(updates.to_string())
+    #logger.info(updates.to_string(max_rows=100))
+    if update_error_report_thresh:
+        selection = updates.loc[updates['ERR_XY'] > update_error_report_thresh]
+        if not(selection.empty()):
+            logger.warn(f'FP_SETUP: found {len(selection)} positioners with updates greater than {update_error_report_thresh}!')
+            logger.warn(f'FP_SETUP: update deatils: \n{selection}')
+    disabled_during_1p = enabled_before_1p - enabled_after_1p
+    logger.info(f'FP_SETUP: {len(disabled_during_1p)} disabled during initial 1p calib. Posids {disabled_during_1p}')
+
+    logger.info('FP_SETUP: running disambiguation loops...')
+    enabled_before_disambig = get_pos_set('enabled')
+    disambig_obj = disambig_class(pecs=cs, logger=logger, num_meas=1, check_unmatched=True, num_tries=4)
+    ambig = disambig_obj.disambig()
+    logger.info('FP_SETUP: Disambiguation loops complete.')
+    if ambig:
+        details = cs.ptlm.quick_table(posids=ambig, coords=['posintTP', 'poslocTP'], as_table=False, sort='POSID')
+        if pc.is_string(details):
+            details_str = details
+        else:
+            details_str = ''
+            for petal_id, table_str in details.items():
+                details_str += f'\n{petal_id}\n{table_str}\n'
+        logger.warning(f'{len(ambig)} positioners remain *unresolved* and will be disabled. Details:\n{details_str}')
+        cs.ptlm.disable_positioners(ids=ambig, comment='FP setup - disabling positioners that remain in ambiguous theta range.')
+    else:
+        logger.info('FP_SETUP: All selected ambiguous cases were resolved!')
+    enabled_after_disambig = get_pos_set('enabled')
+    disabled_during_disambig = enabled_before_disambig - enabled_after_disambig
+    logger.info(f'FP_SETUP: {len(disabled_during_disambig)} disabled during disambiguation loops. Posids {disabled_during_disambig}')
+
+    logger.info(f'FP_SETUP: running final 1p calibration...')
+    enabled_before_1p2 = get_pos_set('enabled')
+    updates = onepoint(cs, mode='posintTP', move=False, commit=True, tp_tol=0.065,
+                       tp_frac=1.0, num_meas=3, use_disabled=True, check_unmatched=True)
+    enabled_after_1p2 = get_pos_set('enabled')
+    updates = updates[['DEVICE_ID', 'DEVICE_LOC', 'PETAL_LOC', 'ERR_XY', 'POS_T', 'POS_P', 'OLD_POS_T', 'OLD_POS_P']]
+    updates.sort_values('ERR_XY', inplace=True, ascending=False)
+    logger.debug('Updates sorted by descending ERR_XY:')
+    logger.debug(updates.to_string())
+    #logger.info(updates.to_string(max_rows=100))
+    if update_error_report_thresh:
+        selection = updates.loc[updates['ERR_XY'] > update_error_report_thresh]
+        if not(selection.empty()):
+            logger.warn(f'FP_SETUP: found {len(selection)} positioners with updates greater than {update_error_report_thresh}!')
+            logger.warn(f'FP_SETUP: update deatils: \n{selection}')
+    disabled_during_1p2 = enabled_before_1p2 - enabled_after_1p2
+    logger.info(f'FP_SETUP: {len(disabled_during_1p2)} disabled during initial 1p calib. Posids {disabled_during_1p2}')
+    final_enabled = get_pos_set('enabled')
+except Exception as e:
+    logger.error('FP_SETUP crashed! See traceback below:')
+    logger.critical(traceback.format_exc())
+    logger.info('Attempting to preform cleanup before hard crashing. Configure the instance before trying again.')
+    try:
+        logger.info('FP_SETUP: Re-disabling initially disabled positioners for safety...')
+        cs.ptlm.disable_positioners(ids=initial_disabled, comment='FP_SETUP - crashed in execution, resetting disabled devices')
+    except:
+        logger.critical('FP_SETUP: failed to return to initial state. DO NOT continue without consulting FP expert.')
+
+if e is None:
+    logger.info('FP_SETUP: Successfully completed! Please record any following notes in the log book in addition to any other observations:')
+    newly_enabled = final_enabled - initial_enabled
+    if newly_enabled:
+        logger.info(f'FP_SETUP NOTE: recovered {len(newly_enabled)} positioner robots.')
+    if ambig:
+        logger.info(f'FP_SETUP NOTE: {len(ambig)} positioners remaing in ambiguous theta range and are not used tonight: posids {ambig}')
+    non_ambig_disabled = disabled_during_1p | disabled_during_1p2 | (disabled_during_disambig-ambig)
+    if non_ambig_disabled:
+        logger.info(f'FP_SETUP_NOTE: {len(non_ambig_disabled)} positioners were disabled during the course of this script for other reasons, likely due to match errors.')
+        logger.info(f'FP_SETUP_NOTE: other disabled posids: {non_ambig_disabled}')
+
+cs.fvc_collect()
+logger.info(f'Log file: {log_path}')
+simple_logger.clear_logger()
