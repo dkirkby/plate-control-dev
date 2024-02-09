a1149bd67fc52f7c3399b91ab67397c2028ecdd4
carldobson-lbl <cadobson@lbl.gov>
2024-02-09 03:17:40 +0000

    Corrected the algorithm in rewrite_linphi_move_table

--- petal/posschedulestage.py
+++ petal/posschedulestage.py
@@ -85,25 +85,23 @@ class PosScheduleStage(object):
                     l_idx += 1
                 else:
                     new_phi_direction = 1 if phi_dist >= 0.0 else -1
-                    scale_ccw = table.posmodel.linphi_params['CCW_SCALE_A'] if new_phi_direction == 1 else table.posmodel.linphi_params['CW_SCALE_A']
-                    scale_cw = table.posmodel.linphi_params['CW_SCALE_A'] if new_phi_direction == 1 else table.posmodel.linphi_params['CCW_SCALE_A']
-                    scale_ccw = float(scale_ccw)
-                    scale_cw = float(scale_cw)
+                    scale_ccw = float(table.posmodel.linphi_params['CCW_SCALE_A'])
+                    scale_cw = float(table.posmodel.linphi_params['CW_SCALE_A'])
                     #NOTE: The first and second moves should have abs(move) >= pc.P_zeno_jog
                     if new_phi_direction == table.posmodel.linphi_params['LAST_P_DIR']:
-                        if new_phi_direction > 0:
+                        if new_phi_direction > 0:   # must go negative, then positive
                             first_move = -pc.P_zeno_jog / scale_cw
                             second_move = (pc.P_zeno_jog + phi_dist) / scale_ccw
-                        else:
-                            first_move = (-pc.P_zeno_jog + phi_dist) / scale_ccw
-                            second_move = pc.P_zeno_jog / scale_cw
-                    else:
-                        if new_phi_direction > 0:
-                            first_move = (pc.P_zeno_jog + phi_dist) / scale_ccw
-                            second_move = -pc.P_zeno_jog / scale_cw
-                        else:
+                        else:                       # must go positive, then negative
                             first_move = pc.P_zeno_jog / scale_ccw
                             second_move = (-pc.P_zeno_jog + phi_dist) / scale_cw
+                    else:
+                        if new_phi_direction > 0:   # must go positive, then negative
+                            first_move = (pc.P_zeno_jog + phi_dist) / scale_ccw
+                            second_move = -pc.P_zeno_jog / scale_cw
+                        else:                       # must go positive, then negative
+                            first_move = (-pc.P_zeno_jog + phi_dist) / scale_cw
+                            second_move = pc.P_zeno_jog / scale_ccw
 #                   Probably need next two lines to prevent banging into hard stops, but if they adjust either move, we'll need
 #                   to adjust the other.
 #                   first_move_limited = self._range_limited_jog(first_move ... and other args)
